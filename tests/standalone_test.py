f415052659b2e05e9fa928958fa81ab22f823833
Firmware Batman <firmware@comma.ai>
2017-04-27 12:08:03 -0700

    improve standalone test

--- tests/standalone_test.py
+++ tests/standalone_test.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import os
-from panda.lib.panda import Panda
+import struct
 import time
+from panda.lib.panda import Panda
 
 if __name__ == "__main__":
   if os.getenv("WIFI") is not None:
@@ -10,12 +11,15 @@ if __name__ == "__main__":
     p = Panda()
   print p.health()
 
+  a = 0
   while 1:
     # flood
-    p.can_send(0xaa, "\xaa"*8, 0)
-    p.can_send(0xaa, "\xaa"*8, 1)
-    p.can_send(0xaa, "\xaa"*8, 4)
+    msg = "\xaa"*4 + struct.pack("I", a)
+    p.can_send(0xaa, msg, 0)
+    p.can_send(0xaa, msg, 1)
+    p.can_send(0xaa, msg, 4)
     time.sleep(0.01)
 
     print p.can_recv()
+    a += 1
 
