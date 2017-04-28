234a514b465b8a0555bf828a4dc36e77cc8d3690
George Hotz <george@comma.ai>
2017-04-27 22:06:31 -0700

    add crypto stuff to ESP as well

--- board/build.mk
+++ board/build.mk
@@ -1,4 +1,4 @@
-CFLAGS += -I inc -nostdlib
+CFLAGS += -I inc -I ../ -nostdlib
 CFLAGS += -Tstm32_flash.ld
 
 CC = arm-none-eabi-gcc
@@ -37,8 +37,8 @@ obj/gitversion.h:
 	echo "const uint8_t gitversion[] = \"RELEASE\";" > $@
 endif
 
-obj/cert.h: tools/getcertheader.py
-	./tools/getcertheader.py $(CERT).pub > $@
+obj/cert.h: ../crypto/getcertheader.py
+	../crypto/getcertheader.py $(CERT).pub > $@
 
 obj/bootstub.$(PROJ_NAME).o: bootstub.c early.h obj/cert.h
 	$(CC) $(CFLAGS) -o $@ -c $<
@@ -47,10 +47,10 @@ obj/main.$(PROJ_NAME).o: main.c *.h obj/gitversion.h
 	$(CC) $(CFLAGS) -o $@ -c $<
 
 # TODO(geohot): learn to use Makefiles
-obj/sha.o: crypto/sha.c
+obj/sha.o: ../crypto/sha.c
 	$(CC) $(CFLAGS) -o $@ -c $<
 
-obj/rsa.o: crypto/rsa.c
+obj/rsa.o: ../crypto/rsa.c
 	$(CC) $(CFLAGS) -o $@ -c $<
 
 obj/$(STARTUP_FILE).o: $(STARTUP_FILE).s
@@ -61,7 +61,7 @@ obj/$(PROJ_NAME).bin: obj/$(STARTUP_FILE).o obj/main.$(PROJ_NAME).o
   # hack
 	$(CC) -Wl,--section-start,.isr_vector=0x8004000 $(CFLAGS) -o obj/$(PROJ_NAME).elf $^
 	$(OBJCOPY) -v -O binary obj/$(PROJ_NAME).elf obj/code.bin
-	./tools/sign.py obj/code.bin $@ $(CERT)
+	../crypto/sign.py obj/code.bin $@ $(CERT)
 
 obj/bootstub.$(PROJ_NAME).bin: obj/$(STARTUP_FILE).o obj/bootstub.$(PROJ_NAME).o obj/sha.o obj/rsa.o
 	$(CC) $(CFLAGS) -o obj/bootstub.$(PROJ_NAME).elf $^
