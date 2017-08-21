6bf3bf5bda9d0d0d4fc4fb7241e9d5c7e3dd7f26
Firmware Batman <firmware@comma.ai>
2017-08-21 14:43:39 -0700

    add panda release maker

---
+++ release/make_release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# make ST
+pushd .
+cd ../board
+make clean
+RELEASE=1 make obj/panda.bin
+popd
+
+
+# make ESP
+pushd .
+cd ../boardesp
+make clean
+RELEASE=1 make user1.bin
+RELEASE=1 make user2.bin
+popd
+
+# make zip file
+pushd .
+cd ..
+cat VERSION > /tmp/version
+echo -en "-" >> /tmp/version
+git rev-parse HEAD >> /tmp/version
+cat /tmp/version
+zip -j release/panda-$(cat /tmp/version).zip ~/one/panda/board/obj/panda.bin ~/one/panda/boardesp/user?.bin /tmp/version
+popd
+
