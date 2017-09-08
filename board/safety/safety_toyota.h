1b00e8c8304a3f5e73dadb428fbdfdb341119f92
George Hotz <george@comma.ai>
2017-09-07 21:37:47 -0700

    toyota torque limit

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -1,4 +1,11 @@
+uint16_t ego_speed = 0;
+
 static void toyota_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
+  // get the up to date speed
+  if ((to_push->RIR>>21) == 0xb4) {
+    // not sure about the unit of this
+    ego_speed = ((to_push->RDHR) & 0xFF00) | ((to_push->RDHR >> 16) & 0xFF);
+  }
 
   // exit controls on ACC off
   if ((to_push->RIR>>21) == 0x1D2) {
@@ -16,10 +23,13 @@ static int toyota_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 
   // STEER: safety check on bytes 2-3
   if ((to_send->RIR>>21) == 0x2E4) {
+    int16_t desired_torque = (to_send->RDLR & 0xFF00) | ((to_send->RDLR >> 16) & 0xFF);
+
     if (controls_allowed) {
       // all messages are fine here
+      // TODO: add speed dependant torque limit
     } else {
-      if (to_send->RDLR & 0xFFFF00) return 0;
+      if (desired_torque != 0) return 0;
     }
   }
 
