bbd931e945978567e6604e5ab3e2f23703cb647d
George Hotz <george@comma.ai>
2017-10-28 20:08:30 -0700

    Up max torque error to 500 to deal with noise

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -9,7 +9,7 @@ const int32_t MAX_TORQUE = 1500;       // max torque cmd allowed ever
 // packet is sent at 100hz, so this limit is 800/sec
 const int32_t MAX_RATE_UP = 8;         // ramp up slow
 const int32_t MAX_RATE_DOWN = 45;      // ramp down fast
-const int32_t MAX_TORQUE_ERROR = 300;  // max torque cmd in excess of torque motor
+const int32_t MAX_TORQUE_ERROR = 500;  // max torque cmd in excess of torque motor
 
 // real time torque limit to prevent controls spamming
 // the real time limit is 1500/sec
