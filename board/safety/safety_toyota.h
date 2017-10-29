8362bad2c1004efc7a0b963989f4c6cf43325aa5
George Hotz <george@comma.ai>
2017-10-28 20:12:13 -0700

    up to 10

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -7,7 +7,7 @@ const int32_t MAX_TORQUE = 1500;       // max torque cmd allowed ever
 
 // rate based torque limit + stay within actually applied
 // packet is sent at 100hz, so this limit is 800/sec
-const int32_t MAX_RATE_UP = 8;         // ramp up slow
+const int32_t MAX_RATE_UP = 10;        // ramp up slow
 const int32_t MAX_RATE_DOWN = 45;      // ramp down fast
 const int32_t MAX_TORQUE_ERROR = 500;  // max torque cmd in excess of torque motor
 
