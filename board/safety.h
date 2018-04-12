cac7b318f0b5c7bce250755886cb15851496500e
George Hotz <george@comma.ai>
2018-04-11 20:47:29 -0700

    only panda has float

--- board/safety.h
+++ board/safety.h
@@ -23,7 +23,9 @@ int controls_allowed = 0;
 #include "safety/safety_defaults.h"
 #include "safety/safety_honda.h"
 #include "safety/safety_toyota.h"
+#ifdef PANDA
 #include "safety/safety_toyota_ipas.h"
+#endif
 #include "safety/safety_gm.h"
 #include "safety/safety_elm327.h"
 
@@ -66,7 +68,9 @@ const safety_hook_config safety_hook_registry[] = {
   {SAFETY_HONDA_BOSCH, &honda_bosch_hooks},
   {SAFETY_TOYOTA, &toyota_hooks},
   {SAFETY_TOYOTA_NOLIMITS, &toyota_nolimits_hooks},
+#ifdef PANDA
   {SAFETY_TOYOTA_IPAS, &toyota_ipas_hooks},
+#endif
   {SAFETY_GM, &gm_hooks},
   {SAFETY_ALLOUTPUT, &alloutput_hooks},
   {SAFETY_ELM327, &elm327_hooks},
