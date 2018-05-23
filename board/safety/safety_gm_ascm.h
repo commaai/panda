50d4da866ce255ddd5dc509b0d0bcbbc87785492
Riccardo <riccardo.biasini@gmail.com>
2018-05-23 12:44:54 -0700

    better using one funciton

--- board/safety/safety_gm_ascm.h
+++ board/safety/safety_gm_ascm.h
@@ -1,13 +1,21 @@
+// block 0x152 and 0x154, which are the lkas command from ASCM1 and ASCM2
+int is_blocked_addr(uint32_t addr) {
+  if ((addr == 0x152) || (addr == 0x154)) {
+    return 1;
+  }
+  return 0;
+}
+
 static int gm_ascm_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   if (bus_num == 0) {
-    if (((to_fwd->RIR>>21) == 0x152) || ((to_fwd->RIR>>21) == 0x154)) {
+    if (is_blocked_addr(to_fwd->RIR>>21)) {
       return -1;
     }
     return 2;
   }
 
   if (bus_num == 2) {
-    if (((to_fwd->RIR>>21) == 0x152) || ((to_fwd->RIR>>21) == 0x154)) {
+    if (is_blocked_addr(to_fwd->RIR>>21)) {
       return -1;
     }
     return 0;
