f7602dfa18e46561d5e39477f2aa4add55c538ed
Riccardo <riccardo.biasini@gmail.com>
2018-05-24 12:32:04 -0700

    blocking 0x152-0x154 and reforwarding 0x153

--- board/safety/safety_gm_ascm.h
+++ board/safety/safety_gm_ascm.h
@@ -1,25 +1,29 @@
-// block 0x152 and 0x154, which are the lkas command from ASCM1 and ASCM2
-int is_blocked_addr(uint32_t addr) {
-  if ((addr == 0x152) || (addr == 0x154)) {
-    return 0;
-  }
-  return 0;
-}
+// BUS 0 is on the LKAS module (ASCM) side
+// BUS 2 is on the actuator (EPS) side
 
 static int gm_ascm_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
+
+  uint32_t addr = to_fwd->RIR>>21;
+
   if (bus_num == 0) {
-    if (is_blocked_addr(to_fwd->RIR>>21)) {
+    // do not propagate lkas messages from ascm to actuators
+    // block 0x152 and 0x154, which are the lkas command from ASCM1 and ASCM2
+    if ((addr == 0x152) || (addr == 0x154)) {
       return -1;
+    }
+    // on the chassis bus, the OBDII port is on the module side, so we need to read
+    // the lkas messages sent by openpilot (put on unused 0x153 addr) and send it to 
+    // the actuator as 0x154
+    if (addr == 0x153) {
+      to_fwd->RIR = (0x154 << 21) | (to_fwd->RIR & 0x1fffff);
     }
     return 2;
   }
 
   if (bus_num == 2) {
-    if (is_blocked_addr(to_fwd->RIR>>21)) {
-      return -1;
-    }
     return 0;
   }
+
   return -1;
 }
 
