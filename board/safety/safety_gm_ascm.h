bba651e6df1ec5f6600361ab7e6811529ac3ca82
Riccardo <riccardo.biasini@gmail.com>
2018-05-23 21:57:26 -0700

    fixced ign bug and passing everything for now

--- board/safety/safety_gm_ascm.h
+++ board/safety/safety_gm_ascm.h
@@ -1,7 +1,7 @@
 // block 0x152 and 0x154, which are the lkas command from ASCM1 and ASCM2
 int is_blocked_addr(uint32_t addr) {
   if ((addr == 0x152) || (addr == 0x154)) {
-    return 1;
+    return 0;
   }
   return 0;
 }
@@ -23,11 +23,16 @@ static int gm_ascm_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   return -1;
 }
 
+static int gm_ascm_ign_hook() {
+  return -1;
+}
+
 const safety_hooks gm_ascm_hooks = {
   .init = nooutput_init,
   .rx = default_rx_hook,
   .tx = alloutput_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
+  .ignition = gm_ascm_ign_hook,
   .fwd = gm_ascm_fwd_hook,
 };
 
