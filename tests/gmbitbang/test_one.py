a1a2d979f0f4162a4e26dca82b5d65e92780e827
George Hotz <george@comma.ai>
2018-06-15 16:55:31 -0700

    gmlan test

--- tests/gmbitbang/test_one.py
+++ tests/gmbitbang/test_one.py
@@ -4,6 +4,14 @@ from panda import Panda
 
 p = Panda()
 p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+
+# ack any crap on bus
+p.set_gmlan(bus=2)
+time.sleep(0.1)
+while len(p.can_recv()) > 0:
+  print "clearing"
+  time.sleep(0.1)
+print "cleared"
 p.set_gmlan(bus=None)
 
 iden = 18000
