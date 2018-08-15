05306c5c9cdc6c78508b6930f238c0c0b19e2cab
Robert Cotran <robert@cotranconsulting.com>
2018-08-15 19:10:21 -0400

    Moved Tesla safety include to within IFDEF PANDA as legacy neo boards don't support floats used in Tesla safety code

--- board/safety.h
+++ board/safety.h
@@ -49,7 +49,9 @@ int controls_allowed = 0;
 #include "safety/safety_gm.h"
 #include "safety/safety_ford.h"
 #include "safety/safety_cadillac.h"
+#ifdef PANDA
 #include "safety/safety_tesla.h"
+#endif
 #include "safety/safety_elm327.h"
 
 const safety_hooks *current_hooks = &nooutput_hooks;
