cf53e369c1ddaebb29aab808248d03e36dd2d852
Bruce Wayne <batman@z840-openpilot.internal>
2018-11-02 20:53:40 -0700

    forward cam on toyota

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -156,7 +156,7 @@ static void toyota_init(int16_t param) {
 static int toyota_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
 
   // forward cam to radar and viceversa if car is dsu-less, except lkas cmd and hud
-  if ((bus_num == 0 || bus_num == 2) && toyota_no_dsu_car && !toyota_giraffe_switch_1) {
+  if (bus_num == 0 || bus_num == 2) {
     int addr = to_fwd->RIR>>21;
     bool is_lkas_msg = (addr == 0x2E4 || addr == 0x412) && bus_num == 2;
     return is_lkas_msg? -1 : (uint8_t)(~bus_num & 0x2);
