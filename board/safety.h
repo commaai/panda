d275fa8489466e2c70e41ad5eda39657c5dd9bfc
Bugsy <30946888+bugsy924@users.noreply.github.com>
2019-01-29 13:42:06 +0800

    Subaru: Forwarding (#152)
    Bus 0 to 1
    Bus 1 to 0 exept LKAS

--- board/safety.h
+++ board/safety.h
@@ -63,6 +63,7 @@ int controls_allowed = 0;
 #include "safety/safety_cadillac.h"
 #include "safety/safety_hyundai.h"
 #include "safety/safety_chrysler.h"
+#include "safety/safety_subaru.h"
 #include "safety/safety_elm327.h"
 
 const safety_hooks *current_hooks = &nooutput_hooks;
@@ -104,6 +105,7 @@ typedef struct {
 #define SAFETY_HYUNDAI 7
 #define SAFETY_TESLA 8
 #define SAFETY_CHRYSLER 9
+#define SAFETY_SUBARU 10
 #define SAFETY_TOYOTA_IPAS 0x1335
 #define SAFETY_TOYOTA_NOLIMITS 0x1336
 #define SAFETY_ALLOUTPUT 0x1337
@@ -119,6 +121,7 @@ const safety_hook_config safety_hook_registry[] = {
   {SAFETY_CADILLAC, &cadillac_hooks},
   {SAFETY_HYUNDAI, &hyundai_hooks},
   {SAFETY_CHRYSLER, &chrysler_hooks},
+  {SAFETY_SUBARU, &subaru_hooks},
   {SAFETY_TOYOTA_NOLIMITS, &toyota_nolimits_hooks},
 #ifdef PANDA
   {SAFETY_TOYOTA_IPAS, &toyota_ipas_hooks},
