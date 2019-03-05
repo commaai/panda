0b10bb70ecc856ab229ed1296309fcd782e577bd
Riccardo <riccardo.biasini@gmail.com>
2019-03-05 15:37:17 -0800

    Subaru safety: move camera to bus 2

--- board/safety/safety_subaru.h
+++ board/safety/safety_subaru.h
@@ -12,17 +12,17 @@ static int subaru_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 }
 
 static int subaru_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
-  
+
   // shifts bits 29 > 11
   int32_t addr = to_fwd->RIR >> 21;
 
   // forward CAN 0 > 1
   if (bus_num == 0) {
-    return 1; // ES CAN
+    return 2; // ES CAN
   }
   // forward CAN 1 > 0, except ES_LKAS
-  else if (bus_num == 1) {
-    
+  else if (bus_num == 2) {
+
     // outback 2015
     if (addr == 0x164) {
       return -1;
