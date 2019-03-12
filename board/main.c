a6193a82850fccf5f3d4dfcab46ac9e745fee94f
rbiasini <riccardo.biasini@gmail.com>
2019-03-11 23:41:40 -0700

    Dcp remove (#168)
    * No DCP charge mode when building from EON

--- board/main.c
+++ board/main.c
@@ -590,8 +590,6 @@ int main() {
     uint64_t marker = 0;
     #define CURRENT_THRESHOLD 0xF00
     #define CLICKS 8
-    // Enough clicks to ensure that enumeration happened. Should be longer than bootup time of the device connected to EON
-    #define CLICKS_BOOTUP 30
   #endif
 
   for (cnt=0;;cnt++) {
@@ -618,8 +616,9 @@ int main() {
           }
           break;
         case USB_POWER_CDP:
-          // been CLICKS_BOOTUP clicks since we switched to CDP
-          if ((cnt-marker) >= CLICKS_BOOTUP ) {
+#ifndef EON
+          // been CLICKS clicks since we switched to CDP
+          if ((cnt-marker) >= CLICKS) {
             // measure current draw, if positive and no enumeration, switch to DCP
             if (!is_enumerated && current < CURRENT_THRESHOLD) {
               puts("USBP: no enumeration with current draw, switching to DCP mode\n");
@@ -631,6 +630,7 @@ int main() {
           if (current >= CURRENT_THRESHOLD) {
             marker = cnt;
           }
+#endif
           break;
         case USB_POWER_DCP:
           // been at least CLICKS clicks since we switched to DCP
