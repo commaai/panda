f679fbaa91059e384b667e95e9c822dac16bfc60
Nigel Armstrong <legonigel@users.noreply.github.com>
2019-03-13 03:37:04 -0700

    More reliable lline relay

--- board/drivers/lline_relay.h
+++ board/drivers/lline_relay.h
@@ -5,10 +5,10 @@
 // 30us cycles, 1 high, 90 low
 
 volatile int turn_on_relay = 0;
-volatile int on_cycles = 90;
+volatile int on_cycles = 25;
 
 //240 ms timeout
-#define LLINE_TIMEOUT_CYCLES 8000
+#define LLINE_TIMEOUT_CYCLES 1250
 volatile int timeout_cycles = LLINE_TIMEOUT_CYCLES;
 
 void TIM5_IRQHandler(void) {
@@ -18,37 +18,54 @@ void TIM5_IRQHandler(void) {
     if (timeout_cycles == 0) {
       turn_on_relay = 0;
     }
-    if (on_cycles) {
+    if (on_cycles > 0) {
       if (turn_on_relay) {
         set_gpio_output(GPIOC, 10, 0);
       }
     }
     else {
       set_gpio_output(GPIOC, 10, 1);
-      on_cycles = 90;
+      on_cycles = 25;
     }
   }
-  TIM5->ARR = 30-1;
+  TIM5->ARR = 160-1;
   TIM5->SR = 0;
 }
 
 void lline_relay_init (void) {
+  puts("INIT LLINE\n");
   set_gpio_mode(GPIOC, 10, MODE_OUTPUT);
 
   // setup
   TIM5->PSC = 48-1; // tick on 1 us
   TIM5->CR1 = TIM_CR1_CEN;   // enable
   TIM5->ARR = 50-1;         // 50 us
+  TIM5->DIER = TIM_DIER_UIE; // update interrupt
+  TIM5->CNT = 0;
 
-  // in case it's disabled
   NVIC_EnableIRQ(TIM5_IRQn);
 
-  // run the interrupt
-  TIM5->DIER = TIM_DIER_UIE; // update interrupt
-  TIM5->SR = 0;
+#ifdef DEBUG
+  puts(" SR ");
+  putui(TIM5->SR);
+  puts(" PSC ");
+  putui(TIM5->PSC);
+  puts(" CR1 ");
+  putui(TIM5->CR1);
+  puts(" ARR ");
+  putui(TIM5->ARR);
+  puts(" DIER ");
+  putui(TIM5->DIER);
+  puts(" SR ");
+  putui(TIM5->SR);
+  puts(" CNT ");
+  putui(TIM5->CNT);
+  puts("\n");
+#endif
 }
 
 void lline_relay_release (void) {
+  puts("RELEASE LLINE\n");
   set_gpio_alternate(GPIOC, 10, GPIO_AF7_USART3);
   NVIC_DisableIRQ(TIM5_IRQn);
 }
