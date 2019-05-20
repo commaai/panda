f887e21da87215f809ec28b3b91a8ff25bad9c29
Riccardo <riccardo.biasini@gmail.com>
2019-05-20 15:12:31 -0700

    better misra test

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
 git clone https://github.com/danmar/cppcheck.git || true
 cd cppcheck
-make
+make -j4
 cd ../../../
-ls
 tests/misra/cppcheck/cppcheck --dump board/main.c
-python tests/misra/cppcheck/addons/misra.py board/main.c.dump
+python tests/misra/cppcheck/addons/misra.py board/main.c.dump 2>/tmp/misra/output.txt  || true
