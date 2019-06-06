20171596f713b7453d7f1d16cdcaa13b110c6f22
Riccardo <riccardo.biasini@gmail.com>
2019-06-06 16:00:53 -0700

    update cppcheck ref to have more comprehensive misra checks

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 git clone https://github.com/danmar/cppcheck.git || true
 cd cppcheck
-git checkout 29e5992e51ecf1ddba469c73a0eed0b28b131de5
+git checkout 1584e6236758d54b0d246d41771d67338e0bd41b
 make -j4
 cd ../../../
 tests/misra/cppcheck/cppcheck --dump board/main.c
