10d805f75b075963e4073591da62d4b98485d099
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2019-06-05 19:00:38 -0700

    replay test: checkout specific commits

--- tests/replay/install_capnp.sh
+++ tests/replay/install_capnp.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 apt-get install -y autoconf curl libtool
 curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
 tar xvf capnproto-c++-0.6.1.tar.gz
@@ -9,6 +11,7 @@ make install
 cd ..
 git clone https://github.com/commaai/c-capnproto.git
 cd c-capnproto
+git checkout 2e625acacf58a5f5c8828d8453d1f8dacc700a96
 git submodule update --init --recursive
 autoreconf -f -i -s
 CFLAGS="-fPIC" ./configure --prefix=/usr/local
