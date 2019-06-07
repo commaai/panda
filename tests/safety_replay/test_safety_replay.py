55f3990b6390dbd85bef5c0b4361d4f3ddd27af8
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2019-06-07 16:31:21 -0700

    change logs for safety replay test

--- tests/safety_replay/test_safety_replay.py
+++ tests/safety_replay/test_safety_replay.py
@@ -10,13 +10,13 @@ BASE_URL = "https://commadataci.blob.core.windows.net/openpilotci/"
 
 # (route, safety mode, param)
 logs = [
-  ("09f75413d2466119|2019-05-31--07-34-50.bz2", "HONDA", 0), # HONDA.CIVIC
-  ("095d5469458829d8|2019-05-30--16-29-42.bz2", "TOYOTA", 66), # TOYOTA.PRIUS
-  ("f8a6dd2a138b15ad|2019-06-01--11-40-53.bz2", "GM", 0), # GM.VOLT
-  ("f1b4c567731f4a1b|2018-06-06--14-43-46.bz2", "HONDA_BOSCH", 1), # HONDA.ACCORD
-  ("10626f7208cd14d4|2019-05-31--11-45-23.bz2", "HYUNDAI", 0), # HYUNDAI.SANTA_FE
-  ("c987eca8a79b421f|2019-05-08--16-50-04.bz2", "CHRYSLER", 0), # CHRYSLER.PACIFICA_2018_HYBRID
-  ("791340bc01ed993d|2019-05-02--10-14-09.bz2", "SUBARU", 0), # SUBARU.IMPREZA
+  ("b0c9d2329ad1606b|2019-05-30--20-23-57.bz2", "HONDA", 0), # HONDA.CIVIC
+  ("38bfd238edecbcd7|2019-06-07--10-15-25.bz2", "TOYOTA", 66), # TOYOTA.PRIUS
+  ("f89c604cf653e2bf|2018-09-29--13-46-50.bz2", "GM", 0), # GM.VOLT
+  ("0375fdf7b1ce594d|2019-05-21--20-10-33.bz2", "HONDA_BOSCH", 1), # HONDA.ACCORD
+  ("02ec6bea180a4d36|2019-04-17--11-21-35.bz2", "HYUNDAI", 0), # HYUNDAI.SANTA_FE
+  #("", "CHRYSLER", 0), # CHRYSLER.PACIFICA_2018_HYBRID
+  ("791340bc01ed993d|2019-04-08--10-26-00.bz2", "SUBARU", 0), # SUBARU.IMPREZA
 ]
 
 if __name__ == "__main__":
