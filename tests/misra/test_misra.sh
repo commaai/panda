274f9ce06d1ea453a9944b2262b7511698dd446d
Riccardo <riccardo.biasini@gmail.com>
2019-06-07 15:39:30 -0700

    Misra: separately save the cppcheck output in its own artifact

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -4,5 +4,5 @@ cd cppcheck
 git checkout 1584e6236758d54b0d246d41771d67338e0bd41b
 make -j4
 cd ../../../
-tests/misra/cppcheck/cppcheck --dump board/main.c
-python tests/misra/cppcheck/addons/misra.py board/main.c.dump 2>/tmp/misra/output.txt || true
+tests/misra/cppcheck/cppcheck --dump --enable=all board/main.c 2>/tmp/misra/cppcheck_output.txt || true
+python tests/misra/cppcheck/addons/misra.py board/main.c.dump 2>/tmp/misra/misra_output.txt || true
