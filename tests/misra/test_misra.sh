38f4288f1e3c5e93223e2aefe8c1c327d0f58dbd
Riccardo <riccardo.biasini@gmail.com>
2019-06-10 21:58:01 -0700

    running misra also specifically for safety code

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -4,5 +4,11 @@ cd cppcheck
 git checkout 1584e6236758d54b0d246d41771d67338e0bd41b
 make -j4
 cd ../../../
+
+# whole panda code
 tests/misra/cppcheck/cppcheck --dump --enable=all board/main.c 2>/tmp/misra/cppcheck_output.txt || true
 python tests/misra/cppcheck/addons/misra.py board/main.c.dump 2>/tmp/misra/misra_output.txt || true
+
+# just safety
+tests/misra/cppcheck/cppcheck --dump --enable=all board/safety.h 2>/tmp/misra/cppcheck_safety_output.txt || true
+python tests/misra/cppcheck/addons/misra.py board/safety.h.dump 2>/tmp/misra/misra_safety_output.txt || true
