9806bbc4e2d16a72d3375caacbcb000888a7dae3
Riccardo <riccardo.biasini@gmail.com>
2019-06-10 23:49:34 -0700

    one last 12.1 violation

--- board/safety/safety_hyundai.h
+++ board/safety/safety_hyundai.h
@@ -141,7 +141,7 @@ static int hyundai_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 static int hyundai_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   // forward cam to ccan and viceversa, except lkas cmd
   if (((bus_num == 0) || (bus_num == hyundai_camera_bus)) && hyundai_giraffe_switch_2) {
-    if ((to_fwd->RIR>>21) == 832 && (bus_num == hyundai_camera_bus)) return -1;
+    if (((to_fwd->RIR>>21) == 832) && (bus_num == hyundai_camera_bus)) return -1;
     if (bus_num == 0) return hyundai_camera_bus;
     if (bus_num == hyundai_camera_bus) return 0;
   }
