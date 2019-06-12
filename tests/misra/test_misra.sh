55679532aa3c0b7671d9f5d5e93b15234bc1ed58
Riccardo <riccardo.biasini@gmail.com>
2019-06-12 06:35:47 -0700

    Fix Misra 20.1 violations: Moved safety declarations in its own header and qdded optional input to run misra tests for safety code only.

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -1,13 +1,20 @@
 #!/bin/bash -e
+
+INPUT1=$1
+
+echo $INPUT1
+
 git clone https://github.com/danmar/cppcheck.git || true
 cd cppcheck
 git checkout 1584e6236758d54b0d246d41771d67338e0bd41b
 make -j4
 cd ../../../
 
-# whole panda code
-tests/misra/cppcheck/cppcheck --dump --enable=all board/main.c 2>/tmp/misra/cppcheck_output.txt || true
-python tests/misra/cppcheck/addons/misra.py board/main.c.dump 2>/tmp/misra/misra_output.txt || true
+if [ "$INPUT1" != "safety-only" ]; then
+  # whole panda code
+  tests/misra/cppcheck/cppcheck --dump --enable=all board/main.c 2>/tmp/misra/cppcheck_output.txt || true
+  python tests/misra/cppcheck/addons/misra.py board/main.c.dump 2>/tmp/misra/misra_output.txt || true
+fi
 
 # just safety
 tests/misra/cppcheck/cppcheck --dump --enable=all board/safety.h 2>/tmp/misra/cppcheck_safety_output.txt || true
