37576ef3b4d2f6b39d40175317d07993b5391714
Riccardo <riccardo.biasini@gmail.com>
2019-06-12 13:16:23 -0700

    Allow inline suppress for the cppcheck

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -12,10 +12,10 @@ cd ../../../
 
 if [ "$INPUT1" != "safety-only" ]; then
   # whole panda code
-  tests/misra/cppcheck/cppcheck --dump --enable=all board/main.c 2>/tmp/misra/cppcheck_output.txt || true
+  tests/misra/cppcheck/cppcheck --dump --enable=all --inline-suppr board/main.c 2>/tmp/misra/cppcheck_output.txt || true
   python tests/misra/cppcheck/addons/misra.py board/main.c.dump 2>/tmp/misra/misra_output.txt || true
 fi
 
 # just safety
-tests/misra/cppcheck/cppcheck --dump --enable=all board/safety.h 2>/tmp/misra/cppcheck_safety_output.txt || true
+tests/misra/cppcheck/cppcheck --dump --enable=all --inline-suppr board/safety.h 2>/tmp/misra/cppcheck_safety_output.txt || true
 python tests/misra/cppcheck/addons/misra.py board/safety.h.dump 2>/tmp/misra/misra_safety_output.txt || true
