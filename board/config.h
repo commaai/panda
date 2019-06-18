578ef22d522f39e33e5d4030d944e211c7b13f49
Riccardo <riccardo.biasini@gmail.com>
2019-06-17 18:39:17 -0700

    Misra 12.1 violation in MIN/MAX macros

--- board/config.h
+++ board/config.h
@@ -27,12 +27,12 @@
 #define MIN(a,b) \
  ({ __typeof__ (a) _a = (a); \
      __typeof__ (b) _b = (b); \
-   _a < _b ? _a : _b; })
+   (_a < _b) ? _a : _b; })
 
 #define MAX(a,b) \
  ({ __typeof__ (a) _a = (a); \
      __typeof__ (b) _b = (b); \
-   _a > _b ? _a : _b; })
+   (_a > _b) ? _a : _b; })
 
 #define MAX_RESP_LEN 0x40
 
