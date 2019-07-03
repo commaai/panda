6973c2a81aaa8888b4b017db6ac0cff9f84f83d9
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2019-07-02 18:08:17 -0700

    fix wifi tests

--- run_automated_tests.sh
+++ run_automated_tests.sh
@@ -10,5 +10,10 @@ cd boardesp
 make flashall
 cd ..
 
+IFS=$'\n'
+for NAME in $(nmcli --fields NAME con show | grep panda | awk '{$1=$1};1')
+do
+  nmcli connection delete "$NAME"
+done
 
 PYTHONPATH="." python $(which nosetests) -v --with-xunit --xunit-file=./$TEST_FILENAME --xunit-testsuite-name=$TESTSUITE_NAME -s tests/automated/$1*.py
