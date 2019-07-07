38146d677ba9624f9c2103f72f700ff5d86d2fcf
Riccardo <riccardo.biasini@gmail.com>
2019-07-07 14:17:46 -0700

    Fail build test on compiler alerts for pedal

--- tests/build_strict/test_build_strict.sh
+++ tests/build_strict/test_build_strict.sh
@@ -5,11 +5,21 @@ cd ../../board/
 make -f Makefile.strict clean
 make -f Makefile.strict bin 2> compiler_output.txt
 
-
 if [[ -s "compiler_output.txt" ]]
 then
   echo "Found alerts from the compiler:"
   cat compiler_output.txt
   exit 1
 fi
-      
+
+cd pedal/
+
+make clean
+make bin 2> compiler_pedal_output.txt
+
+if [[ -s "compiler_pedal_output.txt" ]]
+then
+  echo "Found alerts from the compiler:"
+  cat compiler_pedal_output.txt
+  exit 1
+fi
