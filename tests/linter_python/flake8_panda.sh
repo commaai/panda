efa664e90443a24156e9e788af156fb2030e16fd
Riccardo <riccardo.biasini@gmail.com>
2019-10-14 23:18:48 -0700

    run flake8 in regression tests

--- tests/linter_python/flake8_panda.sh
+++ tests/linter_python/flake8_panda.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RESULT=$(python3 -m pyflakes $(find ../../ -type f | grep -v "/boardesp/" | grep -v "/cppcheck/" | grep "\.py$"))
+RESULT=$(python3 -m flake8 --select=F $(find ../../ -type f | grep -v "/boardesp/" | grep -v "/cppcheck/" | grep "\.py$"))
 if [[ $RESULT  ]]; then
 	echo "Pyflakes found errors in the code. Please fix and try again"
 	echo "$RESULT"
