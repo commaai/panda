569cc783366ced9a4ce80a76b62879dc35ddee8d
Jay Proulx <jproulx@tmg.io>
2019-10-29 13:27:59 -0400

    #307 Update board/README.md for MacOs instructions and move ./get_sdk_mac.sh pycrypto to --user space

--- board/get_sdk_mac.sh
+++ board/get_sdk_mac.sh
@@ -2,4 +2,5 @@
 # Need formula for gcc
 brew tap ArmMbed/homebrew-formulae
 brew install python dfu-util arm-none-eabi-gcc
-pip2 install libusb1 pycrypto requests
+pip2 install libusb1 requests
+pip2 install --user pycrypto
