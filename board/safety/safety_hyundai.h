d063a188853a5d237ff4b037854a191807e45dc7
Riccardo <riccardo.biasini@gmail.com>
2019-11-13 23:57:07 -0800

    Hyundai safety: re-enable button spam safety check

--- board/safety/safety_hyundai.h
+++ board/safety/safety_hyundai.h
@@ -115,12 +115,11 @@ static int hyundai_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   // FORCE CANCEL: safety check only relevant when spamming the cancel button.
   // ensuring that only the cancel button press is sent (VAL 4) when controls are off.
   // This avoids unintended engagements while still allowing resume spam
-  // TODO: fix bug preventing the button msg to be fwd'd on bus 2
-  //if ((addr == 1265) && !controls_allowed && (bus == 0) {
-  //  if ((GET_BYTES_04(to_send) & 0x7) != 4) {
-  //    tx = 0;
-  //  }
-  //}
+  if ((addr == 1265) && !controls_allowed) {
+    if ((GET_BYTES_04(to_send) & 0x7) != 4) {
+      tx = 0;
+    }
+  }
 
   // 1 allows the message through
   return tx;
