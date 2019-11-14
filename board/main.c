f9ae2b842dbcc3726f64b748ebef554bc321a18c
Riccardo <riccardo.biasini@gmail.com>
2019-11-13 19:39:56 -0800

    skip tx_hook if a message is forwarded

--- board/main.c
+++ board/main.c
@@ -233,7 +233,7 @@ void usb_cb_ep3_out(void *usbdata, int len, bool hardwired) {
     to_push.RIR = d32[dpkt];
 
     uint8_t bus_number = (to_push.RDTR >> 4) & CAN_BUS_NUM_MASK;
-    can_send(&to_push, bus_number);
+    can_send(&to_push, bus_number, false);
   }
 }
 
