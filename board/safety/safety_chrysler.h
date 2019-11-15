b832b08c7aa04ff1aa949b00cb2fb061fb0aecf5
Riccardo <riccardo.biasini@gmail.com>
2019-11-14 18:51:52 -0800

    relay malfunction handling. WIP

--- board/safety/safety_chrysler.h
+++ board/safety/safety_chrysler.h
@@ -5,7 +5,6 @@ const int CHRYSLER_MAX_RATE_UP = 3;
 const int CHRYSLER_MAX_RATE_DOWN = 3;
 const int CHRYSLER_MAX_TORQUE_ERROR = 80;    // max torque cmd in excess of torque motor
 
-bool chrysler_camera_detected = 0;             // is giraffe switch 2 high?
 int chrysler_rt_torque_last = 0;
 int chrysler_desired_torque_last = 0;
 int chrysler_cruise_engaged_last = 0;
@@ -36,10 +35,9 @@ static void chrysler_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     chrysler_cruise_engaged_last = cruise_engaged;
   }
 
-  // check if stock camera ECU is still online
+  // check if stock camera ECU is on bus 0
   if ((bus == 0) && (addr == 0x292)) {
-    chrysler_camera_detected = 1;
-    controls_allowed = 0;
+    relay_malfunction = 1;
   }
 }
 
@@ -48,8 +46,7 @@ static int chrysler_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   int addr = GET_ADDR(to_send);
   int tx = 1;
 
-  // If camera is on bus 0, then nothing can be sent
-  if (chrysler_camera_detected) {
+  if (relay_malfunction) {
     tx = 0;
   }
 
@@ -108,30 +105,27 @@ static int chrysler_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   return tx;
 }
 
-static void chrysler_init(int16_t param) {
-  UNUSED(param);
-  controls_allowed = 0;
-  chrysler_camera_detected = 0;
-}
-
 static int chrysler_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
 
   int bus_fwd = -1;
   int addr = GET_ADDR(to_fwd);
-  // forward CAN 0 -> 2 so stock LKAS camera sees messages
-  if ((bus_num == 0) && !chrysler_camera_detected) {
-    bus_fwd = 2;
-  }
-  // forward all messages from camera except LKAS_COMMAND and LKAS_HUD
-  if ((bus_num == 2) && !chrysler_camera_detected && (addr != 658) && (addr != 678)) {
-    bus_fwd = 0;
+
+  if (!relay_malfunction) {
+    // forward CAN 0 -> 2 so stock LKAS camera sees messages
+    if (bus_num == 0) {
+      bus_fwd = 2;
+    }
+    // forward all messages from camera except LKAS_COMMAND and LKAS_HUD
+    if ((bus_num == 2) && (addr != 658) && (addr != 678)) {
+      bus_fwd = 0;
+    }
   }
   return bus_fwd;
 }
 
 
 const safety_hooks chrysler_hooks = {
-  .init = chrysler_init,
+  .init = nooutput_init,
   .rx = chrysler_rx_hook,
   .tx = chrysler_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
