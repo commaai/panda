b85449b9156bd6266fda655bd7cfa5edf8dd1f35
Riccardo <riccardo.biasini@gmail.com>
2019-11-15 21:24:33 -0800

    Honda too is now whitelisted

--- board/safety/safety_honda.h
+++ board/safety/safety_honda.h
@@ -6,8 +6,9 @@
 //      accel rising edge
 //      brake rising edge
 //      brake > 0mph
-
-//const struct AddrBus HONDA_TX_MSGS[] = {{0xE4, 0}, {0x194, 0}, {0x1FA, 0}, {0x200, 0}, {0x296, 0}, {0x30C, 0}, {0x33D, 0}, {0x39F, 0}};
+const struct AddrBus HONDA_N_TX_MSGS[] = {{0xE4, 0}, {0x194, 0}, {0x1FA, 0}, {0x200, 0}, {0x30C, 0}, {0x33D, 0}, {0x39F, 0}};
+const struct AddrBus HONDA_BH_TX_MSGS[] = {{0xE4, 0}, {0x296, 1}, {0x33D, 0}};  // Bosch Harness
+const struct AddrBus HONDA_BG_TX_MSGS[] = {{0xE4, 2}, {0x296, 0}, {0x33D, 2}};  // Bosch Giraffe
 const int HONDA_GAS_INTERCEPTOR_THRESHOLD = 328;  // ratio between offset and gain from dbc file
 int honda_brake = 0;
 int honda_gas_prev = 0;
@@ -123,9 +124,17 @@ static int honda_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   int addr = GET_ADDR(to_send);
   int bus = GET_BUS(to_send);
 
-  //if (!addr_allowed(addr, bus, HONDA_TX_MSGS, sizeof(HONDA_TX_MSGS) / sizeof(HONDA_TX_MSGS[0]))) {
-  //  tx = 0;
-  //}
+  if (honda_bosch_hardware) {
+    if (board_has_relay() && !addr_allowed(addr, bus, HONDA_BH_TX_MSGS, sizeof(HONDA_BH_TX_MSGS)/sizeof(HONDA_BH_TX_MSGS[0]))) {
+      tx = 0;
+    }
+    if (!board_has_relay() && !addr_allowed(addr, bus, HONDA_BG_TX_MSGS, sizeof(HONDA_BG_TX_MSGS)/sizeof(HONDA_BG_TX_MSGS[0]))) {
+      tx = 0;
+    }
+  }
+  if (!honda_bosch_hardware && !addr_allowed(addr, bus, HONDA_N_TX_MSGS, sizeof(HONDA_N_TX_MSGS)/sizeof(HONDA_N_TX_MSGS[0]))) {
+    tx = 0;
+  }
 
   if (relay_malfunction) {
     tx = 0;
--- tests/safety/test_honda_bosch.py
+++ tests/safety/test_honda_bosch.py
@@ -2,9 +2,13 @@
 import unittest
 from panda import Panda
 from panda.tests.safety import libpandasafety_py
-from panda.tests.safety.common import make_msg
+from panda.tests.safety.common import make_msg, test_spam_can_buses
 
 MAX_BRAKE = 255
+
+H_TX_MSGS = [[0xE4, 0], [0x296, 1], [0x33D, 0]]  # Bosch Harness
+G_TX_MSGS = [[0xE4, 2], [0x296, 0], [0x33D, 2]]  # Bosch Giraffe
+
 
 class TestHondaSafety(unittest.TestCase):
   @classmethod
@@ -13,10 +17,15 @@ class TestHondaSafety(unittest.TestCase):
     cls.safety.set_safety_hooks(Panda.SAFETY_HONDA_BOSCH, 0)
     cls.safety.init_tests_honda()
 
+  def test_spam_can_buses(self):
+    if self.safety.board_has_relay():
+      test_spam_can_buses(self, H_TX_MSGS)
+    else:
+      test_spam_can_buses(self, G_TX_MSGS)
+
   def test_fwd_hook(self):
     buss = range(0x0, 0x3)
     msgs = range(0x1, 0x800)
-    #has_relay = self.safety.get_hw_type() == 3  # black panda
     has_relay = self.safety.board_has_relay()
     bus_rdr_cam = 2 if has_relay else 1
     bus_rdr_car = 0 if has_relay else 2
