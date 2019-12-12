f0a5d154cf0a21021fc3a27b651793b6ab32f81b
Riccardo <riccardo.biasini@gmail.com>
2019-12-12 11:11:18 -0800

    typo

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -206,7 +206,7 @@ static int toyota_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
     if (bus_num == 2) {
       int addr = GET_ADDR(to_fwd);
       // block stock lkas messages and stock acc messages (if OP is doing ACC)
-      // in TSS2, 0.191 is LTA which we need to block to avoid controls collision
+      // in TSS2, 0x191 is LTA which we need to block to avoid controls collision
       int is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191));
       // in TSS2 the camera does ACC as well, so filter 0x343
       int is_acc_msg = (addr == 0x343);
