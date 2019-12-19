a8b02986573e478e5e0cb048ae4b3992109898af
Riccardo <riccardo.biasini@gmail.com>
2019-12-18 17:59:17 -0800

    Temp fix: skip bus check on Honda due to too many variants. Need to think to a clean solution

--- board/safety.h
+++ board/safety.h
@@ -80,7 +80,8 @@ int get_addr_check_index(CAN_FIFOMailBox_TypeDef *to_push, AddrCheckStruct addr_
   int index = -1;
   for (int i = 0; i < len; i++) {
     for (uint8_t j = 0U; j < addr_list[i].addr_len; j++) {
-      if ((addr == addr_list[i].addr[j]) && (bus == addr_list[i].bus)) {
+      // TODO: remove rule to skip bus check if expected bus is negative. See safety_honda.h
+      if ((addr == addr_list[i].addr[j]) && ((bus == addr_list[i].bus) || (addr_list[i].bus < 0)) {
         index = i;
         i = len;  // break also second loop
         break;
