e7c504863943784064c51a120dd6771f9708d288
xx979xx <40252818+xx979xx@users.noreply.github.com>
2020-01-19 14:52:11 +0300

    non-SCC support and fix driver torque signal

--- board/safety/safety_hyundai.h
+++ board/safety/safety_hyundai.h
@@ -19,6 +19,7 @@ int hyundai_desired_torque_last = 0;
 int hyundai_cruise_engaged_last = 0;
 uint32_t hyundai_ts_last = 0;
 struct sample_t hyundai_torque_driver;         // last few driver torques measured
+bool hyundai_has_scc = false;
 
 static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
@@ -29,14 +30,17 @@ static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     int bus = GET_BUS(to_push);
     int addr = GET_ADDR(to_push);
 
-    if (addr == 897) {
-      int torque_driver_new = ((GET_BYTES_04(to_push) >> 11) & 0xfff) - 2048;
+    if (addr == 593) {
+      int torque_driver_new = ((GET_BYTES_04(to_push) & 0x7ff) * 0.79) - 808; // scale down new driver torque signal to match previous one
       // update array of samples
       update_sample(&hyundai_torque_driver, torque_driver_new);
     }
 
     // enter controls on rising edge of ACC, exit controls on ACC off
     if (addr == 1057) {
+      if (!hyundai_has_scc) {
+        hyundai_has_scc = true;
+      }
       // 2 bits: 13-14
       int cruise_engaged = (GET_BYTES_04(to_push) >> 13) & 0x3;
       if (cruise_engaged && !hyundai_cruise_engaged_last) {
@@ -47,7 +51,18 @@ static int hyundai_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
       }
       hyundai_cruise_engaged_last = cruise_engaged;
     }
-
+    // cruise control for car without SCC
+    if ((addr == 871) && (!hyundai_has_scc)) {
+      // first byte
+      int cruise_engaged = (GET_BYTES_04(to_push) & 0xFF);
+      if (cruise_engaged && !hyundai_cruise_engaged_last) {
+        controls_allowed = 1;
+      }
+      if (!cruise_engaged) {
+        controls_allowed = 0;
+      }
+      hyundai_cruise_engaged_last = cruise_engaged;
+    }
     // TODO: check gas pressed
 
     // check if stock camera ECU is on bus 0
