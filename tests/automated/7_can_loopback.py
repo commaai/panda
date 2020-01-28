52090c494073ef4d0b582602f1d3ef9fc60a0fc3
Robbe <robbe.derks@gmail.com>
2020-01-28 14:48:21 -0800

    Fixed latency test influence

--- tests/automated/7_can_loopback.py
+++ tests/automated/7_can_loopback.py
@@ -237,3 +237,6 @@ def test_bulk_write(p):
   # All messages should have been received
   if len(rx) != 3*NUM_MESSAGES_PER_BUS:
     Exception("Did not receive all messages!")
+
+  # Set back to silent mode
+  p.set_safety_mode(Panda.SAFETY_SILENT)
