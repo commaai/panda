fe862cc20a4e880fb5ac0fee57dcf19c5d90be59
Jason Young <jyoung8607@gmail.com>
2020-03-27 17:11:52 -0700

    Unwind community port hax for upstream, bump Panda ver

--- tests/safety/test.c
+++ tests/safety/test.c
@@ -47,10 +47,6 @@ TIM_TypeDef *TIM2 = &timer;
 // from main_declarations.h
 uint8_t hw_type = HW_TYPE_UNKNOWN;
 
-bool board_has_relay(void) {
-  return ((hw_type == HW_TYPE_BLACK_PANDA) || (hw_type == HW_TYPE_UNO));
-}
-
 // from config.h
 #define MIN(a,b)                                \
   ({ __typeof__ (a) _a = (a);                   \
