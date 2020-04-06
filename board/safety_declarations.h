055ea07eea31232734c5a9cd34ce1289afc4b947
George Hotz <geohot@gmail.com>
2020-04-06 13:37:00 -0700

    remove that unsafe flag since it isn't implemented and it's unclear how to

--- board/safety_declarations.h
+++ board/safety_declarations.h
@@ -93,7 +93,6 @@ bool brake_pressed_prev = false;
 // It is always 0 on mainline comma.ai openpilot
 #define UNSAFE_DISABLE_DISENGAGE_ON_GAS 1
 #define UNSAFE_DISABLE_STOCK_AEB 2
-#define UNSAFE_ENABLE_WEAK_STEERING_WHILE_NOT_ENGAGED 4
 #define UNSAFE_RAISE_LONGITUDINAL_LIMITS_TO_ISO_MAX 8
 int unsafe_mode = 0;
 
