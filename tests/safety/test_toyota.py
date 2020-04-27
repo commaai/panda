c5a2d7d62a9d53d025d35631e999f8071b6a33f3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-04-27 13:00:55 -0700

    abstract interceptor tests

--- tests/safety/test_toyota.py
+++ tests/safety/test_toyota.py
@@ -20,16 +20,8 @@ MAX_RT_DELTA = 375
 RT_INTERVAL = 250000
 
 MAX_TORQUE_ERROR = 350
-INTERCEPTOR_THRESHOLD = 845
 
-# Toyota gas gains are the same
-def toyota_interceptor_msg(gas, addr):
-  to_send = make_msg(0, addr, 6)
-  to_send[0].RDLR = ((gas & 0xff) << 8) | ((gas & 0xff00) >> 8) | \
-                    ((gas & 0xff) << 24) | ((gas & 0xff00) << 8)
-  return to_send
-
-class TestToyotaSafety(common.PandaSafetyTest):
+class TestToyotaSafety(common.PandaSafetyTest, common.InterceptorSafetyTest):
 
   TX_MSGS = [[0x283, 0], [0x2E6, 0], [0x2E7, 0], [0x33E, 0], [0x344, 0], [0x365, 0], [0x366, 0], [0x4CB, 0],  # DSU bus 0
              [0x128, 1], [0x141, 1], [0x160, 1], [0x161, 1], [0x470, 1],  # DSU bus 1
@@ -40,6 +32,7 @@ class TestToyotaSafety(common.PandaSafetyTest):
   RELAY_MALFUNCTION_BUS = 0
   FWD_BLACKLISTED_ADDRS = {2: [0x2E4, 0x412, 0x191, 0x343]}
   FWD_BUS_LOOKUP = {0: 2, 2: 0}
+  INTERCEPTOR_THRESHOLD = 845
 
   @classmethod
   def setUp(self):
@@ -83,39 +76,12 @@ class TestToyotaSafety(common.PandaSafetyTest):
     values["CHECKSUM"] = 1
     return self.packer.make_can_msg_panda("PCM_CRUISE", 0, values)
 
-  def test_prev_gas_interceptor(self):
-    self._rx(toyota_interceptor_msg(0x0, 0x201))
-    self.assertFalse(self.safety.get_gas_interceptor_prev())
-    self._rx(toyota_interceptor_msg(0x1000, 0x201))
-    self.assertTrue(self.safety.get_gas_interceptor_prev())
-    self._rx(toyota_interceptor_msg(0x0, 0x201))
-
-  def test_disengage_on_gas_interceptor(self):
-    for g in range(0, 0x1000):
-      self._rx(toyota_interceptor_msg(0, 0x201))
-      self.safety.set_controls_allowed(True)
-      self._rx(toyota_interceptor_msg(g, 0x201))
-      remain_enabled = g <= INTERCEPTOR_THRESHOLD
-      self.assertEqual(remain_enabled, self.safety.get_controls_allowed())
-      self._rx(toyota_interceptor_msg(0, 0x201))
-      self.safety.set_gas_interceptor_detected(False)
-
-  def test_unsafe_mode_no_disengage_on_gas_interceptor(self):
-    self.safety.set_controls_allowed(True)
-    self.safety.set_unsafe_mode(UNSAFE_MODE.DISABLE_DISENGAGE_ON_GAS)
-    for g in range(0, 0x1000):
-      self._rx(toyota_interceptor_msg(g, 0x201))
-      self.assertTrue(self.safety.get_controls_allowed())
-      self._rx(toyota_interceptor_msg(0, 0x201))
-      self.safety.set_gas_interceptor_detected(False)
-    self.safety.set_unsafe_mode(UNSAFE_MODE.DEFAULT)
-
-  def test_allow_engage_with_gas_interceptor_pressed(self):
-    self._rx(toyota_interceptor_msg(0x1000, 0x201))
-    self.safety.set_controls_allowed(1)
-    self._rx(toyota_interceptor_msg(0x1000, 0x201))
-    self.assertTrue(self.safety.get_controls_allowed())
-    self._rx(toyota_interceptor_msg(0, 0x201))
+  # Toyota gas gains are the same
+  def _interceptor_msg(self, gas, addr):
+    to_send = make_msg(0, addr, 6)
+    to_send[0].RDLR = ((gas & 0xff) << 8) | ((gas & 0xff00) >> 8) | \
+                      ((gas & 0xff) << 24) | ((gas & 0xff00) << 8)
+    return to_send
 
   def test_accel_actuation_limits(self):
     limits = ((MIN_ACCEL, MAX_ACCEL, UNSAFE_MODE.DEFAULT),
@@ -218,13 +184,6 @@ class TestToyotaSafety(common.PandaSafetyTest):
     self._rx(self._torque_meas_msg(0))
     self.assertEqual(1, self.safety.get_toyota_torque_meas_max())
     self.assertEqual(-1, self.safety.get_toyota_torque_meas_min())
-
-  def test_gas_interceptor_safety_check(self):
-    self.safety.set_controls_allowed(0)
-    self.assertTrue(self._tx(toyota_interceptor_msg(0, 0x200)))
-    self.assertFalse(self._tx(toyota_interceptor_msg(0x1000, 0x200)))
-    self.safety.set_controls_allowed(1)
-    self.assertTrue(self._tx(toyota_interceptor_msg(0x1000, 0x200)))
 
   def test_rx_hook(self):
     # checksum checks
