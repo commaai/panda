67afde26ca6734813da4f8ceb475324d5de167e4
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-05-16 01:01:32 -0700

    this should work

--- boardesp/get_sdk.sh
+++ boardesp/get_sdk.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-sudo apt-get install make unrar-free autoconf automake libtool gcc g++ gperf \
+sudo apt-get install -y make unrar-free autoconf automake libtool gcc g++ gperf \
     flex bison texinfo gawk ncurses-dev libexpat-dev python-dev python python-serial \
     sed git unzip bash help2man wget bzip2
 # huh?
-sudo apt-get install libtool
-sudo apt-get install libtool-bin
+sudo apt-get install -y libtool
+sudo apt-get install -y libtool-bin
 git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
 cd esp-open-sdk
 git checkout 03f5e898a059451ec5f3de30e7feff30455f7cec
 cp ../python2_make.py .
-python2 python2_make.py 'LD_LIBRARY_PATH="" make STANDALONE=y'
+
+if [ ! -z $DOCKER ]; then
+  python2 python2_make.py 'CT_ALLOW_BUILD_AS_ROOT_SURE=y LD_LIBRARY_PATH="" make STANDALONE=y'
+else
+  python2 python2_make.py 'LD_LIBRARY_PATH="" make STANDALONE=y'
+fi
