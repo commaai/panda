eabd4869d7e7c09d92a54e91408024e31d78c324
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-05-17 17:58:53 -0700

    python3

--- boardesp/get_sdk.sh
+++ boardesp/get_sdk.sh
@@ -9,9 +9,4 @@ git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
 cd esp-open-sdk
 git checkout 03f5e898a059451ec5f3de30e7feff30455f7cec
 cp ../python2_make.py .
-
-if [ ! -z $DOCKER ]; then
-  python2 python2_make.py 'CT_ALLOW_BUILD_AS_ROOT_SURE=y LD_LIBRARY_PATH="" make STANDALONE=y'
-else
-  python2 python2_make.py 'LD_LIBRARY_PATH="" make STANDALONE=y'
-fi
+python2 python2_make.py 'LD_LIBRARY_PATH="" make STANDALONE=y'
