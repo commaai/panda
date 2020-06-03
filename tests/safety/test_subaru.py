64919ade75b5ef09b3a2b91f1ca45e910dc7956f
Martin Lillepuu <martin@mlp.ee>
2020-06-03 23:05:54 +0300

    fix tests

--- tests/safety/test_subaru.py
+++ tests/safety/test_subaru.py
@@ -24,15 +24,15 @@ class TestSubaruSafety(common.PandaSafetyTest):
   cnt_speed = 0
   cnt_brake = 0
 
-  TX_MSGS = [[0x122, 0], [0x221, 0], [0x322, 0]]
+  TX_MSGS = [[0x122, 0], [0x220, 0], [0x221, 0], [0x222, 0], [0x321, 0], [0x322, 0], [0x240, 2], [0x13c, 2]]
   STANDSTILL_THRESHOLD = 20  # 1kph (see dbc file)
   RELAY_MALFUNCTION_ADDR = 0x122
   RELAY_MALFUNCTION_BUS = 0
-  FWD_BLACKLISTED_ADDRS = {2: [0x122, 0x221, 0x322]}
+  FWD_BLACKLISTED_ADDRS = {0: [0x240, 0x13c], 2: [0x122, 0x220, 0x221, 0x222, 0x321, 0x322]}
   FWD_BUS_LOOKUP = {0: 2, 2: 0}
 
   def setUp(self):
-    self.packer = CANPackerPanda("subaru_global_2017")
+    self.packer = CANPackerPanda("subaru_global_2017_generated")
     self.safety = libpandasafety_py.libpandasafety
     self.safety.set_safety_hooks(Panda.SAFETY_SUBARU, 0)
     self.safety.init_tests()
