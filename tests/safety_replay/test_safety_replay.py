086be906d7de453bb76ca4470eadd16ca8968550
Adeeb Shihadeh <8762862+adeebshihadeh@users.noreply.github.com>
2020-06-15 18:15:49 -0700

    Add test routes for Hyundai legacy and Subaru legacy safety modes (#558)
    * add subaru legacy route to safety replay
    
    * hyundai legacy routet push

--- tests/safety_replay/test_safety_replay.py
+++ tests/safety_replay/test_safety_replay.py
@@ -9,8 +9,8 @@ from tools.lib.logreader import LogReader  # pylint: disable=import-error
 
 BASE_URL = "https://commadataci.blob.core.windows.net/openpilotci/"
 
-# (route, safety mode, param)
 logs = [
+  # (route, safety mode, param)
   ("2425568437959f9d|2019-12-22--16-24-37.bz2", Panda.SAFETY_HONDA_NIDEC, 0),  # HONDA.CIVIC (fcw presents: 0x1FA blocked as expected)
   ("38bfd238edecbcd7|2019-06-07--10-15-25.bz2", Panda.SAFETY_TOYOTA, 66),  # TOYOTA.PRIUS
   ("f89c604cf653e2bf|2018-09-29--13-46-50.bz2", Panda.SAFETY_GM, 0),  # GM.VOLT
@@ -21,9 +21,15 @@ logs = [
   ("d12cd943127f267b|2020-03-27--15-57-18.bz2", Panda.SAFETY_VOLKSWAGEN_PQ, 0),  # 2009 VW Passat R36 (B6), supporting OP port not yet upstreamed
   ("fbbfa6af821552b9|2020-03-03--08-09-43.bz2", Panda.SAFETY_NISSAN, 0),  # NISSAN.XTRAIL
   ("5b7c365c50084530_2020-04-15--16-13-24--3--rlog.bz2", Panda.SAFETY_HYUNDAI, 0),  # HYUNDAI.SONATA
+  ("610ebb9faaad6b43|2020-06-13--15-28-36.bz2", Panda.SAFETY_HYUNDAI_LEGACY, 0),  # HYUNDAI.IONIQ_EV_LTD
+  ("5ab784f361e19b78_2020-06-08--16-30-41.bz2", Panda.SAFETY_SUBARU_LEGACY, 0),  # SUBARU.OUTBACK
 ]
 
+
+
 if __name__ == "__main__":
+
+  # get all the routes
   for route, _, _ in logs:
     if not os.path.isfile(route):
       with open(route, "wb") as f:
