91f0f80428637533055ed8c1b2ab07cc3019d002
Martin Lillepuu <martin@mlp.ee>
2020-07-23 09:14:45 +0300

    Change Subaru test to use generated dbc

--- tests/safety/test_subaru.py
+++ tests/safety/test_subaru.py
@@ -32,7 +32,7 @@ class TestSubaruSafety(common.PandaSafetyTest):
   FWD_BUS_LOOKUP = {0: 2, 2: 0}
 
   def setUp(self):
-    self.packer = CANPackerPanda("subaru_global_2017")
+    self.packer = CANPackerPanda("subaru_global_2017_generated")
     self.safety = libpandasafety_py.libpandasafety
     self.safety.set_safety_hooks(Panda.SAFETY_SUBARU, 0)
     self.safety.init_tests()
