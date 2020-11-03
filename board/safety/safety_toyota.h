23e4cfedb7c65670afa8ba2cbb790f55664d07e5
birdman6450 <67883352+birdman6450@users.noreply.github.com>
2020-11-02 16:14:17 -0800

    Update safety_toyota.h
    Tss2 limit is above 15. Allows code to be edited on shanes SA. My indi tune with this is the best and i need to gather 5 routes to submit full tune PR. This is the first step. Does not effect vehicles just allows edits for routes for PR. Improved handing for corolla tss2 tune.

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -3,8 +3,8 @@ const int TOYOTA_MAX_TORQUE = 1500;       // max torque cmd allowed ever
 
 // rate based torque limit + stay within actually applied
 // packet is sent at 100hz, so this limit is 1000/sec
-const int TOYOTA_MAX_RATE_UP = 10;        // ramp up slow
-const int TOYOTA_MAX_RATE_DOWN = 25;      // ramp down fast
+const int TOYOTA_MAX_RATE_UP = 15;        // ramp up slow
+const int TOYOTA_MAX_RATE_DOWN = 44;      // ramp down fast
 const int TOYOTA_MAX_TORQUE_ERROR = 350;  // max torque cmd in excess of torque motor
 
 // real time torque limit to prevent controls spamming
