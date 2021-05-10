705de2f739c9f4cea7125c2524f26869021f87f4
ClockeNessMnstr <locke.dftc@gmail.com>
2021-05-10 00:53:49 -0400

    Debounce Brake signal for single frame errors.

--- board/safety.h
+++ board/safety.h
@@ -208,10 +208,15 @@ void generic_rx_checks(bool stock_ecu_detected) {
   }
   gas_pressed_prev = gas_pressed;
 
-  // exit controls on rising edge of brake press
-  if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
+  // exit controls on rising edge while vehicle is not moving
+  if (brake_pressed && !brake_pressed_prev && !vehicle_moving) { 
     controls_allowed = 0;
-  }
+  } 
+  // exit controls while in motion if brake is pressed.
+  // debounce rising edge
+  if (brake_pressed && brake_pressed_prev && vehicle_moving) {
+	controls_allowed = 0;
+  } 
   brake_pressed_prev = brake_pressed;
 
   // check if stock ECU is on bus broken by car harness
