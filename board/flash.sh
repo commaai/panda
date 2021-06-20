9156caa3705feb3ea0263e0b53dd2fe08cbf8c43
Matthew O'Gorman <mog@rldn.net>
2021-06-19 23:15:04 -0400

    Verify flash succeeded
    I had a few bad panda flashes the other day, not sure what caused it
    thought it might be nice to have the panda verify the flash after flashing

--- board/flash.sh
+++ board/flash.sh
@@ -3,3 +3,4 @@ set -e
 
 scons -u
 PYTHONPATH=.. python3 -c "from python import Panda; Panda().flash('obj/panda.bin.signed')"
+PYTHONPATH=../ python3 -c "from python import Panda; print('flash succeeded') if Panda().get_signature() == Panda().get_signature_from_firmware('obj/panda.bin') else print('flashing failed')"
