1ee6709d0cab32c7ff4046182ecc9c342ec57558
Igor Biletksyy <bs@privacy.im>
2021-07-05 18:25:09 -0700

    Organized as unittest

--- tests/pedal/test_pedal.py
+++ tests/pedal/test_pedal.py
@@ -4,6 +4,7 @@ import contextlib
 import struct
 import signal
 from nose.tools import assert_greater
+import unittest
 
 from panda import Panda
 from panda_jungle import PandaJungle  # pylint: disable=import-error
@@ -60,66 +61,74 @@ def flash_over_can(p, bus, fw_file):
   #p.can_send(0x200, b"\xce\xfa\xad\xde\x1e\x0b\xb0\x02", bus) #DFU mode (recover)
 
   time.sleep(0.1)
-  code = open(fw_file, "rb").read()
-  PandaJungle.flash_static(CanHandle(p, bus), code)
+  with open(fw_file, "rb") as code:
+    PandaJungle.flash_static(CanHandle(p, bus), code.read())
 
+############################# unittest #########################
 
-#################################################################
-############################# MAIN ##############################
-#################################################################
+class TestPedal(unittest.TestCase):
+  def setUp(self):
+    self.jungle = TestPedal.jungle
+    self.pedal_bus = TestPedal.pedal_bus
 
-if __name__ == "__main__":
+  @classmethod
+  def setUpClass(cls):
+    super(TestPedal, cls).setUpClass()
+    with open(os.devnull, "w") as devnull:
+      with contextlib.redirect_stdout(devnull):
+        cls.jungle = PandaJungle()
+    cls.jungle.set_panda_power(True)
+    cls.jungle.set_ignition(False)
+    cls.pedal_bus = -1
 
-  with open(os.devnull, "w") as devnull:
-    with contextlib.redirect_stdout(devnull):
-      jungle = PandaJungle()
-  jungle.set_panda_power(True)
-  jungle.set_ignition(False)
+  def test_1_pedal_bus(self):
+    """
+    Find if pedal is connected and to which bus
+    """
+    self.jungle.can_clear(0xFFFF)
+    rounds = 10
+    while rounds > 0:
+      incoming = self.jungle.can_recv()
+      for message in incoming:
+        address, unused, data, bus = message
+        if address == 0x201:
+          self.pedal_bus = bus
+          break
+      time.sleep(0.1)
+      rounds -= 1
+    self.assertTrue(self.pedal_bus > -1)
+    print(f"Pedal is on bus {self.pedal_bus}")
 
-  # Check on which bus pedal is
-  jungle.can_clear(0xFFFF)
-  rounds = 10
-  pedal_bus = -1
-  while rounds > 0:
-    incoming = jungle.can_recv()
-    for message in incoming:
-      address, unused, data, bus = message
-      if address == 0x201:
-        pedal_bus = bus
-        break
-    time.sleep(0.1)
-    rounds -= 1
-  
-  assert_greater(pedal_bus, -1)
-  print(f"Pedal is on bus {pedal_bus}")
+  def test_2_flash_over_can(self):
+    self.pedal_bus = 1
+    print(self.pedal_bus)
+    flash_over_can(self.jungle, self.pedal_bus, "board/obj/pedal.bin.signed")
+    time.sleep(10)
+    pandas_list = Panda.list()
 
-  # Flash firmware without USB
-  flash_over_can(jungle, pedal_bus, "board/obj/pedal.bin.signed")
-  time.sleep(10)
-  pandas_list = Panda.list()
+    flash_over_can(self.jungle, self.pedal_bus, "board/obj/pedal_usb.bin.signed")
+    time.sleep(10)
+    pedal_uid = (set(Panda.list()) ^ set(pandas_list)).pop()
 
-  # Flash firmware with USB
-  flash_over_can(jungle, pedal_bus, "board/obj/pedal_usb.bin.signed")
-  time.sleep(10)
-  pedal_uid = (set(Panda.list()) ^ set(pandas_list)).pop()
+    p = Panda(pedal_uid)
+    self.assertTrue(p.is_pedal())
+    p.close()
 
-  # Connect over USB and check for hardware type
-  p = Panda(pedal_uid)
-  #assert p.is_pedal()
-  p.close()
+  def test_3_can_spam(self):
+    self.jungle.can_clear(0xFFFF)
+    rounds = 10
+    msgs = 0
+    while rounds > 0:
+      incoming = self.jungle.can_recv()
+      for message in incoming:
+        address, unused, data, bus = message
+        if address == 0x201:
+          msgs += 1
+      time.sleep(0.1)
+      rounds -= 1
+    
+    self.assertTrue(msgs > 40)
+    print(f"Got {msgs} messages")
 
-  # Check if pedal spams CAN bus with messages at address x201
-  jungle.can_clear(0xFFFF)
-  rounds = 10
-  msgs = 0
-  while rounds > 0:
-    incoming = jungle.can_recv()
-    for message in incoming:
-      address, unused, data, bus = message
-      if address == 0x201:
-        msgs += 1
-    time.sleep(0.1)
-    rounds -= 1
-  
-  assert_greater(msgs, 40)
-  print(f"Got {msgs} messages")
+if __name__ == '__main__':
+  unittest.main()
