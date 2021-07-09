ad3a1df05d49d68988c0c063dd3f858d221b6b03
Igor Biletksyy <bs@privacy.im>
2021-07-09 15:52:36 -0700

    safety test

--- tests/safety/test.c
+++ tests/safety/test.c
@@ -27,7 +27,8 @@ struct sample_t torque_meas;
 struct sample_t torque_driver;
 
 TIM_TypeDef timer;
-TIM_TypeDef *MICROSECOND_TIMER = &timer;
+TIM_TypeDef *US_TIMER = &timer;
+#define us_timer_get() US_TIMER->CNT
 
 // from board_declarations.h
 #define HW_TYPE_UNKNOWN 0U
