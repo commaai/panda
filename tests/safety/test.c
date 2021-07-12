7400c722abdd06ae31bef8d674ebc201d6549686
Igor Biletksyy <bs@privacy.im>
2021-07-12 13:49:33 -0700

    Revert "safety test"
    This reverts commit 8c01810902da39f842b8447b649d32e29486ce53.

--- tests/safety/test.c
+++ tests/safety/test.c
@@ -27,8 +27,7 @@ struct sample_t torque_meas;
 struct sample_t torque_driver;
 
 TIM_TypeDef timer;
-TIM_TypeDef *US_TIMER = &timer;
-#define us_timer_get() US_TIMER->CNT
+TIM_TypeDef *MICROSECOND_TIMER = &timer;
 
 // from board_declarations.h
 #define HW_TYPE_UNKNOWN 0U
