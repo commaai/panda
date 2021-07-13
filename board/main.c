de4d1771ad1432c367c539113ac4c3ec4d1e1639
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-07-12 21:02:48 -0700

    only set heartbeat lost when in a car safety mode

--- board/main.c
+++ board/main.c
@@ -142,6 +142,12 @@ void set_safety_mode(uint16_t mode, int16_t param) {
   can_init_all();
 }
 
+bool is_car_safety_mode(uint16_t mode) {
+  return (mode != SAFETY_SILENT) &&
+         (mode != SAFETY_NOOUTPUT) &&
+         (mode != SAFETY_ELM327);
+}
+
 // ***************************** USB port *****************************
 
 int get_health_pkt(void *dat) {
@@ -467,9 +473,7 @@ int usb_cb_control_msg(USB_Setup_TypeDef *setup, uint8_t *resp, bool hardwired) 
     // **** 0xdf: set unsafe mode
     case 0xdf:
       // you can only set this if you are in a non car safety mode
-      if ((current_safety_mode == SAFETY_SILENT) ||
-          (current_safety_mode == SAFETY_NOOUTPUT) ||
-          (current_safety_mode == SAFETY_ELM327)) {
+      if (!is_car_safety_mode(current_safety_mode)) {
         unsafe_mode = setup->b.wValue.w;
       }
       break;
@@ -647,7 +651,7 @@ void tick_handler(void) {
     current_board->set_siren((loop_counter & 1U) && (siren_enabled || (siren_countdown > 0U)));
 
     // decimated to 1Hz
-    if(loop_counter == 0U){
+    if (loop_counter == 0U) {
       can_live = pending_can_live;
 
       current_board->usb_power_mode_tick(uptime_cnt);
@@ -695,15 +699,17 @@ void tick_handler(void) {
             siren_countdown = 5U;
           }
 
+          // set flag to indicate the heartbeat was lost
+          if (is_car_safety_mode(current_safety_mode)) {
+            heartbeat_lost = true;
+          }
+
           if (current_safety_mode != SAFETY_SILENT) {
             set_safety_mode(SAFETY_SILENT, 0U);
           }
           if (power_save_status != POWER_SAVE_STATUS_ENABLED) {
             set_power_save_state(POWER_SAVE_STATUS_ENABLED);
           }
-
-          // set flag to indicate the heartbeat was lost
-          heartbeat_lost = true;
 
           // Also disable IR when the heartbeat goes missing
           current_board->set_ir_power(0U);
@@ -728,7 +734,7 @@ void tick_handler(void) {
       // set ignition_can to false after 2s of no CAN seen
       if (ignition_can_cnt > 2U) {
         ignition_can = false;
-      };
+      }
 
       // on to the next one
       uptime_cnt += 1U;
