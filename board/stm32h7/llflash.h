8f0ecaa4b2d7caa878a5a858113f38f87627aac5
Igor Biletksyy <bs@privacy.im>
2021-07-14 15:17:40 -0700

    temporarily disable flasher

--- board/stm32h7/llflash.h
+++ board/stm32h7/llflash.h
@@ -1,27 +1,21 @@
 bool flash_is_locked(void) {
-  return (FLASH->CR & FLASH_CR_LOCK);
+  //return (FLASH->CR & FLASH_CR_LOCK);
+  return false;
 }
 
 void flash_unlock(void) {
-  FLASH->KEYR = 0x45670123;
-  FLASH->KEYR = 0xCDEF89AB;
-  FLASH->CR |= FLASH_CR_PG;
+  //FLASH->KEYR = 0x45670123;
+  //FLASH->KEYR = 0xCDEF89AB;
+  //FLASH->CR |= FLASH_CR_PG;
 }
 
 bool flash_erase_sector(uint8_t sector, bool unlocked) {
-  // don't erase the bootloader(sector 0)
-  if (sector != 0 && sector < 12 && unlocked) {
-    FLASH->CR = (sector << 3) | FLASH_CR_SER;
-    FLASH->CR |= FLASH_CR_STRT;
-    while (FLASH->SR & FLASH_SR_BSY);
-    return true;
-  }
+  UNUSED(sector);
+  UNUSED(unlocked);
   return false;
 }
 
 void flash_write_word(void *prog_ptr, uint32_t data) {
-  uint32_t *pp = prog_ptr;
-  FLASH->CR = FLASH_CR_PSIZE_1 | FLASH_CR_PG;
-  *pp = data;
-  while (FLASH->SR & FLASH_SR_BSY);
+  UNUSED(prog_ptr);
+  UNUSED(data);
 }
