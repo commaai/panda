a019b9bf4a683c78ac84ea52faf44b0571d20e68
Robbe Derks <robbe.derks@gmail.com>
2021-07-14 16:51:48 +0200

    move tesla back to DEBUG with the other dashcam-only brands

--- board/safety.h
+++ board/safety.h
@@ -250,8 +250,8 @@ const safety_hook_config safety_hook_registry[] = {
   {SAFETY_NISSAN, &nissan_hooks},
   {SAFETY_NOOUTPUT, &nooutput_hooks},
   {SAFETY_HYUNDAI_LEGACY, &hyundai_legacy_hooks},
-  {SAFETY_TESLA, &tesla_hooks},
 #ifdef ALLOW_DEBUG
+  {SAFETY_TESLA, &tesla_hooks},
   {SAFETY_MAZDA, &mazda_hooks},
   {SAFETY_SUBARU_LEGACY, &subaru_legacy_hooks},
   {SAFETY_VOLKSWAGEN_PQ, &volkswagen_pq_hooks},
