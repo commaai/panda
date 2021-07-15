6e503ea53e3ba39b2c1f2bcc6673dcbad0d5595d
Igor Biletksyy <bs@privacy.im>
2021-07-14 19:17:20 -0700

    fix llflash

--- board/stm32h7/llflash.h
+++ board/stm32h7/llflash.h
@@ -5,14 +5,14 @@ bool flash_is_locked(void) {
 void flash_unlock(void) {
   FLASH->KEYR1 = 0x45670123;
   FLASH->KEYR1 = 0xCDEF89AB;
-  FLASH->CR1 |= FLASH_CR1_PG;
+  FLASH->CR1 |= FLASH_CR_PG;
 }
 
 bool flash_erase_sector(uint8_t sector, bool unlocked) {
   // don't erase the bootloader(sector 0)
-  if (sector != 0 && sector < 12 && unlocked) {
-    FLASH->CR1 = (sector << 3) | FLASH_CR_SER;
-    FLASH->CR1 |= FLASH_CR_STRT;
+  if (sector != 0 && sector < 8 && unlocked) {
+    FLASH->CR1 = (sector << 8) | FLASH_CR_SER;
+    FLASH->CR1 |= FLASH_CR_START;
     while (FLASH->SR1 & FLASH_SR_BSY);
     return true;
   }
