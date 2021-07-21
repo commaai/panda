2032afe14e267cb30cf894486f8ada7c89b7c89f
Igor Biletksyy <bs@privacy.im>
2021-07-21 13:12:25 -0700

    typo...

--- tests/automated/1_program.py
+++ tests/automated/1_program.py
@@ -13,13 +13,13 @@ def aaaa_reset_before_tests():
 
 @test_all_pandas
 @panda_connect_and_init
-def test_recover_gen3(p):
+def test_recover(p):
   assert p.recover(timeout=30)
 
 
 @test_all_pandas
 @panda_connect_and_init
-def test_flash_gen3(p):
+def test_flash(p):
   p.flash()
 
 
