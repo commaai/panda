5b4f1852c1f42f32d2ab7e9fe27bdd60661e53c7
Igor Biletksyy <bs@privacy.im>
2021-07-22 14:28:36 -0700

    todo: sanity check for speed setting

--- board/main.c
+++ board/main.c
@@ -470,6 +470,7 @@ int usb_cb_control_msg(USB_Setup_TypeDef *setup, uint8_t *resp, bool hardwired) 
     // **** 0xde: set can bitrate
     case 0xde:
       if (setup->b.wValue.w < BUS_MAX) {
+        // TODO: add sanity check, ideally check if value is correct(from array of correct values)
         can_speed[setup->b.wValue.w] = setup->b.wIndex.w;
         bool ret = can_init(CAN_NUM_FROM_BUS_NUM(setup->b.wValue.w));
         UNUSED(ret);
@@ -626,6 +627,7 @@ int usb_cb_control_msg(USB_Setup_TypeDef *setup, uint8_t *resp, bool hardwired) 
     // **** 0xde: set CAN FD data bitrate
     case 0xf9:
       if (setup->b.wValue.w < BUS_MAX) {
+        // TODO: add sanity check, ideally check if value is correct(from array of correct values)
         can_data_speed[setup->b.wValue.w] = setup->b.wIndex.w;
       }
       break;
