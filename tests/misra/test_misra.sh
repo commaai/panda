c4502b4875a80d882f1f6203c925fa599ec611bf
Igor Biletksyy <bs@privacy.im>
2021-07-22 16:10:04 -0700

    misra test script fix

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -8,12 +8,12 @@ mkdir /tmp/misra || true
 #python tests/misra/cppcheck/addons/misra.py -generate-table > tests/misra/coverage_table
 
 printf "\nPANDA F4 CODE\n"
-/home/batman/cppcheck/cppcheck -DPANDA -DSTM32F4 -UPEDAL -DCAN3 -DUID_BASE \
+cppcheck -DPANDA -DSTM32F4 -UPEDAL -DCAN3 -DUID_BASE \
          --suppressions-list=suppressions.txt --suppress=*:*inc/* \
          -I $PANDA_DIR/board/ --dump --enable=all --inline-suppr --force \
          $PANDA_DIR/board/main.c 2>/tmp/misra/cppcheck_f4_output.txt
 
-python /home/batman/cppcheck/addons/misra.py $PANDA_DIR/board/main.c.dump 2> /tmp/misra/misra_f4_output.txt || true
+python /usr/share/cppcheck/addons/misra.py $PANDA_DIR/board/main.c.dump 2> /tmp/misra/misra_f4_output.txt || true
 
 # strip (information) lines
 cppcheck_f4_output=$( cat /tmp/misra/cppcheck_f4_output.txt | grep -v ": information: " ) || true
@@ -21,12 +21,12 @@ misra_f4_output=$( cat /tmp/misra/misra_f4_output.txt | grep -v ": information: 
 
 
 printf "\nPANDA H7 CODE\n"
-/home/batman/cppcheck/cppcheck -DPANDA -DSTM32H7 -UPEDAL -DUID_BASE \
+cppcheck -DPANDA -DSTM32H7 -UPEDAL -DUID_BASE \
          --suppressions-list=suppressions.txt --suppress=*:*inc/* \
          -I $PANDA_DIR/board/ --dump --enable=all --inline-suppr --force \
          $PANDA_DIR/board/main.c 2>/tmp/misra/cppcheck_h7_output.txt
 
-python /home/batman/cppcheck/addons/misra.py $PANDA_DIR/board/main.c.dump 2> /tmp/misra/misra_h7_output.txt || true
+python /usr/share/cppcheck/addons/misra.py $PANDA_DIR/board/main.c.dump 2> /tmp/misra/misra_h7_output.txt || true
 
 # strip (information) lines
 cppcheck_h7_output=$( cat /tmp/misra/cppcheck_h7_output.txt | grep -v ": information: " ) || true
@@ -34,12 +34,12 @@ misra_h7_output=$( cat /tmp/misra/misra_h7_output.txt | grep -v ": information: 
 
 
 printf "\nPEDAL CODE\n"
-/home/batman/cppcheck/cppcheck -UPANDA -DSTM32F2 -DPEDAL -UCAN3 \
+cppcheck -UPANDA -DSTM32F2 -DPEDAL -UCAN3 \
          --suppressions-list=suppressions.txt --suppress=*:*inc/* \
          -I $PANDA_DIR/board/ --dump --enable=all --inline-suppr --force \
          $PANDA_DIR/board/pedal/main.c 2>/tmp/misra/cppcheck_pedal_output.txt
 
-python /home/batman/cppcheck/addons/misra.py $PANDA_DIR/board/pedal/main.c.dump 2> /tmp/misra/misra_pedal_output.txt || true
+python /usr/share/cppcheck/addons/misra.py $PANDA_DIR/board/pedal/main.c.dump 2> /tmp/misra/misra_pedal_output.txt || true
 
 # strip (information) lines
 cppcheck_pedal_output=$( cat /tmp/misra/cppcheck_pedal_output.txt | grep -v ": information: " ) || true
