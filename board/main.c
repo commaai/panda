1f4e84d678c81cb0d843f7da3d355d8aa839467f
Igor Biletksyy <bs@privacy.im>
2021-07-26 14:15:28 -0700

    reinit fd can on data speed change

--- board/main.c
+++ board/main.c
@@ -629,6 +629,8 @@ int usb_cb_control_msg(USB_Setup_TypeDef *setup, uint8_t *resp, bool hardwired) 
       if (setup->b.wValue.w < CAN_MAX) {
         // TODO: add sanity check, ideally check if value is correct(from array of correct values)
         can_data_speed[setup->b.wValue.w] = setup->b.wIndex.w;
+        bool ret = can_init(CAN_NUM_FROM_BUS_NUM(setup->b.wValue.w));
+        UNUSED(ret);
       }
       break;
     default:
