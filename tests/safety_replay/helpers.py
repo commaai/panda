4fd6d6647a43f09927c7835c1aacca1a3af44b32
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-08-28 18:55:19 -0700

    Revert "Honda Bosch: Support extended LKAS_HUD IDs (#615)"
    This reverts commit 46593af6190a09aea8a1a734e6b3cdcd4227cb98.

--- tests/safety_replay/helpers.py
+++ tests/safety_replay/helpers.py
@@ -12,7 +12,7 @@ def to_signed(d, bits):
 def is_steering_msg(mode, addr):
   ret = False
   if mode in [Panda.SAFETY_HONDA_NIDEC, Panda.SAFETY_HONDA_BOSCH_GIRAFFE, Panda.SAFETY_HONDA_BOSCH_HARNESS]:
-    ret = (addr == 0xE4) or (addr == 0x194) or (addr == 0x33D) or (addr == 0x33DA) or (addr == 0x33DB)
+    ret = (addr == 0xE4) or (addr == 0x194) or (addr == 0x33D)
   elif mode == Panda.SAFETY_TOYOTA:
     ret = addr == 0x2E4
   elif mode == Panda.SAFETY_GM:
