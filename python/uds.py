cb0c7fcf64c167c74e7a97e0800e1b6548c1af86
Greg Hogan <gregjhogan@gmail.com>
2021-09-23 17:33:12 -0700

    add tx timeout

--- python/uds.py
+++ python/uds.py
@@ -4,6 +4,7 @@ import struct
 from collections import deque
 from typing import Callable, NamedTuple, Tuple, List, Deque, Generator, Optional, cast
 from enum import IntEnum
+from functools import partial
 
 class SERVICE_TYPE(IntEnum):
   DIAGNOSTIC_SESSION_CONTROL = 0x10
@@ -507,13 +508,13 @@ def get_rx_addr_for_tx_addr(tx_addr, rx_offset=0x8):
 
 
 class UdsClient():
-  def __init__(self, panda, tx_addr: int, rx_addr: int = None, bus: int = 0, timeout: float = 1, debug: bool = False, response_pending_timeout: float = 10):
+  def __init__(self, panda, tx_addr: int, rx_addr: int = None, bus: int = 0, timeout: float = 1, debug: bool = False, tx_timeout: float = 1, response_pending_timeout: float = 10):
     self.bus = bus
     self.tx_addr = tx_addr
     self.rx_addr = rx_addr if rx_addr is not None else get_rx_addr_for_tx_addr(tx_addr)
     self.timeout = timeout
     self.debug = debug
-    self._can_client = CanClient(panda.can_send, panda.can_recv, self.tx_addr, self.rx_addr, self.bus, debug=self.debug)
+    self._can_client = CanClient(partial(panda.can_send, timeout=int(tx_timeout*1000)), panda.can_recv, self.tx_addr, self.rx_addr, self.bus, debug=self.debug)
     self.response_pending_timeout = response_pending_timeout
 
   # generic uds request
