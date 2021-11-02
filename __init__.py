df7d235cc37547851cac3532664169a2e989fa44
Igor Biletksyy <bs@privacy.im>
2021-11-02 13:57:34 -0700

    ...

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 # pylint: skip-file
 from .python import Panda, PandaWifiStreaming, PandaDFU, flash_release, \
                     BASEDIR, ensure_st_up_to_date, PandaSerial, \
-                    DEFAULT_FW_FN, DEFAULT_H7_FW_FN, MCU_TYPE_H7, MCU_TYPE_F4
+                    DEFAULT_FW_FN, DEFAULT_H7_FW_FN, MCU_TYPE_H7, MCU_TYPE_F4, DLC_TO_LEN, LEN_TO_DLC
 
 from .python.config import BOOTSTUB_ADDRESS, BLOCK_SIZE_FX, APP_ADDRESS_FX, \
                            BLOCK_SIZE_H7, APP_ADDRESS_H7, DEVICE_SERIAL_NUMBER_ADDR_H7, \
