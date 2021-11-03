a47e67a332318d7f0a0ebe743b72e63cdfe0f6e8
Igor Biletksyy <bs@privacy.im>
2021-11-02 17:05:36 -0700

    angry MISRA

--- board/libc.h
+++ board/libc.h
@@ -18,7 +18,7 @@ void *memset(void *str, int c, unsigned int n) {
 void *memcpy(void *dest, const void *src, unsigned int n) {
   const uint32_t *s32 = (const uint32_t *)src;
   uint32_t *d32 = (uint32_t *)dest;
-  for (unsigned int i = (n / 4U); i > 0; i--) {
+  for (unsigned int i = (n / 4U); i > 0U; i--) {
     *d32 = *s32;
     d32++;
     s32++;
@@ -26,7 +26,7 @@ void *memcpy(void *dest, const void *src, unsigned int n) {
 
   const uint8_t *s8 = (const uint8_t *)s32;
   uint8_t *d8 = (uint8_t *)d32;
-  for (unsigned int i = (n & 0x03U); i > 0; i--) {
+  for (unsigned int i = (n & 0x03U); i > 0U; i--) {
     *d8 = *s8;
     d8++;
     s8++;
