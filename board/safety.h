9aed87df7f09b2e2e0a13433de0fa45935d5c353
Igor Biletskyy <briskspirit@users.noreply.github.com>
2021-11-15 16:19:30 -0800

    Add passthrough option to alloutput safety mode (#782)
    * safety_passthrough
    
    * save few lines of code
    
    * ..

--- board/safety.h
+++ board/safety.h
@@ -38,6 +38,7 @@
 #define SAFETY_SUBARU_LEGACY 22U
 #define SAFETY_HYUNDAI_LEGACY 23U
 #define SAFETY_HYUNDAI_COMMUNITY 24U
+#define SAFETY_STELLANTIS 25U
 
 uint16_t current_safety_mode = SAFETY_SILENT;
 int16_t current_safety_param = 0;
