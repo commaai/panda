69db8128b040128f951f7de498a11c8091c0b13c
Jason Young <46612682+jyoung8607@users.noreply.github.com>
2022-01-18 16:27:48 -0500

    VW: Split MQB and PQ into separate safety headers (#830)
    * inline steering torque tx checks
    
    * expand lateral control constants
    
    * rename safety_volkswagen.h -> safety_volkswagen_mqb.h
    
    * add safety_volkswagen_pq.h and migrate
    
    * un-abstract torque and HUD message IDs
    
    * garbage collect now-superfluous comments

--- board/safety.h
+++ board/safety.h
@@ -12,7 +12,8 @@
 #include "safety/safety_subaru.h"
 #include "safety/safety_mazda.h"
 #include "safety/safety_nissan.h"
-#include "safety/safety_volkswagen.h"
+#include "safety/safety_volkswagen_mqb.h"
+#include "safety/safety_volkswagen_pq.h"
 #include "safety/safety_elm327.h"
 
 // from cereal.car.CarParams.SafetyModel
