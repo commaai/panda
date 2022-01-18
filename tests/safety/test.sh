3c85be5d16436952525a18e5e7055830e7de16ce
Jason Young <jyoung8607@gmail.com>
2022-01-18 00:34:01 -0500

    Fix safety test coverage and permissions

--- tests/safety/test.sh
+++ tests/safety/test.sh
@@ -1,19 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# Loop over all hardware types:
-#   HW_TYPE_UNKNOWN 0U
-#   HW_TYPE_WHITE_PANDA 1U
-#   HW_TYPE_GREY_PANDA 2U
-#   HW_TYPE_BLACK_PANDA 3U
-#   HW_TYPE_PEDAL 4U
-#   HW_TYPE_UNO 5U
-
+# Loop over all HW_TYPEs, see board/boards/board_declarations.h
 # Make sure test fails if one HW_TYPE fails
 set -e
 
 scons -u --test
 
-for hw_type in 0 1 2 3 4 5 6; do
+for hw_type in {0..7}; do
   echo "Testing HW_TYPE: $hw_type"
   HW_TYPE=$hw_type python -m unittest discover .
 done
