27aec867bd3dc697ca3d0fcd4364a2722434ecc3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-01-27 22:30:57 -0800

    Honda: remove redundant check

--- board/safety/safety_honda.h
+++ board/safety/safety_honda.h
@@ -228,7 +228,7 @@ static int honda_tx_hook(CANPacket_t *to_send) {
   int pedal_pressed = brake_pressed_prev && vehicle_moving;
   bool unsafe_allow_gas = unsafe_mode & UNSAFE_DISABLE_DISENGAGE_ON_GAS;
   if (!unsafe_allow_gas) {
-    pedal_pressed = pedal_pressed || gas_pressed_prev || (gas_interceptor_prev > HONDA_GAS_INTERCEPTOR_THRESHOLD);
+    pedal_pressed = pedal_pressed || gas_pressed_prev;
   }
   bool current_controls_allowed = controls_allowed && !(pedal_pressed);
   int bus_pt = (honda_hw == HONDA_BOSCH) ? 1 : 0;
