2d96ef02df0e591244988739008e5d162da28cd0
Shane Smiskol <shane@smiskol.com>
2022-02-03 12:30:07 -0800

    GM ignition: remove signal with noise and false positives (#845)

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -29,7 +29,6 @@ extern int can_silent;
 
 // Ignition detected from CAN meessages
 bool ignition_can = false;
-bool ignition_cadillac = false;
 uint32_t ignition_can_cnt = 0U;
 
 #define ALL_CAN_SILENT 0xFF
@@ -197,16 +196,9 @@ void ignition_can_hook(CANPacket_t *to_push) {
 
   if (bus == 0) {
     // GM exception
-    // TODO: verify on all supported GM models that we can reliably detect ignition using only this signal,
-    // since the 0x1F1 signal can briefly go low immediately after ignition
     if ((addr == 0x160) && (len == 5)) {
       // this message isn't all zeros when ignition is on
-      ignition_cadillac = GET_BYTES_04(to_push) != 0U;
-    }
-    if ((addr == 0x1F1) && (len == 8)) {
-      // Bit 5 is ignition "on"
-      bool ignition_gm = ((GET_BYTE(to_push, 0) & 0x20U) != 0U);
-      ignition_can = ignition_gm || ignition_cadillac;
+      ignition_can = GET_BYTES_04(to_push) != 0U;
     }
 
     // Tesla exception
