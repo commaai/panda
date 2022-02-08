740b9199b95eecc89457bbae6c51e27311fdbacd
Igor Biletskyy <bs@privacy.im>
2022-02-08 09:52:46 -0800

    delay 3 sec

--- board/config.h
+++ board/config.h
@@ -7,7 +7,7 @@
 //#define DEBUG_SPI
 //#define DEBUG_FAULTS
 
-#define DEEPSLEEP_WAKEUP_DELAY 5U
+#define DEEPSLEEP_WAKEUP_DELAY 3U
 
 #define NULL ((void*)0)
 #define COMPILE_TIME_ASSERT(pred) ((void)sizeof(char[1 - (2 * ((int)(!(pred))))]))
