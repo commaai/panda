8d54cb76c3cc9330d472e532bce8374f882bf9ac
Igor Biletksyy <bs@privacy.im>
2022-03-05 17:40:35 -0800

    my dear pedal!

--- board/stm32fx/stm32fx_config.h
+++ board/stm32fx/stm32fx_config.h
@@ -67,6 +67,9 @@
 #if !defined(BOOTSTUB) && (defined(PANDA) || defined(PEDAL_USB))
   #include "drivers/uart.h"
   #include "stm32fx/lluart.h"
+#endif
+
+#if !defined(PEDAL_USB) && !defined(PEDAL) && !defined(BOOTSTUB)
   #include "stm32fx/llexti.h"
 #endif
 
