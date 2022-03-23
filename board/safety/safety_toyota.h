4b7706f664371d6bb023ad4fa5797c36a5af1f25
Harald Schafer <harald.the.engineer@gmail.com>
2022-03-22 18:00:17 -0700

    Change MAX_RT_DELTA too

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -8,8 +8,8 @@ const int TOYOTA_MAX_RATE_DOWN = 25;      // ramp down fast
 const int TOYOTA_MAX_TORQUE_ERROR = 350;  // max torque cmd in excess of torque motor
 
 // real time torque limit to prevent controls spamming
-// the real time limit is 1500/sec
-const int TOYOTA_MAX_RT_DELTA = 375;      // max delta torque allowed for real time checks
+// the real time limit is 2000/sec
+const int TOYOTA_MAX_RT_DELTA = 500;      // max delta torque allowed for real time checks
 const uint32_t TOYOTA_RT_INTERVAL = 250000;    // 250ms between real time checks
 
 // longitudinal limits
