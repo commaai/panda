0be5a2c7b43a10a79f625da503a9d15f696e195d
Shane Smiskol <shane@smiskol.com>
2022-03-25 15:59:36 -0700

    clean up

--- tests/safety/test_tesla.py
+++ tests/safety/test_tesla.py
@@ -184,12 +184,10 @@ class TestTeslaLongitudinalSafety(TestTeslaSafety):
           # floats might not hit exact boundary conditions without rounding
           min_accel = round(min_accel, 2)
           max_accel = round(max_accel, 2)
-
           if controls_allowed:
             send = (MIN_ACCEL <= min_accel <= MAX_ACCEL) and (MIN_ACCEL <= max_accel <= MAX_ACCEL)
           else:
             send = np.isclose([min_accel, max_accel], 0, atol=0.0001)
-
           self.assertEqual(send, self._tx(self._long_control_msg(10, acc_val=4, accel_limits=[min_accel, max_accel])))
 
 class TestTeslaChassisLongitudinalSafety(TestTeslaLongitudinalSafety):
