8c31eb6e30ec0ea6d0414f368a9e851ea6dc7130
Shane Smiskol <shane@smiskol.com>
2022-03-26 01:00:29 -0700

    rename related function

--- tests/safety/test_hyundai.py
+++ tests/safety/test_hyundai.py
@@ -285,7 +285,7 @@ class TestHyundaiLongitudinalSafety(TestHyundaiSafety):
     self.__class__.cnt_button += 1
     return self.packer.make_can_msg_panda("CLU11", 0, values)
 
-  def _send_accel_msg(self, accel, aeb_req=False, aeb_decel=0):
+  def _accel_control_msg(self, accel, aeb_req=False, aeb_decel=0):
     values = {
       "aReqRaw": accel,
       "aReqValue": accel,
@@ -311,9 +311,9 @@ class TestHyundaiLongitudinalSafety(TestHyundaiSafety):
     self.assertFalse(self._tx(self._send_fca11_msg(aeb_decel=1.0)))
 
   def test_no_aeb_scc12(self):
-    self.assertTrue(self._tx(self._send_accel_msg(0)))
-    self.assertFalse(self._tx(self._send_accel_msg(0, aeb_req=True)))
-    self.assertFalse(self._tx(self._send_accel_msg(0, aeb_decel=1.0)))
+    self.assertTrue(self._tx(self._accel_control_msg(0)))
+    self.assertFalse(self._tx(self._accel_control_msg(0, aeb_req=True)))
+    self.assertFalse(self._tx(self._accel_control_msg(0, aeb_decel=1.0)))
 
   def test_set_resume_buttons(self):
     """
@@ -341,7 +341,7 @@ class TestHyundaiLongitudinalSafety(TestHyundaiSafety):
         accel = round(accel, 2) # floats might not hit exact boundary conditions without rounding
         self.safety.set_controls_allowed(controls_allowed)
         send = MIN_ACCEL <= accel <= MAX_ACCEL if controls_allowed else accel == 0
-        self.assertEqual(send, self._tx(self._send_accel_msg(accel)), (controls_allowed, accel))
+        self.assertEqual(send, self._tx(self._accel_control_msg(accel)), (controls_allowed, accel))
 
   def test_diagnostics(self):
     tester_present = common.package_can_msg((0x7d0, 0, b"\x02\x3E\x80\x00\x00\x00\x00\x00", 0))
