3c265612894a9d4dd42cbb5e2ed7ee0f4e09a6e9
Shane Smiskol <shane@smiskol.com>
2022-03-28 16:07:12 -0700

    do tesla safety

--- board/safety/safety_tesla.h
+++ board/safety/safety_tesla.h
@@ -118,7 +118,6 @@ static int tesla_rx_hook(CANPacket_t *to_push) {
 
 
 static int tesla_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
-  UNUSED(longitudinal_allowed);
 
   int tx = 1;
   int addr = GET_ADDR(to_send);
@@ -198,7 +197,7 @@ static int tesla_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
       }
 
       // Don't allow longitudinal actuation if controls aren't allowed
-      if (!controls_allowed) {
+      if (!controls_allowed || !longitudinal_allowed) {
         if ((raw_accel_max != TESLA_NO_ACCEL_VALUE) || (raw_accel_min != TESLA_NO_ACCEL_VALUE)) {
           violation = true;
         }
