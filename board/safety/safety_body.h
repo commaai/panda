49a0bf6a4aa56aa34e2f91274f3c6017160352ba
Igor Biletksyy <bs@privacy.im>
2022-03-30 15:54:38 -0700

    safety mode for body

---
+++ board/safety/safety_body.h
@@ -0,0 +1,22 @@
+
+
+static int ford_tx_hook(CANPacket_t *to_send) {
+
+  int tx = 0;
+  int addr = GET_ADDR(to_send);
+
+  if (addr == 0x200U) {
+    tx = 1;
+  }
+
+  // 1 allows the message through
+  return tx;
+}
+
+const safety_hooks ford_hooks = {
+  .init = nooutput_init,
+  .rx = default_rx_hook,
+  .tx = body_tx_hook,
+  .tx_lin = nooutput_tx_lin_hook,
+  .fwd = default_fwd_hook,
+};
