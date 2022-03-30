c1aab995e1753bb4266d4345b2a9ab46e0cd11b1
Shane Smiskol <shane@smiskol.com>
2022-03-30 16:23:42 -0700

    make sure we don't change current_controls_allowed braking behavior

--- board/safety.h
+++ board/safety.h
@@ -63,7 +63,9 @@ int safety_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
 }
 
 bool get_longitudinal_allowed(void) {
-  return controls_allowed && !gas_pressed_prev;
+  int pedal_pressed = brake_pressed_prev && vehicle_moving;
+  pedal_pressed = pedal_pressed || gas_pressed_prev;
+  return controls_allowed && !pedal_pressed;
 }
 
 // Given a CRC-8 poly, generate a static lookup table to use with a fast CRC-8
