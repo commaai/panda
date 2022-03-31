560bcc4063f573e4d5d221235469334a4af14d50
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-03-31 15:58:54 -0700

    body: add RX checks (#903)
    * body: add RX checks
    
    * add that

--- board/safety/safety_body.h
+++ board/safety/safety_body.h
@@ -1,3 +1,16 @@
+AddrCheckStruct body_addr_checks[] = {
+  {.msg = {{0x201, 0, 8, .check_checksum = false, .max_counter = 0U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+};
+#define BODY_ADDR_CHECK_LEN (sizeof(body_addr_checks) / sizeof(body_addr_checks[0]))
+addr_checks body_rx_checks = {body_addr_checks, BODY_ADDR_CHECK_LEN};
+
+static int body_rx_hook(CANPacket_t *to_push) {
+
+  bool valid = addr_safety_check(to_push, &body_rx_checks, NULL, NULL, NULL);
+
+  return valid;
+}
+
 static int body_tx_hook(CANPacket_t *to_send) {
 
   int tx = 0;
@@ -12,13 +25,19 @@ static int body_tx_hook(CANPacket_t *to_send) {
     tx = 1;
   }
 
-  // 1 allows the message through
   return tx;
 }
 
+static const addr_checks* body_init(int16_t param) {
+  UNUSED(param);
+  controls_allowed = false;
+  relay_malfunction_reset();
+  return &body_rx_checks;
+}
+
 const safety_hooks body_hooks = {
-  .init = nooutput_init,
-  .rx = default_rx_hook,
+  .init = body_init,
+  .rx = body_rx_hook,
   .tx = body_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
   .fwd = default_fwd_hook,
--- tests/safety/test.c
+++ tests/safety/test.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "panda.h"
 #include "can_definitions.h"
@@ -72,6 +73,7 @@ void safety_tick_current_rx_checks() {
 
 bool addr_checks_valid() {
   if (current_rx_checks->len <= 0) {
+    printf("missing RX checks\n");
     return false;
   }
 
@@ -79,6 +81,7 @@ bool addr_checks_valid() {
     const AddrCheckStruct addr = current_rx_checks->check[i];
     bool valid = addr.msg_seen && !addr.lagging && addr.valid_checksum && (addr.wrong_counters < MAX_WRONG_COUNTERS);
     if (!valid) {
+      //printf("seen %d lagging %d valid checksum %d wrong counters %d\n", addr.msg_seen, addr.lagging, addr.valid_checksum, addr.wrong_counters);
       return false;
     }
   }
