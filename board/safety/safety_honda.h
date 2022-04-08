fa07424fff645ce9d54216543e97e0e012fb0fae
Shane Smiskol <shane@smiskol.com>
2022-04-08 15:41:37 -0700

    update panda interceptor gains

--- board/safety/safety_honda.h
+++ board/safety/safety_honda.h
@@ -10,13 +10,11 @@ const CanMsg HONDA_N_TX_MSGS[] = {{0xE4, 0, 5}, {0x194, 0, 4}, {0x1FA, 0, 8}, {0
 const CanMsg HONDA_BOSCH_TX_MSGS[] = {{0xE4, 0, 5}, {0xE5, 0, 8}, {0x296, 1, 4}, {0x33D, 0, 5}, {0x33DA, 0, 5}, {0x33DB, 0, 8}};  // Bosch
 const CanMsg HONDA_BOSCH_LONG_TX_MSGS[] = {{0xE4, 1, 5}, {0x1DF, 1, 8}, {0x1EF, 1, 8}, {0x1FA, 1, 8}, {0x30C, 1, 8}, {0x33D, 1, 5}, {0x33DA, 1, 5}, {0x33DB, 1, 8}, {0x39F, 1, 8}, {0x18DAB0F1, 1, 8}};  // Bosch w/ gas and brakes
 
-// Roughly calculated using the offsets in openpilot +5%:
-// In openpilot: ((gas1_norm + gas2_norm)/2) > 15
-// gas_norm1 = ((gain_dbc1*gas1) + offset_dbc)
-// gas_norm2 = ((gain_dbc2*gas2) + offset_dbc)
-// assuming that 2*(gain_dbc1*gas1) == (gain_dbc2*gas2)
-// In this safety: ((gas1 + (gas2/2))/2) > THRESHOLD
-const int HONDA_GAS_INTERCEPTOR_THRESHOLD = 344;
+// panda interceptor threshold needs to be roughly equal to openpilot threshold (1e-5)
+// If thresholds are mismatched, then if gas is in between the openpilot and panda thresholds in pre-enable,
+// then it is possible for panda to see the gas fall and rise, while openpilot believes it's still pressed, causing a controls mismatch
+// Threshold calculated from DBC gains: round(((1e-5 + 83.3) / 0.253984064 + (1e-5 + 83.3) / 0.253984064) / 2) = 328
+const int HONDA_GAS_INTERCEPTOR_THRESHOLD = 328;
 #define HONDA_GET_INTERCEPTOR(msg) (((GET_BYTE((msg), 0) << 8) + GET_BYTE((msg), 1) + ((GET_BYTE((msg), 2) << 8) + GET_BYTE((msg), 3)) / 2U ) / 2U) // avg between 2 tracks
 const int HONDA_BOSCH_NO_GAS_VALUE = -30000; // value sent when not requesting gas
 const int HONDA_BOSCH_GAS_MAX = 2000;
