ee2a4b6b5837bcc118011826d81890ff58708d2a
Shane Smiskol <shane@smiskol.com>
2022-04-21 20:11:13 -0700

    sample_t: fixup comments (#921)

--- board/safety.h
+++ board/safety.h
@@ -320,7 +320,7 @@ int to_signed(int d, int bits) {
   return d_signed;
 }
 
-// given a new sample, update the smaple_t struct
+// given a new sample, update the sample_t struct
 void update_sample(struct sample_t *sample, int sample_new) {
   int sample_size = sizeof(sample->values) / sizeof(sample->values[0]);
   for (int i = sample_size - 1; i > 0; i--) {
