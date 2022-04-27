fca7304c2140962786a4509f29adea56f44d0949
Shane Smiskol <shane@smiskol.com>
2022-04-27 16:30:28 -0700

    safety

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -155,7 +155,7 @@ static int toyota_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
 
     // GAS PEDAL: safety check
     if (addr == 0x200) {
-      if (!longitudinal_allowed || toyota_stock_longitudinal) {
+      if (!longitudinal_allowed) {
         if (GET_BYTE(to_send, 0) || GET_BYTE(to_send, 1)) {
           tx = 0;
         }
