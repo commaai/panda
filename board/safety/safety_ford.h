89c5a5b60d9e0de852d3cb37d9646a7485415ad6
Cameron Clough <cameronjclough@gmail.com>
2022-04-29 19:57:41 +0100

    fix cruise state bug

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -96,7 +96,8 @@ static int ford_rx_hook(CANPacket_t *to_push) {
       brake_pressed = ((GET_BYTE(to_push, 0) & 0x30U) >> 4) == 2U;
 
       // Signal: CcStat_D_Actl
-      bool cruise_engaged = (GET_BYTE(to_push, 1) & 0x07U) == 5U;
+      unsigned int cruise_state = GET_BYTE(to_push, 1) & 0x07U;
+      bool cruise_engaged = cruise_state == 4U || cruise_state == 5U;
 
       // Enter controls on rising edge of stock ACC, exit controls if stock ACC disengages
       if (cruise_engaged && !cruise_engaged_prev) {
