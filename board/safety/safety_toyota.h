f437b61b42892b8ffa802a16643d052267693906
Rick Lan <ricklan@gmail.com>
2022-05-04 12:35:49 +0800

    do not block 0x343 if toyota_stock_longitudinal set to true

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -287,7 +287,7 @@ static int toyota_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
     // in TSS2, 0x191 is LTA which we need to block to avoid controls collision
     int is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191));
     // in TSS2 the camera does ACC as well, so filter 0x343
-    int is_acc_msg = (addr == 0x343);
+    int is_acc_msg = !toyota_stock_longitudinal && (addr == 0x343);
     int block_msg = is_lkas_msg || is_acc_msg;
     if (!block_msg) {
       bus_fwd = 0;
