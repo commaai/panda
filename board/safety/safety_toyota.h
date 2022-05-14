9f8392bf1e275143cbb0cdf414a0609ba16c2691
Shane Smiskol <shane@smiskol.com>
2022-05-14 00:29:18 -0700

    think this is clearer on first glance

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -244,7 +244,7 @@ static int toyota_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
       }
 
       // no torque if controls is not allowed or mismatch with STEER_REQUEST bit
-      if (!(controls_allowed && steer_req) && (desired_torque != 0)) {
+      if ((!controls_allowed || !steer_req) && (desired_torque != 0)) {
         violation = 1;
       }
 
