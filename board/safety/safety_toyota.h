fe5662f197413d1e9112e14234332fe0baccdeb5
Shane Smiskol <shane@smiskol.com>
2022-05-14 00:47:13 -0700

    Update board/safety/safety_toyota.h

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -287,8 +287,8 @@ static int toyota_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
     // in TSS2, 0x191 is LTA which we need to block to avoid controls collision
     int is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191));
     // in TSS2 the camera does ACC as well, so filter 0x343
-    int is_acc_msg = !toyota_stock_longitudinal && (addr == 0x343);
-    int block_msg = is_lkas_msg || is_acc_msg;
+    int is_acc_msg = (addr == 0x343);
+    int block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_long);
     if (!block_msg) {
       bus_fwd = 0;
     }
