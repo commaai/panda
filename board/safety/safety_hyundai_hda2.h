f23a02ddbe9a82b6360be0363296c7d92768993e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-05-15 19:14:50 -0700

    counter + vehicle moving

--- board/safety/safety_hyundai_hda2.h
+++ board/safety/safety_hyundai_hda2.h
@@ -1,4 +1,4 @@
-const int HYUNDAI_HDA2_MAX_STEER = 384;             // like stock
+const int HYUNDAI_HDA2_MAX_STEER = 150;
 const int HYUNDAI_HDA2_MAX_RT_DELTA = 112;          // max delta torque allowed for real time checks
 const uint32_t HYUNDAI_HDA2_RT_INTERVAL = 250000;   // 250ms between real time checks
 const int HYUNDAI_HDA2_MAX_RATE_UP = 3;
@@ -13,23 +13,28 @@ const CanMsg HYUNDAI_HDA2_TX_MSGS[] = {
 };
 
 AddrCheckStruct hyundai_hda2_addr_checks[] = {
-  {.msg = {{53, 1, 32, .check_checksum = false, .max_counter = 0U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
-  {.msg = {{101, 1, 32, .check_checksum = false, .max_counter = 0U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
-  {.msg = {{373, 1, 24, .check_checksum = false, .max_counter = 0U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+  {.msg = {{0x35, 1, 32, .check_checksum = false, .max_counter = 0xffU, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+  {.msg = {{0x65, 1, 32, .check_checksum = false, .max_counter = 0xffU, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+  {.msg = {{0xa0, 1, 24, .check_checksum = false, .max_counter = 0xffU, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+  {.msg = {{0x175, 1, 24, .check_checksum = false, .max_counter = 0xffU, .expected_timestep = 10000U}, { 0 }, { 0 }}},
 };
 #define HYUNDAI_HDA2_ADDR_CHECK_LEN (sizeof(hyundai_hda2_addr_checks) / sizeof(hyundai_hda2_addr_checks[0]))
 
 addr_checks hyundai_hda2_rx_checks = {hyundai_hda2_addr_checks, HYUNDAI_HDA2_ADDR_CHECK_LEN};
 
+static uint8_t hyundai_hda2_get_counter(CANPacket_t *to_push) {
+  return GET_BYTE(to_push, 2);
+}
 
 static int hyundai_hda2_rx_hook(CANPacket_t *to_push) {
 
-  bool valid = addr_safety_check(to_push, &hyundai_hda2_rx_checks, NULL, NULL, NULL);
+  bool valid = addr_safety_check(to_push, &hyundai_hda2_rx_checks,
+                                 NULL, NULL, hyundai_hda2_get_counter);
 
   if (valid && (GET_BUS(to_push) == 1U)) {
     int addr = GET_ADDR(to_push);
 
-    if (addr == 373) {
+    if (addr == 0x175) {
       bool cruise_engaged = GET_BIT(to_push, 68);
 
       if (cruise_engaged && !cruise_engaged_prev) {
@@ -42,12 +47,21 @@ static int hyundai_hda2_rx_hook(CANPacket_t *to_push) {
       cruise_engaged_prev = cruise_engaged;
     }
 
-    if (addr == 53) {
+    if (addr == 0x35) {
       gas_pressed = GET_BYTE(to_push, 5) != 0U;
     }
 
-    if (addr == 101) {
+    if (addr == 0x64) {
       brake_pressed = GET_BIT(to_push, 57) != 0U;
+    }
+
+    if (addr == 0xa0) {
+      int speed = 0;
+      for (int i = 8; i < 15; i+=2) {
+        speed += GET_BYTE(to_push, i) | (GET_BYTE(to_push, i + 1) << 8);
+      }
+      speed = ABS(speed) / 4;
+      vehicle_moving = speed > HYUNDAI_HDA2_STANDSTILL_THRSLD;
     }
 
     generic_rx_checks(addr == 0x50);
