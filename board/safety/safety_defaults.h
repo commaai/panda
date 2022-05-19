4afa552ed81a85f8558f7ac81b08db1b797804f6
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-05-18 23:40:13 -0700

    add back for all output

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -49,6 +49,7 @@ const uint16_t ALLOUTPUT_PARAM_PASSTHROUGH = 1;
 bool alloutput_passthrough = false;
 
 static const addr_checks* alloutput_init(uint16_t param) {
+  controls_allowed = true;
   alloutput_passthrough = GET_FLAG(param, ALLOUTPUT_PARAM_PASSTHROUGH);
   return &default_rx_checks;
 }
