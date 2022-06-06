4af4494b57fd854abf08330fc0b0c5954e8ff176
cmyui <cmyuiosu@gmail.com>
2022-06-05 22:59:35 -0400

    type hints for python/serial.py

--- python/serial.py
+++ python/serial.py
@@ -1,6 +1,8 @@
+from . import Panda
+
 # mimic a python serial port
 class PandaSerial(object):
-  def __init__(self, panda, port, baud):
+  def __init__(self, panda: Panda, port: int, baud: int) -> None:
     self.panda = panda
     self.port = port
     self.panda.set_uart_parity(self.port, 0)
@@ -8,7 +10,7 @@ class PandaSerial(object):
     self.panda.set_uart_baud(self.port, baud)
     self.buf = b""
 
-  def read(self, l=1):  # noqa: E741
+  def read(self, l: int = 1) -> bytes:  # noqa: E741
     tt = self.panda.serial_read(self.port)
     if len(tt) > 0:
       self.buf += tt
@@ -16,20 +18,20 @@ class PandaSerial(object):
     self.buf = self.buf[l:]
     return ret
 
-  def write(self, dat):
+  def write(self, dat: bytes) -> int:
     return self.panda.serial_write(self.port, dat)
 
-  def close(self):
+  def close(self) -> None:
     pass
 
-  def flush(self):
+  def flush(self) -> None:
     pass
 
   @property
-  def baudrate(self):
+  def baudrate(self) -> int:
     return self._baudrate
 
   @baudrate.setter
-  def baudrate(self, value):
+  def baudrate(self, value: int) -> None:
     self.panda.set_uart_baud(self.port, value)
     self._baudrate = value
