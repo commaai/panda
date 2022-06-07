0b25b33e5f79eee4eead1da1fa9bea4d5640436a
Shane Smiskol <shane@smiskol.com>
2022-06-07 13:55:28 -0700

    radarless uses SCM_FEEDBACK too

--- board/safety/safety_honda.h
+++ board/safety/safety_honda.h
@@ -54,6 +54,7 @@ AddrCheckStruct honda_radarless_addr_checks[] = {
   {.msg = {{0x296, 0, 4, .check_checksum = true, .max_counter = 3U, .expected_timestep = 40000U}, { 0 }, { 0 }}},
   {.msg = {{0x158, 0, 8, .check_checksum = true, .max_counter = 3U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
   {.msg = {{0x17C, 0, 8, .check_checksum = true, .max_counter = 3U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+  {.msg = {{0x326, 0, 8, .check_checksum = true, .max_counter = 3U, .expected_timestep = 100000U}, { 0 }, { 0 }}},
 };
 #define HONDA_RADARLESS_ADDR_CHECKS_LEN (sizeof(honda_radarless_addr_checks) / sizeof(honda_radarless_addr_checks[0]))
 
