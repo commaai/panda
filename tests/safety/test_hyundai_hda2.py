1053269e04b71a460d3f344fcb27690ab413b146
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-06-23 13:18:11 -0700

    Update test_hyundai_hda2.py

--- tests/safety/test_hyundai_hda2.py
+++ tests/safety/test_hyundai_hda2.py
@@ -22,7 +22,7 @@ class TestHyundaiHDA2(common.PandaSafetyTest, common.DriverTorqueSteeringSafetyT
   MAX_RT_DELTA = 112
   RT_INTERVAL = 250000
 
-  DRIVER_TORQUE_ALLOWANCE = 50
+  DRIVER_TORQUE_ALLOWANCE = 250
   DRIVER_TORQUE_FACTOR = 2
 
   def setUp(self):
