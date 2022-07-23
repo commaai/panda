38167555c76c962daaa203102de39e3770ab3017
Cameron Clough <cameronjclough@gmail.com>
2022-07-23 11:34:16 +0100

    Maybe this works?

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -94,9 +94,7 @@ static int ford_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
   if (addr == MSG_STEERING_DATA_FD1) {
     // Violation if any button other than cancel is pressed
     // Signal: CcAslButtnCnclPress
-    bool violation = 0U != ((GET_BYTES_04(to_send) & 0xFEFFU) |
-                            GET_BYTE(to_send, 4) |
-                            GET_BYTE(to_send, 5));
+    bool violation = ((GET_BYTES_04(to_send) | GET_BYTES_48(to_send)) & 0xFEFFFFFFU) != 0U;
     if (violation) {
       tx = 0;
     }
