ef67ea509bda61c8caafd8e3f5e8696275839564
Comma Device <device@comma.ai>
2022-07-25 16:10:57 +0200

    big comms refactor, building now

--- board/usb_protocol.h
+++ board/usb_protocol.h
@@ -3,103 +3,100 @@ typedef struct {
   int tail_size;
   uint8_t data[72];
   uint8_t counter;
-} usb_asm_buffer;
+} asm_buffer;
 
-usb_asm_buffer ep1_buffer = {.ptr = 0, .tail_size = 0, .counter = 0};
+asm_buffer can_read_buffer = {.ptr = 0, .tail_size = 0, .counter = 0};
 int total_rx_size = 0;
 
-int usb_cb_ep1_in(void *usbdata, int len) {
-  int pos = 1;
-  uint8_t *usbdata8 = (uint8_t *)usbdata;
-  usbdata8[0] = ep1_buffer.counter;
+int comms_can_read(uint8_t *data, uint32_t max_len) {
+  uint32_t pos = 1;
+  data[0] = can_read_buffer.counter;
   // Send tail of previous message if it is in buffer
-  if (ep1_buffer.ptr > 0) {
-    if (ep1_buffer.ptr <= 63) {
-      (void)memcpy(&usbdata8[pos], ep1_buffer.data, ep1_buffer.ptr);
-      pos += ep1_buffer.ptr;
-      ep1_buffer.ptr = 0;
+  if (can_read_buffer.ptr > 0) {
+    if (can_read_buffer.ptr <= 63) {
+      (void)memcpy(&data[pos], can_read_buffer.data, can_read_buffer.ptr);
+      pos += can_read_buffer.ptr;
+      can_read_buffer.ptr = 0;
     } else {
-      (void)memcpy(&usbdata8[pos], ep1_buffer.data, 63);
-      ep1_buffer.ptr = ep1_buffer.ptr - 63;
-      (void)memcpy(ep1_buffer.data, &ep1_buffer.data[63], ep1_buffer.ptr);
+      (void)memcpy(&data[pos], can_read_buffer.data, 63);
+      can_read_buffer.ptr = can_read_buffer.ptr - 63;
+      (void)memcpy(can_read_buffer.data, &can_read_buffer.data[63], can_read_buffer.ptr);
       pos += 63;
     }
   }
 
   if (total_rx_size > MAX_EP1_CHUNK_PER_BULK_TRANSFER) {
     total_rx_size = 0;
-    ep1_buffer.counter = 0U;
+    can_read_buffer.counter = 0U;
   } else {
     CANPacket_t can_packet;
-    while ((pos < len) && can_pop(&can_rx_q, &can_packet)) {
+    while ((pos < max_len) && can_pop(&can_rx_q, &can_packet)) {
       int pckt_len = CANPACKET_HEAD_SIZE + dlc_to_len[can_packet.data_len_code];
-      if ((pos + pckt_len) <= len) {
-        (void)memcpy(&usbdata8[pos], &can_packet, pckt_len);
+      if ((pos + pckt_len) <= max_len) {
+        (void)memcpy(&data[pos], &can_packet, pckt_len);
         pos += pckt_len;
       } else {
-        (void)memcpy(&usbdata8[pos], &can_packet, len - pos);
-        ep1_buffer.ptr = pckt_len - (len - pos);
-        //(void)memcpy(ep1_buffer.data, ((uint8_t*)&can_packet + (len - pos)), ep1_buffer.ptr);
+        (void)memcpy(&data[pos], &can_packet, max_len - pos);
+        can_read_buffer.ptr = pckt_len - (max_len - pos);
         // cppcheck-suppress objectIndex
-        (void)memcpy(ep1_buffer.data, &((uint8_t*)&can_packet)[(len - pos)], ep1_buffer.ptr);
-        pos = len;
+        (void)memcpy(can_read_buffer.data, &((uint8_t*)&can_packet)[(max_len - pos)], can_read_buffer.ptr);
+        pos = max_len;
       }
     }
-    ep1_buffer.counter++;
+    can_read_buffer.counter++;
     total_rx_size += pos;
   }
-  if (pos != len) {
-    ep1_buffer.counter = 0U;
+  if (pos != max_len) {
+    can_read_buffer.counter = 0U;
     total_rx_size = 0;
   }
   if (pos <= 1) { pos = 0; }
   return pos;
 }
 
-usb_asm_buffer ep3_buffer = {.ptr = 0, .tail_size = 0, .counter = 0};
+asm_buffer can_write_buffer = {.ptr = 0, .tail_size = 0, .counter = 0};
 
 // send on CAN
-void usb_cb_ep3_out(void *usbdata, int len) {
-  uint8_t *usbdata8 = (uint8_t *)usbdata;
+void comms_can_write(uint8_t *data, uint32_t len) {
   // Got first packet from a stream, resetting buffer and counter
-  if (usbdata8[0] == 0U) {
-    ep3_buffer.counter = 0U;
-    ep3_buffer.ptr = 0;
-    ep3_buffer.tail_size = 0;
+  if (data[0] == 0U) {
+    can_write_buffer.counter = 0U;
+    can_write_buffer.ptr = 0;
+    can_write_buffer.tail_size = 0;
   }
   // Assembling can message with data from buffer
-  if (usbdata8[0] == ep3_buffer.counter) {
-    int pos = 1;
-    ep3_buffer.counter++;
-    if (ep3_buffer.ptr != 0) {
-      if (ep3_buffer.tail_size <= 63) {
+  if (data[0] == can_write_buffer.counter) {
+    uint32_t pos = 1;
+    can_write_buffer.counter++;
+    if (can_write_buffer.ptr != 0) {
+      if (can_write_buffer.tail_size <= 63) {
         CANPacket_t to_push;
-        (void)memcpy(&ep3_buffer.data[ep3_buffer.ptr], &usbdata8[pos], ep3_buffer.tail_size);
-        (void)memcpy(&to_push, ep3_buffer.data, ep3_buffer.ptr + ep3_buffer.tail_size);
+        (void)memcpy(&can_write_buffer.data[can_write_buffer.ptr], &data[pos], can_write_buffer.tail_size);
+        (void)memcpy(&to_push, can_write_buffer.data, can_write_buffer.ptr + can_write_buffer.tail_size);
         can_send(&to_push, to_push.bus, false);
-        pos += ep3_buffer.tail_size;
-        ep3_buffer.ptr = 0;
-        ep3_buffer.tail_size = 0;
+        pos += can_write_buffer.tail_size;
+        can_write_buffer.ptr = 0;
+        can_write_buffer.tail_size = 0;
       } else {
-        (void)memcpy(&ep3_buffer.data[ep3_buffer.ptr], &usbdata8[pos], len - pos);
-        ep3_buffer.tail_size -= 63;
-        ep3_buffer.ptr += 63;
+        (void)memcpy(&can_write_buffer.data[can_write_buffer.ptr], &data[pos], len - pos);
+        can_write_buffer.tail_size -= 63;
+        can_write_buffer.ptr += 63;
         pos += 63;
       }
     }
 
     while (pos < len) {
-      int pckt_len = CANPACKET_HEAD_SIZE + dlc_to_len[(usbdata8[pos] >> 4U)];
+      int pckt_len = CANPACKET_HEAD_SIZE + dlc_to_len[(data[pos] >> 4U)];
       if ((pos + pckt_len) <= len) {
         CANPacket_t to_push;
-        (void)memcpy(&to_push, &usbdata8[pos], pckt_len);
+        (void)memcpy(&to_push, &data[pos], pckt_len);
         can_send(&to_push, to_push.bus, false);
         pos += pckt_len;
       } else {
-        (void)memcpy(ep3_buffer.data, &usbdata8[pos], len - pos);
-        ep3_buffer.ptr = len - pos;
-        ep3_buffer.tail_size = pckt_len - ep3_buffer.ptr;
-        pos += ep3_buffer.ptr;
+        (void)memcpy(can_write_buffer.data, &data[pos], len - pos);
+        can_write_buffer.ptr = len - pos;
+        can_write_buffer.tail_size = pckt_len - can_write_buffer.ptr;
+        pos += can_write_buffer.ptr;
       }
     }
   }
