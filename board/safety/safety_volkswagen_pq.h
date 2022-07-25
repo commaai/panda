f11418d3cfded0c4bd6be757ec78fe747de0971b
Jason Young <jyoung8607@gmail.com>
2022-07-25 15:25:49 -0400

    more

--- board/safety/safety_volkswagen_pq.h
+++ board/safety/safety_volkswagen_pq.h
@@ -20,8 +20,8 @@ const int VOLKSWAGEN_PQ_MIN_ACCEL = -3500;              // Max decel 3.5 m/s2
 #define MSG_LDW_1               0x5BE   // TX by OP, Lane line recognition and text alerts
 
 // Transmit of GRA_Neu is allowed on bus 0 and 2 to keep compatibility with gateway and camera integration
-const CanMsg VOLKSWAGEN_PQ_STOCK_TX_MSGS[] = {{MSG_HCA_1, 0, 5}, {MSG_GRA_NEU, 0, 4},
-                                              {MSG_GRA_NEU, 2, 4}, {MSG_LDW_1, 0, 8}};
+const CanMsg VOLKSWAGEN_PQ_STOCK_TX_MSGS[] = {{MSG_HCA_1, 0, 5}, {MSG_LDW_1, 0, 8},
+                                              {MSG_GRA_NEU, 0, 4}, {MSG_GRA_NEU, 2, 4}};
 const CanMsg VOLKSWAGEN_PQ_LONG_TX_MSGS[] =  {{MSG_HCA_1, 0, 5}, {MSG_LDW_1, 0, 8},
                                               {MSG_ACC_SYSTEM, 0, 8}, {MSG_ACC_GRA_ANZIEGE, 0, 8}};
 
