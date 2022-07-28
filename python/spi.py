83cc7657591370a988909fbd420bf2743f7d74f6
Comma Device <device@comma.ai>
2022-07-28 15:24:46 +0200

    add retrying

--- python/spi.py
+++ python/spi.py
@@ -9,6 +9,8 @@ DACK = 0x85
 NACK = 0x1F
 CHECKSUM_START = 0xAB
 
+MAX_RETRY_COUNT = 5
+
 # This mimics the handle given by libusb1 for easy interoperability
 class SpiHandle:
   def __init__(self):
@@ -19,35 +21,40 @@ class SpiHandle:
 
   # helpers
   def _transfer(self, endpoint, data, max_rx_len=1000):
-    packet = struct.pack(">BBHH", SYNC, endpoint, len(data), max_rx_len)
-    packet += bytes([reduce(lambda x, y: x^y, packet) ^ CHECKSUM_START])
-    self.spi.writebytes(packet)
+    for _ in range(MAX_RETRY_COUNT):
+      try:
+        packet = struct.pack(">BBHH", SYNC, endpoint, len(data), max_rx_len)
+        packet += bytes([reduce(lambda x, y: x^y, packet) ^ CHECKSUM_START])
+        self.spi.writebytes(packet)
 
-    dat = b"\x00"
-    while dat[0] not in [HACK, NACK]:
-      dat = self.spi.xfer(b"\x12")
+        dat = b"\x00"
+        while dat[0] not in [HACK, NACK]:
+          dat = self.spi.xfer(b"\x12")
 
-    if dat[0] == NACK:
-      raise Exception("Got NACK response for header")
+        if dat[0] == NACK:
+          raise Exception("Got NACK response for header")
 
-    packet = bytes(data)
-    packet += bytes([reduce(lambda x, y: x^y, packet) ^ CHECKSUM_START])
-    self.spi.xfer(packet)
+        packet = bytes(data)
+        packet += bytes([reduce(lambda x, y: x^y, packet) ^ CHECKSUM_START])
+        self.spi.xfer(packet)
 
-    dat = b"\x00"
-    while dat[0] not in [DACK, NACK]:
-      dat = self.spi.xfer(b"\xab")
+        dat = b"\x00"
+        while dat[0] not in [DACK, NACK]:
+          dat = self.spi.xfer(b"\xab")
 
-    if dat[0] == NACK:
-      raise Exception("Got NACK response for data")
+        if dat[0] == NACK:
+          raise Exception("Got NACK response for data")
 
-    response_len = struct.unpack(">H", bytes(self.spi.xfer(b"\x00" * 2)))[0]
+        response_len = struct.unpack(">H", bytes(self.spi.xfer(b"\x00" * 2)))[0]
 
-    dat = bytes(self.spi.xfer(b"\x00" * (response_len + 1)))
-    # TODO: verify CRC
-    dat = dat[:-1]
+        dat = bytes(self.spi.xfer(b"\x00" * (response_len + 1)))
+        # TODO: verify CRC
+        dat = dat[:-1]
 
-    return dat
+        return dat
+      except:
+        pass
+    raise Exception(f"SPI transaction failed {MAX_RETRY_COUNT} times")
 
   # libusb1 functions
   def close(self):
