317ffc3533d5446e90e48abd41903f684e3d2919
Comma Device <device@comma.ai>
2022-08-01 16:13:05 +0200

    also test long messages!

--- tests/usbprotocol/test_pandalib.py
+++ tests/usbprotocol/test_pandalib.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import random
 import unittest
-from panda import pack_can_buffer, unpack_can_buffer
+from panda import pack_can_buffer, unpack_can_buffer, DLC_TO_LEN
 
 
 class PandaTestPackUnpack(unittest.TestCase):
@@ -9,7 +9,8 @@ class PandaTestPackUnpack(unittest.TestCase):
     to_pack = []
     for _ in range(10000):
       address = random.randint(1, 0x1FFFFFFF)
-      data = bytes([random.getrandbits(8) for _ in range(random.randrange(1, 9))])
+      data = bytes([random.getrandbits(8) for _ in range(DLC_TO_LEN[random.randrange(len(DLC_TO_LEN))])])
+      print(len(data))
       to_pack.append((address, 0, data, 0))
 
     packed = pack_can_buffer(to_pack)
