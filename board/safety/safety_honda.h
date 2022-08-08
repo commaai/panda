6a00fe61f92e478f7d0dd112dee252910245e4ba
royjr <royjr96@gmail.com>
2022-08-07 23:30:22 -0400

    maybe?

--- board/safety/safety_honda.h
+++ board/safety/safety_honda.h
@@ -254,8 +254,10 @@ static int honda_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
   int addr = GET_ADDR(to_send);
   int bus = GET_BUS(to_send);
 
-  if ((honda_hw == HONDA_BOSCH) && honda_bosch_radarless) {
+  if ((honda_hw == HONDA_BOSCH) && honda_bosch_radarless && !honda_bosch_long) {
     tx = msg_allowed(to_send, HONDA_RADARLESS_TX_MSGS, sizeof(HONDA_RADARLESS_TX_MSGS)/sizeof(HONDA_RADARLESS_TX_MSGS[0]));
+  } else if ((honda_hw == HONDA_BOSCH) && honda_bosch_radarless && honda_bosch_long) {
+    tx = msg_allowed(to_send, HONDA_RADARLESS_LONG_TX_MSGS, sizeof(HONDA_RADARLESS_LONG_TX_MSGS)/sizeof(HONDA_RADARLESS_LONG_TX_MSGS[0]));
   } else if ((honda_hw == HONDA_BOSCH) && !honda_bosch_long) {
     tx = msg_allowed(to_send, HONDA_BOSCH_TX_MSGS, sizeof(HONDA_BOSCH_TX_MSGS)/sizeof(HONDA_BOSCH_TX_MSGS[0]));
   } else if ((honda_hw == HONDA_BOSCH) && honda_bosch_long) {
@@ -441,9 +443,15 @@ static int honda_bosch_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
   if (bus_num == 2)  {
     int addr = GET_ADDR(to_fwd);
     int is_lkas_msg = (addr == 0xE4) || (addr == 0xE5) || (addr == 0x33D) || (addr == 0x33DA) || (addr == 0x33DB);
-    int is_acc_msg = honda_bosch_radarless && (addr == 0x1C8);
-    if (!is_lkas_msg && !is_acc_msg) {
-      bus_fwd = 0;
+    int is_acc_msg = (addr == 0x1C8);
+    if (honda_bosch_radarless && honda_bosch_long) {
+      if (!is_lkas_msg && !is_acc_msg) {
+        bus_fwd = 0;
+      }
+    } else {
+      if (!is_lkas_msg) {
+        bus_fwd = 0;
+      }
     }
   }
 
