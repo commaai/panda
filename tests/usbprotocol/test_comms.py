8e4d987797bc71b9a4107453a16b74bcdc331fd7
Robbe Derks <robbe.derks@gmail.com>
2022-08-08 21:26:26 +0200

    can send test

--- tests/usbprotocol/test_comms.py
+++ tests/usbprotocol/test_comms.py
@@ -1,6 +1,7 @@
+import random
 import unittest
 
-from panda import LEN_TO_DLC, DLC_TO_LEN
+from panda import LEN_TO_DLC, DLC_TO_LEN, pack_can_buffer
 from panda.tests.usbprotocol.libpandaprotocol_py import ffi, libpandaprotocol as lpp
 
 TX_QUEUES = (lpp.tx1_q, lpp.tx2_q, lpp.tx3_q, lpp.txgmlan_q)
@@ -32,5 +33,31 @@ class PandaCommsTest(unittest.TestCase):
       assert lpp.can_pop(TX_QUEUES[bus], can_pkt_rx), "CAN pop failed"
 
       assert unpackage_can_msg(can_pkt_rx) == message
+
+  def test_can_send(self):
+    for bus in range(3):
+      for _ in range(100):
+        msgs = []
+        for _ in range(200):
+          address = random.randint(1, 0x1FFFFFFF)
+          data = bytes([random.getrandbits(8) for _ in range(DLC_TO_LEN[random.randrange(0, len(DLC_TO_LEN))])])
+          msgs.append((address, None, data, bus))
+
+        packed = pack_can_buffer(msgs)
+
+        # Simulate USB bulk chunks
+        CHUNK_SIZE = 0x40
+        for buf in packed:
+          for i in range(0, len(buf), CHUNK_SIZE):
+            chunk_len = min(CHUNK_SIZE, len(buf) - i)
+            lpp.comms_can_write(buf[i:i+chunk_len], chunk_len)
+
+        # Check that they ended up in the right buffers
+        queue_msgs = []
+        pkt = ffi.new('CANPacket_t *')
+        while lpp.can_pop(TX_QUEUES[bus], pkt):
+          queue_msgs.append(unpackage_can_msg(pkt))
+
+        assert len(queue_msgs) == len(msgs), f"{len(msgs)} {len(queue_msgs)}"
 
 
