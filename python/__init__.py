07136b9cbeb0d9148b0ec25a3ee6dad5c70a28ae
Shane Smiskol <shane@smiskol.com>
2022-08-09 11:22:03 -0700

    Revert "Add VOACC safetyparam to init.py"
    This reverts commit 2e46f8746f31c399823d9fae9fb9e77e6fee6eea.

--- python/__init__.py
+++ python/__init__.py
@@ -205,7 +205,6 @@ class Panda:
   FLAG_SUBARU_GEN2 = 1
 
   FLAG_GM_HW_CAM = 1
-  FLAG_GM_HW_CAM_VOACC = 2
 
   def __init__(self, serial: Optional[str] = None, claim: bool = True):
     self._serial = serial
