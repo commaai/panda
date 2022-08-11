3dfbe3972f5d87bc80d9f0dd1019efa8d654547f
Comma Device <device@comma.ai>
2022-08-11 13:33:02 +0000

    this seems stable

--- board/drivers/fan.h
+++ board/drivers/fan.h
@@ -1,18 +1,30 @@
 uint16_t fan_tach_counter = 0U;
 uint16_t fan_rpm = 0U;
-uint16_t fan_rpm_fast = 0U;
+uint16_t fan_target_rpm = 0U;
+
+#define FAN_MAX_RPM 6500U
+
+#define FAN_I 0.001f
+float fan_error_integral = 0.0f;
 
 void fan_set_power(uint8_t percentage){
-  pwm_set(TIM3, 3, percentage);
+  percentage = MIN(100U, MAX(0U, percentage));
+  fan_target_rpm = ((FAN_MAX_RPM * percentage) / 100U);
 }
 
 // Call this at 8Hz
 void fan_tick(void){
   // 4 interrupts per rotation
-  fan_rpm_fast = fan_tach_counter * 15U * 8U;
-  puth(fan_rpm_fast); puts("\n");
+  uint16_t fan_rpm_fast = fan_tach_counter * (60U * 8U / 4U);
   fan_tach_counter = 0U;
-
-  // Calculate low-pass filtered version
   fan_rpm = (fan_rpm_fast + (3U * fan_rpm)) / 4U;
+
+  // Update controller
+  float feedforward = (fan_target_rpm * 100.0f) / FAN_MAX_RPM;
+  float error = fan_target_rpm - fan_rpm_fast;
+
+  fan_error_integral += FAN_I * error;
+  
+  uint8_t output = MIN(100U, MAX(0U, feedforward + fan_error_integral));
+  pwm_set(TIM3, 3, output);
 }
