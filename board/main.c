fe8d6259d26f126df1a036ba4a4d9c540ad26d41
Robbe Derks <robbe.derks@gmail.com>
2022-08-12 12:40:14 +0200

    Minimum uptime for deep sleep (#1020)
    add minimum uptime before going back to deepsleep

--- board/main.c
+++ board/main.c
@@ -421,7 +421,7 @@ int main(void) {
         }
       #endif
     } else {
-      if (deepsleep_allowed && !usb_enumerated && !check_started() && ignition_seen) {
+      if (deepsleep_allowed && !usb_enumerated && !check_started() && ignition_seen && (heartbeat_counter > 20U)) {
         usb_soft_disconnect(true);
         current_board->set_fan_power(0U);
         current_board->set_usb_power_mode(USB_POWER_CLIENT);
