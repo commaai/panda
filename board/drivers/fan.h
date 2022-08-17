d2bd67db87e964f44d31dc9bc7b0dd2f7af3680b
Comma Device <device@comma.ai>
2022-08-17 15:59:56 -0700

    stall detection and reset

--- board/drivers/fan.h
+++ board/drivers/fan.h
@@ -4,14 +4,15 @@ struct fan_state_t {
   uint16_t target_rpm;
   uint8_t power;
   float error_integral;
+  uint8_t stall_counter;
 } fan_state_t;
 struct fan_state_t fan_state;
 
 #define FAN_I 0.001f
+#define FAN_STALL_THRESHOLD 25U
 
 void fan_set_power(uint8_t percentage){
   fan_state.target_rpm = ((current_board->fan_max_rpm * MIN(100U, MAX(0U, percentage))) / 100U);
-  current_board->set_fan_enabled(percentage > 0U);
 }
 
 // Call this at 8Hz
@@ -21,6 +22,26 @@ void fan_tick(void){
     uint16_t fan_rpm_fast = fan_state.tach_counter * (60U * 8U / 4U);
     fan_state.tach_counter = 0U;
     fan_state.rpm = (fan_rpm_fast + (3U * fan_state.rpm)) / 4U;
+
+    // Enable fan if we want it to spin
+    current_board->set_fan_enabled(fan_state.target_rpm > 0U);
+
+    // Stall detection
+    if(fan_state.power > 0U) {
+      if (fan_rpm_fast == 0U) {
+        fan_state.stall_counter = MIN(fan_state.stall_counter + 1U, 255U);
+      } else {
+        fan_state.stall_counter = 0U;
+      }
+
+      if (fan_state.stall_counter > FAN_STALL_THRESHOLD) {
+        // Stall detected, power cycling fan controller
+        current_board->set_fan_enabled(false);
+        fan_state.error_integral = 0U;
+      }
+    } else {
+      fan_state.stall_counter = 0U;
+    }
 
     // Update controller
     float feedforward = (fan_state.target_rpm * 100.0f) / current_board->fan_max_rpm;
