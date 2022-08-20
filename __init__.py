82e33acfb7de515a5dd82c70fade7f3f36dcae68
Igor Biletksyy <bs@privacy.im>
2022-08-19 17:46:36 -0700

    mcu_type F2

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 # pylint: skip-file
 from .python import Panda, PandaDFU, flash_release, \
                     BASEDIR, ensure_st_up_to_date, PandaSerial, pack_can_buffer, unpack_can_buffer, \
-                    DEFAULT_FW_FN, DEFAULT_H7_FW_FN, MCU_TYPE_H7, MCU_TYPE_F4, DLC_TO_LEN, LEN_TO_DLC, \
+                    DEFAULT_FW_FN, DEFAULT_H7_FW_FN, MCU_TYPE_H7, MCU_TYPE_F4, MCU_TYPE_F2, DLC_TO_LEN, LEN_TO_DLC, \
                     ALTERNATIVE_EXPERIENCE
 
 from .python.config import BOOTSTUB_ADDRESS, BLOCK_SIZE_FX, APP_ADDRESS_FX, \
