046fd58e8d64c58ed80769fcbec5ac2417a04c71
Igor Biletksyy <bs@privacy.im>
2022-09-15 14:54:38 -0700

    python lib: check get_packets_versions length

--- python/__init__.py
+++ python/__init__.py
@@ -540,7 +540,7 @@ class Panda:
   # Returns tuple with health packet version and CAN packet/USB packet version
   def get_packets_versions(self):
     dat = self._handle.controlRead(Panda.REQUEST_IN, 0xdd, 0, 0, 3)
-    if dat:
+    if dat and len(dat) == 3:
       a = struct.unpack("BBB", dat)
       return (a[0], a[1], a[2])
     else:
