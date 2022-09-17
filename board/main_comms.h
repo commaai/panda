a19f12559e1626680195c60a2b7f04001068bd41
Igor Biletskyy <briskspirit@users.noreply.github.com>
2022-09-16 23:51:55 -0700

    check if set CAN speed is valid (#1073)
    * check
    
    * MISRA

--- board/main_comms.h
+++ board/main_comms.h
@@ -384,8 +384,7 @@ int comms_control_handler(ControlPacket_t *req, uint8_t *resp) {
       break;
     // **** 0xde: set can bitrate
     case 0xde:
-      if (req->param1 < BUS_CNT) {
-        // TODO: add sanity check, ideally check if value is correct(from array of correct values)
+      if ((req->param1 < BUS_CNT) && is_speed_valid(req->param2, speeds, sizeof(speeds)/sizeof(speeds[0]))) {
         bus_config[req->param1].can_speed = req->param2;
         bool ret = can_init(CAN_NUM_FROM_BUS_NUM(req->param1));
         UNUSED(ret);
@@ -538,8 +537,9 @@ int comms_control_handler(ControlPacket_t *req, uint8_t *resp) {
       break;
     // **** 0xde: set CAN FD data bitrate
     case 0xf9:
-      if ((req->param1 < CAN_CNT) && current_board->has_canfd) {
-        // TODO: add sanity check, ideally check if value is correct (from array of correct values)
+      if ((req->param1 < CAN_CNT) &&
+           current_board->has_canfd &&
+           is_speed_valid(req->param2, data_speeds, sizeof(data_speeds)/sizeof(data_speeds[0]))) {
         bus_config[req->param1].can_data_speed = req->param2;
         bus_config[req->param1].canfd_enabled = (req->param2 >= bus_config[req->param1].can_speed);
         bus_config[req->param1].brs_enabled = (req->param2 > bus_config[req->param1].can_speed);
