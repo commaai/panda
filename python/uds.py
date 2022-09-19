b28053a6632c73adf19fba5312b84cc7b9fcc7e5
Shane Smiskol <shane@smiskol.com>
2022-09-19 13:12:21 -0700

    update separation time to 10ms

--- python/uds.py
+++ python/uds.py
@@ -457,7 +457,7 @@ class IsoTpMessage():
       if self.debug:
         print(f"ISO-TP: TX - flow control continue - {hex(self._can_client.tx_addr)}")
       # send flow control message (send all bytes)
-      msg = b"\x30\x01\x00".ljust(self.max_len, b"\x00")
+      msg = b"\x30\x00\x0a".ljust(self.max_len, b"\x00")
       self._can_client.send([msg])
       return
 
@@ -470,9 +470,6 @@ class IsoTpMessage():
       self.rx_dat += rx_data[1:1 + rx_size]
       if self.rx_len == len(self.rx_dat):
         self.rx_done = True
-      else:
-        msg = b"\x30\x01\x00".ljust(self.max_len, b"\x00")
-        self._can_client.send([msg])
       if self.debug:
         print(f"ISO-TP: RX - consecutive frame - {hex(self._can_client.rx_addr)} idx={self.rx_idx} done={self.rx_done}")
       return
