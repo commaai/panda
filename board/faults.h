a402d91fd92d82333b582127773ae8a4a4a41299
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-10-02 17:20:07 -0700

    add fault for invalid safety RX checks

--- board/faults.h
+++ board/faults.h
@@ -26,6 +26,7 @@
 #define FAULT_INTERRUPT_RATE_CLOCK_SOURCE   (1U << 20)
 #define FAULT_INTERRUPT_RATE_TICK           (1U << 21)
 #define FAULT_INTERRUPT_RATE_EXTI           (1U << 22)
+#define FAULT_SAFETY_RX_CHECKS_INVALID      (1U << 23)
 
 // Permanent faults
 #define PERMANENT_FAULTS 0U
