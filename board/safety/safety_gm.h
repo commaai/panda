c957df557faa19e613282d13ff75cff8f912d7ba
Shane Smiskol <shane@smiskol.com>
2022-10-07 15:26:33 -0700

    label platforms

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -33,9 +33,9 @@ AddrCheckStruct gm_addr_checks[] = {
   {.msg = {{388, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
   {.msg = {{842, 0, 5, .expected_timestep = 100000U}, { 0 }, { 0 }}},
   {.msg = {{481, 0, 7, .expected_timestep = 100000U}, { 0 }, { 0 }}},
-  {.msg = {{190, 0, 6, .expected_timestep = 100000U},
-           {190, 0, 7, .expected_timestep = 100000U},
-           {190, 0, 8, .expected_timestep = 100000U}}},  // 8 bytes on msg varies in size by platform
+  {.msg = {{190, 0, 6, .expected_timestep = 100000U},   // Volt, Silverado, Acadia Denali
+           {190, 0, 7, .expected_timestep = 100000U},   // Bolt EUV
+           {190, 0, 8, .expected_timestep = 100000U}}}, // Escalade
   {.msg = {{452, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
 };
 #define GM_RX_CHECK_LEN (sizeof(gm_addr_checks) / sizeof(gm_addr_checks[0]))
