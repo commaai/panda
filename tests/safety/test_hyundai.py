86489ad41f4a0d750fc221f798f9b44555aa0950
Shane Smiskol <shane@smiskol.com>
2022-10-10 14:39:44 -0700

    tests: fix counter in FCA message

--- tests/safety/test_hyundai.py
+++ tests/safety/test_hyundai.py
@@ -277,7 +277,7 @@ class TestHyundaiLongitudinalSafety(TestHyundaiSafety):
   def _send_fca11_msg(self, idx=0, vsm_aeb_req=False, fca_aeb_req=False, aeb_decel=0):
     values = {
       "CR_FCA_Alive": ((-((idx % 0xF) + 2) % 4) << 2) + 1,
-      "Supplemental_Counter": idx % 0xF,
+      "CR_FCA_Alive": idx % 0xF,
       "FCA_Status": 2,
       "CR_VSM_DecCmd": aeb_decel,
       "CF_VSM_DecCmdAct": int(vsm_aeb_req),
