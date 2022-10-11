3644d9412b1bae7c6e62d363735a882e50bb4c7f
Shane Smiskol <shane@smiskol.com>
2022-10-11 11:25:41 -0700

    Revert "Hyundai: steering fault bit safety (#1095)"
    This reverts commit 2334a413bc533f058bc3b61b47604b5207035a08.

--- board/safety/safety_hyundai.h
+++ board/safety/safety_hyundai.h
@@ -324,10 +324,6 @@ static int hyundai_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
   if (addr == 832) {
     int desired_torque = ((GET_BYTES_04(to_send) >> 16) & 0x7ffU) - 1024U;
     bool steer_req = GET_BIT(to_send, 27U) != 0U;
-    bool toi_flt = GET_BIT(to_send, 28U) != 0U;
-    if (toi_flt && !controls_allowed) {
-      tx = 0;
-    }
 
     if (steer_torque_cmd_checks(desired_torque, steer_req, HYUNDAI_STEERING_LIMITS)) {
       tx = 0;
