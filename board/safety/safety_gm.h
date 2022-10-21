352f6e7c9be01f9ee1c435993beb38b092326b76
Shane Smiskol <shane@smiskol.com>
2022-10-21 01:29:38 -0700

    temporary fault fix

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -124,7 +124,7 @@ static int gm_rx_hook(CANPacket_t *to_push) {
       if (gm_hw == GM_ASCM) {
         brake_pressed = GET_BYTE(to_push, 1) >= 8U;
       } else {
-        brake_pressed = GET_BYTE(to_push, 1) >= 20U;
+        brake_pressed = GET_BYTE(to_push, 1) >= (gm_pcm_cruise ? 20U : 8U);
       }
     }
 
