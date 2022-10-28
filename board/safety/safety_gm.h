dce4412d0655dc11e1273d34cad1803ed21d5bbf
Shane Smiskol <shane@smiskol.com>
2022-10-27 21:57:43 -0700

    clean that up

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -141,8 +141,8 @@ static int gm_rx_hook(CANPacket_t *to_push) {
 
     bool stock_ecu_detected = (addr == 384);  // ASCMLKASteeringCmd
 
-    // Only check ASCMGasRegenCmd if ASCM, GM_CAM uses stock longitudinal
-    if (((gm_hw == GM_ASCM) || ((gm_hw == GM_CAM) && gm_cam_long)) && (addr == 715)) {
+    // Check ASCMGasRegenCmd only if we're sending it
+    if (!gm_pcm_cruise && (addr == 715)) {
       stock_ecu_detected = true;
     }
     generic_rx_checks(stock_ecu_detected);
