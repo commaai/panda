ca681ff8299c91491558c0e44f4a4779dc0a2998
Shane Smiskol <shane@smiskol.com>
2022-11-07 22:13:17 -0800

    Hyundai: CAMERA_SCC works on both safety modes (#1139)
    * Make camera SCC work on both safety modes
    
    * update in safety
    
    * move to common

--- board/safety/safety_hyundai_canfd.h
+++ board/safety/safety_hyundai_canfd.h
@@ -96,8 +96,8 @@ addr_checks hyundai_canfd_rx_checks = {hyundai_canfd_addr_checks, HYUNDAI_CANFD_
 uint16_t hyundai_canfd_crc_lut[256];
 
 
-const int HYUNDAI_PARAM_CANFD_HDA2 = 8;
-const int HYUNDAI_PARAM_CANFD_ALT_BUTTONS = 16;
+const int HYUNDAI_PARAM_CANFD_HDA2 = 16;
+const int HYUNDAI_PARAM_CANFD_ALT_BUTTONS = 32;
 bool hyundai_canfd_hda2 = false;
 bool hyundai_canfd_alt_buttons = false;
 
