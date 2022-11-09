4c5cff1b01294e95f3518cbfaa9aba4830eaa649
Shane Smiskol <shane@smiskol.com>
2022-11-08 17:50:49 -0800

    length 8

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -61,7 +61,7 @@ AddrCheckStruct gm_addr_checks[] = {
            {190, 0, 7, .expected_timestep = 100000U},    // Bolt EUV
            {190, 0, 8, .expected_timestep = 100000U}}},  // Escalade
   {.msg = {{452, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
-  {.msg = {{201, 0, 6, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{201, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
 };
 #define GM_RX_CHECK_LEN (sizeof(gm_addr_checks) / sizeof(gm_addr_checks[0]))
 addr_checks gm_rx_checks = {gm_addr_checks, GM_RX_CHECK_LEN};
