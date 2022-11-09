f2e899cf4451ee18e8791acd5eb61a69031695fd
Shane Smiskol <shane@smiskol.com>
2022-11-08 19:40:48 -0800

    common gmcamera class

--- tests/safety/test_gm.py
+++ tests/safety/test_gm.py
@@ -165,11 +165,26 @@ class TestGmAscmSafety(GmLongitudinalBase, TestGmSafetyBase):
     self.safety.init_tests()
 
 
-class TestGmCameraSafety(TestGmSafetyBase):
+class TestGmCameraSafetyBase(TestGmSafetyBase):
+
+  FWD_BUS_LOOKUP = {0: 2, 2: 0}
+
+  @classmethod
+  def setUpClass(cls):
+    if cls.__name__ == "TestGmCameraSafetyBase":
+      cls.packer = None
+      cls.safety = None
+      raise unittest.SkipTest
+
+  def _user_brake_msg(self, brake):
+    values = {"BrakePressed": brake}
+    return self.packer.make_can_msg_panda("ECMEngineStatus", 0, values)
+
+
+class TestGmCameraSafety(TestGmCameraSafetyBase):
   TX_MSGS = [[384, 0],  # pt bus
              [388, 2]]  # camera bus
   FWD_BLACKLISTED_ADDRS = {2: [384], 0: [388]}  # block LKAS message and PSCMStatus
-  FWD_BUS_LOOKUP = {0: 2, 2: 0}
   BUTTONS_BUS = 2  # tx only
 
   def setUp(self):
@@ -214,11 +229,10 @@ class TestGmCameraSafety(TestGmSafetyBase):
     pass
 
 
-class TestGmCameraLongitudinalSafety(GmLongitudinalBase, TestGmSafetyBase):
+class TestGmCameraLongitudinalSafety(GmLongitudinalBase, TestGmCameraSafetyBase):
   TX_MSGS = [[384, 0], [789, 0], [715, 0], [880, 0],  # pt bus
              [388, 2]]  # camera bus
   FWD_BLACKLISTED_ADDRS = {2: [384, 715, 880, 789], 0: [388]}  # block LKAS, ACC messages and PSCMStatus
-  FWD_BUS_LOOKUP = {0: 2, 2: 0}
   BUTTONS_BUS = 0  # rx only
 
   MAX_GAS = 3400
@@ -232,10 +246,6 @@ class TestGmCameraLongitudinalSafety(GmLongitudinalBase, TestGmSafetyBase):
     self.safety = libpandasafety_py.libpandasafety
     self.safety.set_safety_hooks(Panda.SAFETY_GM, Panda.FLAG_GM_HW_CAM | Panda.FLAG_GM_HW_CAM_LONG)
     self.safety.init_tests()
-
-  def _user_brake_msg(self, brake):
-    values = {"BrakePressed": brake}
-    return self.packer.make_can_msg_panda("ECMEngineStatus", 0, values)
 
 
 if __name__ == "__main__":
