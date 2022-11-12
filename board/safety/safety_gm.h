c7fdc3b00c12da64763c371f8cf017faa0273b1d
Shane Smiskol <shane@smiskol.com>
2022-11-12 00:47:12 -0800

    This is a test of both PRs combined

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -107,7 +107,7 @@ static int gm_rx_hook(CANPacket_t *to_push) {
 
       // enter controls on falling edge of set or resume
       bool set = (button != GM_BTN_SET) && (cruise_button_prev == GM_BTN_SET);
-      bool res = (button != GM_BTN_RESUME) && (cruise_button_prev == GM_BTN_RESUME);
+      bool res = (button == GM_BTN_RESUME) && (cruise_button_prev != GM_BTN_RESUME);
       if (set || res) {
         controls_allowed = 1;
       }
