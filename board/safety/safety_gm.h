c0632cd32b78279dfbb3ce28c7a10ded8090d40f
Shane Smiskol <shane@smiskol.com>
2022-11-13 19:10:45 -0800

    GM: enable on rising edge of resume (#1157)
    * rising edge of resume
    
    * fix test
    
    * comment
    
    * todo fix tests
    
    * fix test
    
    * we want to test the transition from btn prev to btn cur, not unpressed to x to y
    
    * range

--- board/safety/safety_gm.h
+++ board/safety/safety_gm.h
@@ -105,9 +105,9 @@ static int gm_rx_hook(CANPacket_t *to_push) {
     if ((addr == 481) && !gm_pcm_cruise) {
       int button = (GET_BYTE(to_push, 5) & 0x70U) >> 4;
 
-      // enter controls on falling edge of set or resume
+      // enter controls on falling edge of set or rising edge of resume (avoids fault)
       bool set = (button != GM_BTN_SET) && (cruise_button_prev == GM_BTN_SET);
-      bool res = (button != GM_BTN_RESUME) && (cruise_button_prev == GM_BTN_RESUME);
+      bool res = (button == GM_BTN_RESUME) && (cruise_button_prev != GM_BTN_RESUME);
       if (set || res) {
         controls_allowed = 1;
       }
