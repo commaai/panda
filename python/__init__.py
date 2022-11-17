1f9e46f3915962a236d9dfec4f9884f06244dc72
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-11-17 11:00:49 -0800

    green led

--- python/__init__.py
+++ python/__init__.py
@@ -847,7 +847,3 @@ class Panda:
   # ****************** Siren *****************
   def set_siren(self, enabled):
     self._handle.controlWrite(Panda.REQUEST_OUT, 0xf6, int(enabled), 0, b'')
-
-  # ****************** Debug *****************
-  def set_green_led(self, enabled):
-    self._handle.controlWrite(Panda.REQUEST_OUT, 0xf7, int(enabled), 0, b'')
