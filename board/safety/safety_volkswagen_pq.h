edcefb04b39a7f22537da5bbf17e24ac9af68299
Shane Smiskol <shane@smiskol.com>
2022-11-29 15:35:06 -0800

    some clean up

--- board/safety/safety_volkswagen_pq.h
+++ board/safety/safety_volkswagen_pq.h
@@ -14,10 +14,10 @@ const SteeringLimits VOLKSWAGEN_PQ_STEERING_LIMITS = {
 
 // longitudinal limits
 // acceleration in m/s2 * 1000 to avoid floating point math
-const LongitudinalLimits VOLKSWAGEN_LONG_LIMITS = {
+const LongitudinalLimits VOLKSWAGEN_PQ_LONG_LIMITS = {
   .max_accel = 2000,
   .min_accel = -3500,
-  .inactive_accel = 3010,
+  .inactive_accel = 3010,  // VW sends one increment above the max range when inactive
 };
 
 #define MSG_LENKHILFE_3         0x0D0   // RX from EPS, for steering angle and driver steering torque
@@ -206,16 +206,10 @@ static int volkswagen_pq_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed
   // Safety check for acceleration commands
   // To avoid floating point math, scale upward and compare to pre-scaled safety m/s2 boundaries
   if (addr == MSG_ACC_SYSTEM) {
-    bool violation = 0;
-    int desired_accel = 0;
-
     // Signal: ACC_System.ACS_Sollbeschl (acceleration in m/s2, scale 0.005, offset -7.22)
-    desired_accel = ((((GET_BYTE(to_send, 4) & 0x7U) << 8) | GET_BYTE(to_send, 3)) * 5U) - 7220U;
+    int desired_accel = ((((GET_BYTE(to_send, 4) & 0x7U) << 8) | GET_BYTE(to_send, 3)) * 5U) - 7220U;
 
-    // VW sends one increment above the max range when inactive
-    violation |= long_accel_checks(desired_accel, VOLKSWAGEN_LONG_LIMITS, longitudinal_allowed);
-
-    if (violation) {
+    if (long_accel_checks(desired_accel, VOLKSWAGEN_PQ_LONG_LIMITS, longitudinal_allowed)) {
       tx = 0;
     }
   }
