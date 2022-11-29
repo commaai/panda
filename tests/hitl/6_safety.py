d9528992c2bf74c7dd0f2ac9af6bb1c847339816
Bruce Wayne <batman@comma.ai>
2022-11-29 15:55:16 -0800

    fix test

--- tests/hitl/6_safety.py
+++ tests/hitl/6_safety.py
@@ -1,4 +1,5 @@
 import time
+from nose.tools import assert_equal
 
 from panda import Panda
 from .helpers import test_all_pandas, panda_connect_and_init
@@ -23,10 +24,10 @@ def test_safety_nooutput(p):
 @panda_connect_and_init
 def test_canfd_safety_modes(p):
   # works on all pandas
-  p.set_safety_mode(Panda.SAFETY_ELM327)
-  assert p.health()['safety_mode'] == Panda.SAFETY_ELM327
+  p.set_safety_mode(Panda.SAFETY_TOYOTA)
+  assert_equal(p.health()['safety_mode'], Panda.SAFETY_TOYOTA)
 
   # shouldn't be able to set a CAN-FD safety mode on non CAN-FD panda
   p.set_safety_mode(Panda.SAFETY_HYUNDAI_CANFD)
-  expected_mode = Panda.SAFETY_HYUNDAI_CANFD if p.get_type() in Panda.H7_DEVICES else Panda.SAFETY_ELM327
-  assert p.health()['safety_mode'] == expected_mode
+  expected_mode = Panda.SAFETY_HYUNDAI_CANFD if p.get_type() in Panda.H7_DEVICES else Panda.SAFETY_SILENT
+  assert_equal(p.health()['safety_mode'], expected_mode)
