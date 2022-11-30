c257b55fe14a39154085a64f05bac85764efd435
moretore <ryleymccarroll@gmail.com>
2022-11-30 12:12:03 -0600

    added new mazda can ign

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -210,6 +210,9 @@ void ignition_can_hook(CANPacket_t *to_push) {
     if ((addr == 0x9E) && (len == 8)) {
       ignition_can = (GET_BYTE(to_push, 0) >> 5) == 0x6U;
     }
+    if ((addr == 0x436) && (len == 8)) {
+      ignition_can = ((GET_BYTE(to_push, 0)) & 0xFFU) | ((GET_BYTE(to_push, 1)) & 0xFFU);
+    }
 
   }
 }
