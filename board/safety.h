eaf246c8e23786c68c385ef7adeb912331af3f68
Shane Smiskol <shane@smiskol.com>
2022-11-30 17:06:17 -0800

    whoops

--- board/safety.h
+++ board/safety.h
@@ -512,7 +512,7 @@ bool longitudinal_brake_checks(int desired_brake, const LongitudinalLimits limit
 }
 
 bool longitudinal_interceptor_checks(CANPacket_t *to_send, bool longitudinal_allowed) {
-  bool !longitudinal_allowed && ((GET_BYTE(to_send, 0) || GET_BYTE(to_send, 1)) != 0);
+  return !longitudinal_allowed && ((GET_BYTE(to_send, 0) || GET_BYTE(to_send, 1)) != 0);
 }
 
 // Safety checks for torque-based steering commands
