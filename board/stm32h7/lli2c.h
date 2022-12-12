b53f2ef688334959edc75cab51e209fc2a4688d1
Robbe Derks <robbe.derks@gmail.com>
2022-12-12 14:35:38 +0100

    fix misra violations

--- board/stm32h7/lli2c.h
+++ board/stm32h7/lli2c.h
@@ -3,6 +3,8 @@
 // if we want it to be more async, we should use interrupts
 
 #define I2C_TIMEOUT_US 100000U
+
+// cppcheck-suppress misra-c2012-2.7; not sure why it triggers here?
 bool i2c_status_wait(volatile uint32_t *reg, uint32_t mask, uint32_t val) {
   uint32_t start_time = microsecond_timer_get();
   while(((*reg & mask) != val) && (get_ts_elapsed(microsecond_timer_get(), start_time) < I2C_TIMEOUT_US));
@@ -11,7 +13,7 @@ bool i2c_status_wait(volatile uint32_t *reg, uint32_t mask, uint32_t val) {
 
 bool i2c_write_reg(I2C_TypeDef *I2C, uint8_t addr, uint8_t reg, uint8_t value) {
   // Setup transfer and send START + addr
-  bool start_success = false;
+  bool ret = false;
   for(uint32_t i=0U; i<10U; i++) {
     register_clear_bits(&I2C->CR2, I2C_CR2_ADD10);
     I2C->CR2 = ((addr << 1U) & I2C_CR2_SADD_Msk);
@@ -28,32 +30,35 @@ bool i2c_write_reg(I2C_TypeDef *I2C, uint8_t addr, uint8_t reg, uint8_t value) {
     if ((I2C->ISR & I2C_ISR_ARLO) != 0U) {
       register_set_bits(&I2C->ICR, I2C_ICR_ARLOCF);
     } else {
-      start_success = true;
+      ret = true;
       break;
     }
   }
 
-  if (!start_success) {
-    return false;
+  if (!ret) {
+    goto end;
   }
 
   // Send data
-  if(!i2c_status_wait(&I2C->ISR, I2C_ISR_TXIS, I2C_ISR_TXIS)) {
-    return false;
+  ret = i2c_status_wait(&I2C->ISR, I2C_ISR_TXIS, I2C_ISR_TXIS);
+  if(!ret) {
+    goto end;
   }
   I2C->TXDR = reg;
 
-  if(!i2c_status_wait(&I2C->ISR, I2C_ISR_TXIS, I2C_ISR_TXIS)) {
-    return false;
+  ret = i2c_status_wait(&I2C->ISR, I2C_ISR_TXIS, I2C_ISR_TXIS);
+  if(!ret) {
+    goto end;
   }
   I2C->TXDR = value;
 
-  return true;
+end:
+  return ret;
 }
 
 bool i2c_read_reg(I2C_TypeDef *I2C, uint8_t addr, uint8_t reg, uint8_t *value) {
   // Setup transfer and send START + addr
-  bool start_success = false;
+  bool ret = false;
   for(uint32_t i=0U; i<10U; i++) {
     register_clear_bits(&I2C->CR2, I2C_CR2_ADD10);
     I2C->CR2 = ((addr << 1U) & I2C_CR2_SADD_Msk);
@@ -70,67 +75,75 @@ bool i2c_read_reg(I2C_TypeDef *I2C, uint8_t addr, uint8_t reg, uint8_t *value) {
     if ((I2C->ISR & I2C_ISR_ARLO) != 0U) {
       register_set_bits(&I2C->ICR, I2C_ICR_ARLOCF);
     } else {
-      start_success = true;
+      ret = true;
       break;
     }
   }
 
-  if (!start_success) {
-    return false;
+  if (!ret) {
+    goto end;
   }
 
   // Send data
-  if(!i2c_status_wait(&I2C->ISR, I2C_ISR_TXIS, I2C_ISR_TXIS)) {
-    return false;
+  ret = i2c_status_wait(&I2C->ISR, I2C_ISR_TXIS, I2C_ISR_TXIS);
+  if(!ret) {
+    goto end;
   }
   I2C->TXDR = reg;
 
   // Restart
-  I2C->CR2 = (((addr << 1U) | 0x1) & I2C_CR2_SADD_Msk) | (1 << I2C_CR2_NBYTES_Pos) | I2C_CR2_RD_WRN | I2C_CR2_START;
-  if(!i2c_status_wait(&I2C->CR2, I2C_CR2_START, 0U)) {
-    return false;
+  I2C->CR2 = (((addr << 1) | 0x1U) & I2C_CR2_SADD_Msk) | (1U << I2C_CR2_NBYTES_Pos) | I2C_CR2_RD_WRN | I2C_CR2_START;
+  ret = i2c_status_wait(&I2C->CR2, I2C_CR2_START, 0U);
+  if(!ret) {
+    goto end;
   }
 
   // check if we lost arbitration
   if ((I2C->ISR & I2C_ISR_ARLO) != 0U) {
     register_set_bits(&I2C->ICR, I2C_ICR_ARLOCF);
-    return false;
+    ret = false;
+    goto end;
   }
 
   // Read data
-  if(!i2c_status_wait(&I2C->ISR, I2C_ISR_RXNE, I2C_ISR_RXNE)) {
-    return false;
+  ret = i2c_status_wait(&I2C->ISR, I2C_ISR_RXNE, I2C_ISR_RXNE);
+  if(!ret) {
+    goto end;
   }
   *value = I2C->RXDR;
 
   // Stop
   I2C->CR2 |= I2C_CR2_STOP;
 
-  return true;
+end:
+  return ret;
 }
 
 bool i2c_set_reg_bits(I2C_TypeDef *I2C, uint8_t addr, uint8_t reg, uint8_t bits) {
   uint8_t value;
-  if(!i2c_read_reg(I2C, addr, reg, &value)) {
-    return false;
-  } 
-  return i2c_write_reg(I2C, addr, reg, value | bits);
+  bool ret = i2c_read_reg(I2C, addr, reg, &value);
+  if(ret) {
+    ret = i2c_write_reg(I2C, addr, reg, value | bits);
+  }
+  return ret;
 }
 
 bool i2c_clear_reg_bits(I2C_TypeDef *I2C, uint8_t addr, uint8_t reg, uint8_t bits) {
   uint8_t value;
-  if(!i2c_read_reg(I2C, addr, reg, &value)) {
-    return false;
-  } 
-  return i2c_write_reg(I2C, addr, reg, value & ~bits);
+  bool ret = i2c_read_reg(I2C, addr, reg, &value);
+  if(ret) {
+    ret = i2c_write_reg(I2C, addr, reg, value & (uint8_t) (~bits));
+  }
+  return ret;
 }
 
 bool i2c_set_reg_mask(I2C_TypeDef *I2C, uint8_t addr, uint8_t reg, uint8_t value, uint8_t mask) {
   uint8_t old_value;
-  if(!i2c_read_reg(I2C, addr, reg, &old_value)) {
-    return false;
-  } 
-  return i2c_write_reg(I2C, addr, reg, (old_value & ~mask) | (value & mask));
+  bool ret = i2c_read_reg(I2C, addr, reg, &old_value);
+  if(ret) {
+    ret = i2c_write_reg(I2C, addr, reg, (old_value & (uint8_t) (~mask)) | (value & mask));
+  }
+  return ret;
 }
 
 void i2c_init(I2C_TypeDef *I2C) {
