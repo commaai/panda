cf426a34d608ad71e07e3a809ded5821af46a45c
Comma Device <device@comma.ai>
2022-12-26 18:38:51 -0800

    support both for now

--- python/spi.py
+++ python/spi.py
@@ -2,10 +2,15 @@ import math
 import time
 import struct
 import spidev
-import spidev2
 import logging
 from functools import reduce
 from typing import List
+
+# TODO: remove this once spidev2 is in agnos
+try:
+  import spidev2
+except ImportError:
+  spidev2 = None
 
 # Constants
 SYNC = 0x5A
@@ -38,22 +43,15 @@ class PandaSpiTransferFailed(PandaSpiException):
   pass
 
 
-import os
-DEBUG = "SPI_DEBUG" in os.environ
-def debug(string, *args):
-  if DEBUG:
-    print(string % tuple(args))
-#
 # This mimics the handle given by libusb1 for easy interoperability
 class SpiHandle:
   def __init__(self):
-    #self.spi = spidev.SpiDev()  # pylint: disable=c-extension-no-member
-    #self.spi.open(0, 0)
-    #self.spi.max_speed_hz = 30_000_000
-
-    self.spi = spidev2.SPIBus('/dev/spidev0.0', 'w+b', bits_per_word=8, speed_hz=30_000_000, spi_mode=spidev2.SPIMode32.SPI_MODE_0)
-
-    self.rx_buf = bytearray(XFER_SIZE)
+    if spidev2 is not None:
+      self.spi = spidev2.SPIBus('/dev/spidev0.0', 'w+b', bits_per_word=8, speed_hz=30_000_000, spi_mode=spidev2.SPIMode32.SPI_MODE_0)
+    else:
+      self.spi = spidev.SpiDev()  # pylint: disable=c-extension-no-member
+      self.spi.open(0, 0)
+      self.spi.max_speed_hz = 30_000_000
 
   # helpers
   def _calc_checksum(self, data: List[int]) -> int:
@@ -62,69 +60,63 @@ class SpiHandle:
       cksum ^= b
     return cksum
 
-  def _transfer(self, endpoint: int, data, max_rx_len: int = 1000) -> bytes:
-    debug("starting transfer: endpoint=%d, max_rx_len=%d", endpoint, max_rx_len)
-    debug("==============================================")
+  def _transfer(self, tx_data: bytes, rx_len: int) -> bytes:
+    if isinstance(self.spi, spidev.SpiDev):
+      self.spi.xfer2(tx_data)
+      rx_buf = self.spi.xfer2(b'\x00'*XFER_SIZE)
+    else:
+      rx_buf = bytearray(rx_len)
+      transfer_list = spidev2.SPITransferList((
+        {
+          'tx_buf': tx_data,
+          'delay_usecs': 100,
+        },
+        {
+          'rx_buf': rx_buf,
+        },
+      ))
+      self.spi.submitTransferList(transfer_list)
+
+    ack = rx_buf[0]
+    response_len = struct.unpack("<H", bytes(rx_buf[1:3]))[0]
+
+    #logging.debug("* response")
+    #logging.debug("  * length: %d", response_len)
+    #logging.debug("  * buf length: %d", len(rx_buf))
+    #logging.debug("  * ACK: %s %s", repr(rx_buf[0]), hex(rx_buf[0]))
+    #logging.debug("  * raw: %s", repr(rx_buf))
+
+    if ack == NACK:
+      raise PandaSpiNackResponse
+    elif ack != DACK:
+      raise PandaSpiMissingAck
+
+    dat = rx_buf[3:3+response_len+1]
+    if self._calc_checksum(rx_buf[:1+2+response_len+1]) != 0:
+      raise PandaSpiBadChecksum
+
+    return bytes(dat[:-1])
+
+  def _transfer_retry(self, endpoint: int, data, rx_len: int = 0x40) -> bytes:
+    #logging.debug("starting transfer: endpoint=%d, rx_len=%d", endpoint, rx_len)
+    #logging.debug("==============================================")
 
     # build the packet
-    header = struct.pack("<BBHH", SYNC, endpoint, len(data), max_rx_len)
+    header = struct.pack("<BBHH", SYNC, endpoint, len(data), rx_len)
     packet = header + bytes(data)
     packet += bytes([reduce(lambda x, y: x^y, packet) ^ CHECKSUM_START])
     assert len(packet) < XFER_SIZE
-    debug("- header %s", repr(header))
-    debug("- data %s", repr(data))
-    debug("- checksum %s", repr(packet[-1]))
-    debug("- length %d", len(packet))
+    #logging.debug("- header %s", repr(header))
+    #logging.debug("- data %s", repr(data))
+    #logging.debug("- checksum %s", repr(packet[-1]))
+    #logging.debug("- length %d", len(packet))
 
-    # pad to max size
     packet = packet.ljust(XFER_SIZE, b'\x00')
 
     for n in range(MAX_XFER_RETRY_COUNT):
-      debug("\ntry #%d", n+1)
+      #logging.debug("\ntry #%d", n+1)
       try:
-        """
-        debug("- send header + data + checksum")
-        self.spi.xfer2(packet)
-        #self.spi.write(packet)
-
-        time.sleep(0.000001)
-
-        debug("- waiting for response")
-        self.rx_buf = self.spi.xfer2(b'\x00'*XFER_SIZE)
-        #self.spi.transfer(rx_buf=self.rx_buf)
-        #print("self.rx_buf", self.rx_buf)
-        """
-
-        transfer_list = spidev2.SPITransferList((
-          {
-            'tx_buf': packet,
-            'delay_usecs': 1000,
-          },
-          {
-            'rx_buf': self.rx_buf,
-          },
-        ))
-        self.spi.submitTransferList(transfer_list)
-
-        ack = self.rx_buf[0]
-        response_len = struct.unpack("<H", bytes(self.rx_buf[1:3]))[0]
-
-        debug("* response")
-        debug("  * length: %d", response_len)
-        debug("  * buf length: %d", len(self.rx_buf))
-        debug("  * ACK: %s %s", repr(self.rx_buf[0]), hex(self.rx_buf[0]))
-        debug("  * raw: %s", repr(self.rx_buf))
-
-        if ack == NACK:
-          raise PandaSpiNackResponse
-        elif ack != DACK:
-          raise PandaSpiMissingAck
-
-        dat = self.rx_buf[3:3+response_len+1]
-        if self._calc_checksum(self.rx_buf[:1+2+response_len+1]) != 0:
-          raise PandaSpiBadChecksum
-
-        return bytes(dat[:-1])
+        return self._transfer(packet, rx_len)
       except PandaSpiException:
         #logging.exception("SPI transfer failed, %d retries left", n)
         pass
@@ -135,15 +127,15 @@ class SpiHandle:
     self.spi.close()
 
   def controlWrite(self, request_type: int, request: int, value: int, index: int, data, timeout: int = 0):
-    return self._transfer(0, struct.pack("<BHHH", request, value, index, 0))
+    return self._transfer_retry(0, struct.pack("<BHHH", request, value, index, 0))
 
   def controlRead(self, request_type: int, request: int, value: int, index: int, length: int, timeout: int = 0):
-    return self._transfer(0, struct.pack("<BHHH", request, value, index, length))
+    return self._transfer_retry(0, struct.pack("<BHHH", request, value, index, length))
 
   # TODO: implement these properly
   def bulkWrite(self, endpoint: int, data: List[int], timeout: int = 0) -> int:
     #for x in range(math.ceil(len(data) / USB_MAX_SIZE)):
-    #  self._transfer(endpoint, data[USB_MAX_SIZE*x:USB_MAX_SIZE*(x+1)])
+    #  self._transfer_retry(endpoint, data[USB_MAX_SIZE*x:USB_MAX_SIZE*(x+1)])
     return len(data)
 
   def bulkRead(self, endpoint: int, length: int, timeout: int = 0) -> bytes:
@@ -151,7 +143,7 @@ class SpiHandle:
 
     ret: List[int] = []
     for _ in range(math.ceil(length / USB_MAX_SIZE)):
-      d = self._transfer(endpoint, [], max_rx_len=USB_MAX_SIZE)
+      d = self._transfer_retry(endpoint, [], max_rx_len=USB_MAX_SIZE)
       ret += d
       if len(d) < USB_MAX_SIZE:
         break
