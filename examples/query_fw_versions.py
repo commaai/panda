360a320e919180dcc5e8d7294bbd755633751453
Shane Smiskol <shane@smiskol.com>
2023-01-13 15:20:04 -0800

    formatting

--- examples/query_fw_versions.py
+++ examples/query_fw_versions.py
@@ -4,7 +4,6 @@ from tqdm import tqdm
 from panda import Panda
 from panda.python.uds import UdsClient, MessageTimeoutError, NegativeResponseError, SESSION_TYPE, DATA_IDENTIFIER_TYPE
 
-
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument('--rxoffset', default="")
@@ -26,11 +25,11 @@ if __name__ == "__main__":
   for std_id in DATA_IDENTIFIER_TYPE:
     uds_data_ids[std_id.value] = std_id.name
   if args.nonstandard:
-    for uds_id in range(0xf100,0xf180):
+    for uds_id in range(0xf100, 0xf180):
       uds_data_ids[uds_id] = "IDENTIFICATION_OPTION_VEHICLE_MANUFACTURER_SPECIFIC_DATA_IDENTIFIER"
-    for uds_id in range(0xf1a0,0xf1f0):
+    for uds_id in range(0xf1a0, 0xf1f0):
       uds_data_ids[uds_id] = "IDENTIFICATION_OPTION_VEHICLE_MANUFACTURER_SPECIFIC"
-    for uds_id in range(0xf1f0,0xf200):
+    for uds_id in range(0xf1f0, 0xf200):
       uds_data_ids[uds_id] = "IDENTIFICATION_OPTION_SYSTEM_SUPPLIER_SPECIFIC"
 
   panda = Panda()
