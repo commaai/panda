68ba1b06fa252a990590b663699ac889a18a0b24
Shane Smiskol <shane@smiskol.com>
2023-01-13 15:20:18 -0800

    consistent quotes

--- examples/query_fw_versions.py
+++ examples/query_fw_versions.py
@@ -6,12 +6,12 @@ from panda.python.uds import UdsClient, MessageTimeoutError, NegativeResponseErr
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument('--rxoffset', default="")
-  parser.add_argument('--nonstandard', action='store_true')
-  parser.add_argument('--no-obd', action='store_true', help='Bus 1 will not be multiplexed to the OBD-II port')
-  parser.add_argument('--debug', action='store_true')
-  parser.add_argument('--addr')
-  parser.add_argument('--bus')
+  parser.add_argument("--rxoffset", default="")
+  parser.add_argument("--nonstandard", action="store_true")
+  parser.add_argument("--no-obd", action="store_true", help="Bus 1 will not be multiplexed to the OBD-II port")
+  parser.add_argument("--debug", action="store_true")
+  parser.add_argument("--addr")
+  parser.add_argument("--bus")
   args = parser.parse_args()
 
   if args.addr:
