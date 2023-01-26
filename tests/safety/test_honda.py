7b57a385857f5f1567a4009c8e653c154bb4aa31
royjr <royjr96@gmail.com>
2023-01-26 12:05:55 -0500

    remove unused comments

--- tests/safety/test_honda.py
+++ tests/safety/test_honda.py
@@ -547,8 +547,6 @@ class TestHondaBoschRadarlessLongSafety(HondaPcmEnableBase, TestHondaBoschSafety
   """
     Covers the Honda Bosch Radarless safety mode with longitudinal control
   """
-  # NO_GAS = -30000
-  # MAX_GAS = 2000
   MAX_ACCEL = 2.0  # accel is used for brakes, but openpilot can set positive values
   MIN_ACCEL = -3.5
 
@@ -578,16 +576,6 @@ class TestHondaBoschRadarlessLongSafety(HondaPcmEnableBase, TestHondaBoschSafety
   # These cars do not have 0x1BE (BRAKE_MODULE)
   def test_alt_disengage_on_brake(self):
     pass
-
-  # TODO: make test_accel_actuation_limits test common
-
-  # def test_brake_safety_check(self):
-  #   for controls_allowed in [True, False]:
-  #     for accel in np.arange(self.MIN_ACCEL - 1, self.MAX_ACCEL + 1, 0.01):
-  #       accel = round(accel, 2)  # floats might not hit exact boundary conditions without rounding
-  #       self.safety.set_controls_allowed(controls_allowed)
-  #       send = self.MIN_ACCEL <= accel <= self.MAX_ACCEL if controls_allowed else accel == 0
-  #       self.assertEqual(send, self._tx(self._send_gas_brake_msg(self.NO_GAS, accel)), (controls_allowed, accel))
 
 
 if __name__ == "__main__":
