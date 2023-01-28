9bc6e4671d0d8570efbb5c65985b1cf919e603f9
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-01-28 15:52:57 -0800

    more types

--- python/base.py
+++ python/base.py
@@ -5,15 +5,15 @@ from typing import List
 # This mimics the handle given by libusb1 for easy interoperability
 class BaseHandle(ABC):
   @abstractmethod
-  def close(self):
+  def close(self) -> None:
     ...
 
   @abstractmethod
-  def controlWrite(self, request_type: int, request: int, value: int, index: int, data, timeout: int = 0):
+  def controlWrite(self, request_type: int, request: int, value: int, index: int, data, timeout: int = 0) -> int:
     ...
 
   @abstractmethod
-  def controlRead(self, request_type: int, request: int, value: int, index: int, length: int, timeout: int = 0):
+  def controlRead(self, request_type: int, request: int, value: int, index: int, length: int, timeout: int = 0) -> bytes:
     ...
 
   @abstractmethod
