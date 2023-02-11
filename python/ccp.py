6dc97aeefaf31ce3ad4ba987873e554c362f9797
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-02-11 12:07:34 -0800

    fix

--- python/ccp.py
+++ python/ccp.py
@@ -100,9 +100,9 @@ class CcpClient():
       msgs = self._panda.can_recv() or []
       if len(msgs) >= 256:
         print("CAN RX buffer overflow!!!", file=sys.stderr)
-      for rx_addr, _, rx_data, rx_bus in msgs:
+      for rx_addr, _, rx_data_bytearray, rx_bus in msgs:
         if rx_bus == self.can_bus and rx_addr == self.rx_addr:
-          rx_data = bytes(rx_data)  # convert bytearray to bytes
+          rx_data = bytes(rx_data_bytearray)  # convert bytearray to bytes
           if self.debug:
             print(f"CAN-RX: {hex(rx_addr)} - 0x{bytes.hex(rx_data)}")
           assert len(rx_data) == 8, f"message length not 8: {len(rx_data)}"
@@ -182,7 +182,7 @@ class CcpClient():
     self._send_cro(COMMAND_CODE.DNLOAD, bytes([len(data)]) + data)
     resp = self._recv_dto(0.025)
     # mta_addr_ext = resp[0]
-    mta_addr = struct.unpack(f"{self.byte_order.value}I", resp[1:5])[0]
+    mta_addr: int = struct.unpack(f"{self.byte_order.value}I", resp[1:5])[0]
     return mta_addr
 
   def download_6_bytes(self, data: bytes) -> int:
@@ -191,7 +191,7 @@ class CcpClient():
     self._send_cro(COMMAND_CODE.DNLOAD_6, data)
     resp = self._recv_dto(0.025)
     # mta_addr_ext = resp[0]
-    mta_addr = struct.unpack(f"{self.byte_order.value}I", resp[1:5])[0]
+    mta_addr: int = struct.unpack(f"{self.byte_order.value}I", resp[1:5])[0]
     return mta_addr
 
   def upload(self, size: int) -> bytes:
@@ -295,7 +295,7 @@ class CcpClient():
     self._send_cro(COMMAND_CODE.PROGRAM, bytes([size]) + data)
     resp = self._recv_dto(0.1)
     # mta_addr_ext = resp[0]
-    mta_addr = struct.unpack(f"{self.byte_order.value}I", resp[1:5])[0]
+    mta_addr: int = struct.unpack(f"{self.byte_order.value}I", resp[1:5])[0]
     return mta_addr
 
   def program_6_bytes(self, data: bytes) -> int:
@@ -304,7 +304,7 @@ class CcpClient():
     self._send_cro(COMMAND_CODE.PROGRAM_6, data)
     resp = self._recv_dto(0.1)
     # mta_addr_ext = resp[0]
-    mta_addr = struct.unpack(f"{self.byte_order.value}I", resp[1:5])[0]
+    mta_addr: int = struct.unpack(f"{self.byte_order.value}I", resp[1:5])[0]
     return mta_addr
 
   def move_memory_block(self, size: int) -> None:
