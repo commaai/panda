c00aa3813c09dbf7ca130a339c30a382b4454de2
Shane Smiskol <shane@smiskol.com>
2023-02-21 22:48:53 -0800

    comment

--- board/safety.h
+++ board/safety.h
@@ -623,8 +623,8 @@ bool steer_angle_cmd_checks(int desired_angle, bool steer_control_enabled, const
   }
   desired_angle_last = desired_angle;
 
+  // Angle should be the same as current angle while not steering
   if (!limits.angle_disable_near_angle_check) {
-    // Angle should be the same as current angle while not steering
     violation |= (!controls_allowed &&
                     ((desired_angle < (angle_meas.min - 1)) ||
                     (desired_angle > (angle_meas.max + 1))));
