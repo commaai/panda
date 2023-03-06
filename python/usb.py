f9012cd218f9170f89b066f9761421494b893bba
Comma Device <device@comma.ai>
2023-03-06 02:04:21 +0000

    rename

--- python/usb.py
+++ python/usb.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 
 from .base import BaseHandle, BaseSTBootloaderHandle
 
-# *** DFU mode ***
+
 DFU_DNLOAD = 1
 DFU_UPLOAD = 2
 DFU_GETSTATUS = 3
@@ -34,35 +34,35 @@ class PandaUsbHandle(BaseHandle):
 
 class STBootloaderUSBHandle(BaseSTBootloaderHandle):
   def __init__(self, libusb_handle):
-    self._handle = libusb_handle
+    self._libusb_handle = libusb_handle
 
-  def status(self):
+  def status(self) -> None:
     while 1:
-      dat = self._handle.controlRead(0x21, DFU_GETSTATUS, 0, 0, 6)
+      dat = self._libusb_handle.controlRead(0x21, DFU_GETSTATUS, 0, 0, 6)
       if dat[1] == 0:
         break
 
-  def clear_status(self):
+  def clear_status(self) -> None:
     # Clear status
-    stat = self._handle.controlRead(0x21, DFU_GETSTATUS, 0, 0, 6)
+    stat = self._libusb_handle.controlRead(0x21, DFU_GETSTATUS, 0, 0, 6)
     if stat[4] == 0xa:
-      self._handle.controlRead(0x21, DFU_CLRSTATUS, 0, 0, 0)
+      self._libusb_handle.controlRead(0x21, DFU_CLRSTATUS, 0, 0, 0)
     elif stat[4] == 0x9:
-      self._handle.controlWrite(0x21, DFU_ABORT, 0, 0, b"")
+      self._libusb_handle.controlWrite(0x21, DFU_ABORT, 0, 0, b"")
       self.status()
-    stat = str(self._handle.controlRead(0x21, DFU_GETSTATUS, 0, 0, 6))
+    stat = str(self._libusb_handle.controlRead(0x21, DFU_GETSTATUS, 0, 0, 6))
 
   # *** API ***
 
   def close(self):
-    self._handle.close()
+    self._libusb_handle.close()
 
   def program(self, address: int, dat: bytes, block_size: Optional[int] = None) -> None:
     if block_size is None:
       block_size = len(dat)
 
     # Set Address Pointer
-    self._handle.controlWrite(0x21, DFU_DNLOAD, 0, 0, b"\x21" + struct.pack("I", address))
+    self._libusb_handle.controlWrite(0x21, DFU_DNLOAD, 0, 0, b"\x21" + struct.pack("I", address))
     self.status()
 
     # Program
@@ -70,18 +70,18 @@ class STBootloaderUSBHandle(BaseSTBootloaderHandle):
     for i in range(0, len(dat) // block_size):
       ldat = dat[i * block_size:(i + 1) * block_size]
       print("programming %d with length %d" % (i, len(ldat)))
-      self._handle.controlWrite(0x21, DFU_DNLOAD, 2 + i, 0, ldat)
+      self._libusb_handle.controlWrite(0x21, DFU_DNLOAD, 2 + i, 0, ldat)
       self.status()
 
   def erase(self, address):
-    self._handle.controlWrite(0x21, DFU_DNLOAD, 0, 0, b"\x41" + struct.pack("I", address))
+    self._libusb_handle.controlWrite(0x21, DFU_DNLOAD, 0, 0, b"\x41" + struct.pack("I", address))
     self.status()
 
   def jump(self, address):
-    self._handle.controlWrite(0x21, DFU_DNLOAD, 0, 0, b"\x21" + struct.pack("I", address))
+    self._libusb_handle.controlWrite(0x21, DFU_DNLOAD, 0, 0, b"\x21" + struct.pack("I", address))
     self.status()
     try:
-      self._handle.controlWrite(0x21, DFU_DNLOAD, 2, 0, b"")
-      _ = str(self._handle.controlRead(0x21, DFU_GETSTATUS, 0, 0, 6))
+      self._libusb_handle.controlWrite(0x21, DFU_DNLOAD, 2, 0, b"")
+      _ = str(self._libusb_handle.controlRead(0x21, DFU_GETSTATUS, 0, 0, 6))
     except Exception:
       pass
