43df5a3024287eb27073266395bb4b5a12900294
Bruce Wayne <batman@comma.ai>
2023-03-22 14:52:57 -0700

    run all

--- tests/hitl/test.sh
+++ tests/hitl/test.sh
@@ -4,5 +4,5 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-#pytest -v -s --durations=0 $(ls 2_*.py)
-pytest -v -s --durations=0 5_gps.py
+pytest -v -s --durations=0 *.py
+#pytest -v -s --durations=0 5_gps.py
