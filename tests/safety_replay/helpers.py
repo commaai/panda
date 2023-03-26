6cb3561f1687efdc4bb04d812ab15e269b7626e8
AlexandreSato <66435071+AlexandreSato@users.noreply.github.com>
2023-03-25 23:02:34 -0300

    safety replay: update Honda steer value getter (#1292)
    * safety replay: update Honda steer value getter 
    
    I think now it´s right!
    
    ```
    BO_ 228 STEERING_CONTROL: 5 EON
      SG_ STEER_TORQUE : 7|16@0- (1,0) [-4096|4096] "" EPS
    ```
    
    * ops

--- tests/safety_replay/helpers.py
+++ tests/safety_replay/helpers.py
@@ -29,7 +29,8 @@ def is_steering_msg(mode, addr):
 def get_steer_value(mode, to_send):
   torque, angle = 0, 0
   if mode in (Panda.SAFETY_HONDA_NIDEC, Panda.SAFETY_HONDA_BOSCH):
-    torque = to_send.RDLR & 0xFFFF0000
+    torque = (to_send.data[0] << 8) | to_send.data[1]
+    torque = to_signed(torque, 16)
   elif mode == Panda.SAFETY_TOYOTA:
     torque = (to_send.data[1] << 8) | (to_send.data[2])
     torque = to_signed(torque, 16)
