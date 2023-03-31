d9e1a2a6cc558035eed91f5bcdb1aee12046fa68
Shane Smiskol <shane@smiskol.com>
2023-03-31 13:46:57 -0700

    use UdsClient

--- tests/python/test_uds.py
+++ tests/python/test_uds.py
@@ -2,12 +2,10 @@
 import unittest
 from unittest.mock import patch
 from panda import Panda
-from panda.python.uds import UdsClient, MessageTimeoutError, NegativeResponseError, SERVICE_TYPE, SESSION_TYPE, DATA_IDENTIFIER_TYPE, CanClient, IsoTpMessage
-from functools import partial
+from panda.python.uds import UdsClient, MessageTimeoutError, NegativeResponseError, SERVICE_TYPE, SESSION_TYPE, DATA_IDENTIFIER_TYPE
 from parameterized import parameterized
 
 
-# TODO: patch something higher level
 class FakePanda(Panda):
   def __init__(self, msg):
     self.msg = msg
@@ -19,29 +17,26 @@ class FakePanda(Panda):
     return [self.msg]
 
 
+def make_response_dat(sub_addr):
+  ecu_rx_dat = [0x02, SERVICE_TYPE.TESTER_PRESENT + 0x40, 0x0]
+  if sub_addr is not None:
+    ecu_rx_dat.insert(0, sub_addr)
+  ecu_rx_dat.extend([0x0] * (8 - len(ecu_rx_dat)))
+  return bytes(ecu_rx_dat)
+
+
 class TestUds(unittest.TestCase):
   @parameterized.expand([
     (0x750, 0xf, 0x750 + 8),
     (0x750, None, 0x750 + 8),
   ])
   def test_fw_query(self, tx_addr, sub_addr, rx_addr):
-    tx_timeout = 0.1
-
-    ecu_rx_dat = [0x02, SERVICE_TYPE.TESTER_PRESENT + 0x40, 0x0]
-    if sub_addr is not None:
-      ecu_rx_dat.insert(0, sub_addr)
-    ecu_rx_dat.extend([0x0] * (8 - len(ecu_rx_dat)))
-    ecu_rx_dat = bytes(ecu_rx_dat)
-
-    panda = FakePanda(msg=(0x750 + 0x8, 0, bytes(ecu_rx_dat), 0))
-
-    partial(panda.can_send, timeout=int(tx_timeout * 1000))
-    can_client = CanClient(panda.can_send, panda.can_recv, tx_addr, rx_addr, 0, sub_addr, debug=True)
-    isotp_msg = IsoTpMessage(can_client, timeout=0, max_len=7)
-    isotp_msg.send(b'\x3e')
-
-    dat, updated = isotp_msg.recv(0)
-    self.assertEqual(dat, b'\x7e\x00')
+    ecu_response_dat = make_response_dat(sub_addr)
+    panda = FakePanda(msg=(0x750 + 0x8, 0, bytes(ecu_response_dat), 0))
+    uds_client = UdsClient(panda, tx_addr, rx_addr, sub_addr=sub_addr)
+    dat = uds_client._uds_request(0x3e)
+    self.assertEqual(dat, b'\x00')
+    print('FINAL dat', dat)
 
 
 if __name__ == '__main__':
