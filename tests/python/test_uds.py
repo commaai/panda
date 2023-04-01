b9348260e272701ac1e6263ac8a82c0113cfadbb
Shane Smiskol <shane@smiskol.com>
2023-03-31 18:14:02 -0700

    clean up more

--- tests/python/test_uds.py
+++ tests/python/test_uds.py
@@ -1,47 +1,26 @@
 #!/usr/bin/env python3
-import struct
-import time
-import unittest
-from unittest.mock import patch
-from panda import Panda
-from panda.python.uds import UdsClient, MessageTimeoutError, NegativeResponseError, SERVICE_TYPE, SESSION_TYPE, DATA_IDENTIFIER_TYPE, IsoTpMessage, CanClient, get_rx_addr_for_tx_addr
-from parameterized import parameterized
 from parameterized import parameterized_class
-import itertools
+import unittest
 
-
-def p16(val):
-  return struct.pack("!H", val)
+from panda import Panda
+from panda.python.uds import SERVICE_TYPE, IsoTpMessage, CanClient, get_rx_addr_for_tx_addr
 
 
 class FakePanda(Panda):
-  def __init__(self, msg):
-    self.msg = msg
-    # self.last_tx_msg = None
+  def __init__(self):
+    self.rx_msg = None
     self.tx_msgs = []
 
   def can_send(self, addr, dat, bus, timeout=0):
-    # self.last_tx_msg = (addr, 0, dat, bus)
     self.tx_msgs.append((addr, 0, dat, bus))
 
   def can_recv(self):
-    return [self.msg]
-
-
-def make_response_dat(service_type, sub_addr, data: bytes = None):
-  ecu_rx_dat = [service_type + 0x40]
-  if data is not None:
-    ecu_rx_dat += data
-  ecu_rx_dat.insert(0, len(ecu_rx_dat))
-  if sub_addr is not None:
-    ecu_rx_dat.insert(0, sub_addr)
-  ecu_rx_dat.extend([0x0] * (8 - len(ecu_rx_dat)))
-  return bytes(ecu_rx_dat)
+    return [self.rx_msg]
 
 
 def simulate_isotp_comms(tx_addr: int, rx_addr: int, request: bytes, response: bytes = None,
                          sub_addr: int = None):
-  panda = FakePanda([])
+  panda = FakePanda()
   max_len = 8 if sub_addr is None else 7
 
   can_client_openpilot = CanClient(panda.can_send, panda.can_recv, tx_addr, rx_addr, 0, sub_addr, debug=True)
@@ -56,7 +35,7 @@ def simulate_isotp_comms(tx_addr: int, rx_addr: int, request: bytes, response: b
 
   # send first message to car ECU and process responses
   isotp_msg_openpilot.send(request)
-  panda.msg = panda.tx_msgs.pop()  # put message to tx in recv buffer
+  panda.rx_msg = panda.tx_msgs.pop()  # put message to tx in recv buffer
 
   while not (isotp_msg_openpilot.rx_done and isotp_msg_openpilot.rx_done):
     # car ECU receives OP's message
@@ -64,18 +43,18 @@ def simulate_isotp_comms(tx_addr: int, rx_addr: int, request: bytes, response: b
 
     if msg_from_op is None:
       # Car ECU is either sending a consecutive frame or a flow control continue to OP
-      panda.msg = panda.tx_msgs.pop()
+      panda.rx_msg = panda.tx_msgs.pop()
 
     else:
       # Message complete from openpilot, now respond
       resp_sid = msg_from_op[0] if len(msg_from_op) > 0 else None
       if response is not None:
         isotp_msg_ecu.send(response)
-        panda.msg = panda.tx_msgs.pop()  # update rx message for OP to receive
+        panda.rx_msg = panda.tx_msgs.pop()  # update rx message for OP to receive
 
       elif resp_sid == SERVICE_TYPE.TESTER_PRESENT:
         isotp_msg_ecu.send(bytes([SERVICE_TYPE.TESTER_PRESENT + 0x40]))
-        panda.msg = panda.tx_msgs.pop()  # update rx message for OP to receive
+        panda.rx_msg = panda.tx_msgs.pop()  # update rx message for OP to receive
 
       else:
         raise Exception(f"Service id not supported: {resp_sid}")
@@ -87,7 +66,7 @@ def simulate_isotp_comms(tx_addr: int, rx_addr: int, request: bytes, response: b
       return msg
 
     # openpilot is either sending a consecutive frame or a flow control continue to car ECU
-    panda.msg = panda.tx_msgs.pop()
+    panda.rx_msg = panda.tx_msgs.pop()
 
 
 @parameterized_class([
