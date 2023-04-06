71b84bc2bbc7d55bd2f2b6b4543cab52ffa32b60
Robbe Derks <robbe.derks@gmail.com>
2023-04-06 13:05:39 +0200

    small cleanup

--- tests/hitl/7_embedded.py
+++ tests/hitl/7_embedded.py
@@ -1,17 +1,16 @@
 import time
 import pytest
 
-from panda.tests.hitl.conftest import PandaGroup
+from panda import Panda
 
-@pytest.mark.test_panda_types(PandaGroup.EMBEDDED)
+@pytest.mark.test_panda_types(Panda.INTERNAL_DEVICES)
 def test_fan_controller(p):
   for power, expected_rpm in [(50, 3250), (100, 6500)]:
     p.set_fan_power(power)
     time.sleep(10)
     assert 0.95 * expected_rpm <= p.get_fan_rpm() <= 1.05 * expected_rpm
-  p.set_fan_power(0)
 
-@pytest.mark.test_panda_types(PandaGroup.EMBEDDED)
+@pytest.mark.test_panda_types(Panda.INTERNAL_DEVICES)
 def test_fan_cooldown(p):
   # if the fan cooldown doesn't work, we get high frequency noise on the tach line 
   # while the rotor spins down. this makes sure it never goes beyond the expected max RPM
