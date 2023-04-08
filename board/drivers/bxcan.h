43e4f5517808283dc57a06f34bf29b91ec984cb3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-04-08 16:39:39 -0700

    safety: only pass addr to fwd hook

--- board/drivers/bxcan.h
+++ board/drivers/bxcan.h
@@ -191,7 +191,7 @@ void can_rx(uint8_t can_number) {
     can_set_checksum(&to_push);
 
     // forwarding (panda only)
-    int bus_fwd_num = safety_fwd_hook(bus_number, &to_push);
+    int bus_fwd_num = safety_fwd_hook(bus_number, to_push.addr);
     if (bus_fwd_num != -1) {
       CANPacket_t to_send;
 
