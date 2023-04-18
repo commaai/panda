6d4c1a1768c1dc5c972ee7510b673bd74954155f
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-04-18 13:55:14 -0700

    update clip

--- board/drivers/fan.h
+++ board/drivers/fan.h
@@ -48,7 +48,7 @@ void fan_tick(void){
           current_board->set_fan_enabled(false);
 
           // clip integral, can't fully reset otherwise we may always be stuck in stall detection
-          fan_state.error_integral = MIN(50.0f, MAX(0.0f, fan_state.error_integral));
+          fan_state.error_integral = MIN(0.0f, fan_state.error_integral);
         }
       } else {
         fan_state.stall_counter = 0U;
