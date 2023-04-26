9b223e902940e875fc58c0b88edfa3292df466b9
Shane Smiskol <shane@smiskol.com>
2023-04-25 19:13:31 -0700

    checksum matches except it goes to zero when turning the car off despite counter still active

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -87,8 +87,9 @@ static uint32_t ford_compute_checksum(CANPacket_t *to_push) {
     chksum += (GET_BYTE(to_push, 2) >> 2) & 0xFU;           // VehVActlBrk_No_Cnt
     chksum = 0xFFU - chksum;
   } else if (addr == MSG_EngVehicleSpThrottle2) {
-    // GearRvrse_D_Actl does nothing
-
+    chksum += (GET_BYTE(to_push, 2) >> 3) & 0xf;            // VehVActlEng_No_Cnt
+    chksum += (GET_BYTE(to_push, 4) >> 5) & 0x3;            // VehVActlEng_D_Qf
+    chksum += GET_BYTE(to_push, 6) + GET_BYTE(to_push, 7);  // Veh_V_ActlEng
   } else if (addr == MSG_Yaw_Data_FD1) {
     chksum += GET_BYTE(to_push, 0) + GET_BYTE(to_push, 1);  // VehRol_W_Actl
     chksum += GET_BYTE(to_push, 2) + GET_BYTE(to_push, 3);  // VehYaw_W_Actl
