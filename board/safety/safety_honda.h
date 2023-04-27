7aef934d4cd740dc5f25a64cf2675b2ecf7f7525
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-04-26 22:59:58 -0700

    Fix undefined behavior in GET_BYTES macro (#1367)

--- board/safety/safety_honda.h
+++ board/safety/safety_honda.h
@@ -347,7 +347,7 @@ static int honda_tx_hook(CANPacket_t *to_send) {
 
   // Bosch supplemental control check
   if (addr == 0xE5) {
-    if ((GET_BYTES_04(to_send) != 0x10800004U) || ((GET_BYTES_48(to_send) & 0x00FFFFFFU) != 0x0U)) {
+    if ((GET_BYTES(to_send, 0, 4) != 0x10800004U) || ((GET_BYTES(to_send, 4, 4) & 0x00FFFFFFU) != 0x0U)) {
       tx = 0;
     }
   }
@@ -370,7 +370,7 @@ static int honda_tx_hook(CANPacket_t *to_send) {
 
   // Only tester present ("\x02\x3E\x80\x00\x00\x00\x00\x00") allowed on diagnostics address
   if (addr == 0x18DAB0F1) {
-    if ((GET_BYTES_04(to_send) != 0x00803E02U) || (GET_BYTES_48(to_send) != 0x0U)) {
+    if ((GET_BYTES(to_send, 0, 4) != 0x00803E02U) || (GET_BYTES(to_send, 4, 4) != 0x0U)) {
       tx = 0;
     }
   }
