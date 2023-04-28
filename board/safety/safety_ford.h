b2001b07f803ab994ffdbcd93813e93efe955f59
Shane Smiskol <shane@smiskol.com>
2023-04-27 23:49:22 -0700

    add updated bp

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -137,13 +137,13 @@ const SteeringLimits FORD_STEERING_LIMITS = {
   .max_steer = 1000,
   .angle_deg_to_can = 50000,        // 1 / (2e-5) rad to can
   .max_angle_error = 100,           // 0.002 * FORD_STEERING_LIMITS.angle_deg_to_can
-  .angle_rate_up_lookup = {
-    {5., 15., 25.},
-    {0.004, 0.00044, 0.00016}
+  .angle_rate_up_lookup = {  // Ford only uses two breakpoints
+    {5., 25., 25.},
+    {0.0002, 0.0001, 0.0001}
   },
   .angle_rate_down_lookup = {
-    {5., 15., 25.},
-    {0.006, 0.00066, 0.00024}
+    {5., 25., 25.},
+    {0.000225, 0.00015, 0.00015}
   },
 
   // no blending at low speed due to lack of torque wind-up and inaccurate current curvature
