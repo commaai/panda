07680692d99c2856e5fff6e01f5bf0d3d49c88f6
Shane Smiskol <shane@smiskol.com>
2023-04-27 17:27:18 -0700

    Ford: add subtests (#1371)
    * subtest here
    
    * and here
    
    * bit faster

--- tests/safety/test_ford.py
+++ tests/safety/test_ford.py
@@ -228,21 +228,25 @@ class TestFordSafety(common.PandaSafetyTest):
 
                 should_tx = path_offset == 0 and path_angle == 0 and curvature_rate == 0
                 should_tx = should_tx and (not enabled or controls_allowed)
-                self.assertEqual(should_tx, self._tx(self._tja_command_msg(steer_control_enabled, path_offset, path_angle, curvature, curvature_rate)))
+                with self.subTest(controls_allowed=controls_allowed, steer_control_enabled=steer_control_enabled,
+                                  path_offset=path_offset, path_angle=path_angle, curvature_rate=curvature_rate,
+                                  curvature=curvature):
+                  self.assertEqual(should_tx, self._tx(self._tja_command_msg(steer_control_enabled, path_offset, path_angle, curvature, curvature_rate)))
 
   def test_steer_meas_delta(self):
     """This safety model enforces a maximum distance from measured and commanded curvature, only above a certain speed"""
     self.safety.set_controls_allowed(1)
 
     for speed in np.linspace(0, 50, 11):
-      for initial_curvature in np.linspace(-self.MAX_CURVATURE, self.MAX_CURVATURE, 51):
+      for initial_curvature in np.linspace(-self.MAX_CURVATURE, self.MAX_CURVATURE, 21):
         self._reset_curvature_measurements(initial_curvature, speed)
 
         limit_command = speed > self.CURVATURE_DELTA_LIMIT_SPEED
-        for new_curvature in np.linspace(-self.MAX_CURVATURE, self.MAX_CURVATURE, 51):
+        for new_curvature in np.linspace(-self.MAX_CURVATURE, self.MAX_CURVATURE, 41):
           too_far_away = round_curvature_can(abs(new_curvature - initial_curvature)) > self.MAX_CURVATURE_DELTA
           should_tx = not limit_command or not too_far_away
-          self.assertEqual(should_tx, self._tx(self._tja_command_msg(True, 0, 0, new_curvature, 0)))
+          with self.subTest(speed=speed, initial_curvature=initial_curvature, new_curvature=new_curvature):
+            self.assertEqual(should_tx, self._tx(self._tja_command_msg(True, 0, 0, new_curvature, 0)))
 
   def test_prevent_lkas_action(self):
     self.safety.set_controls_allowed(1)
