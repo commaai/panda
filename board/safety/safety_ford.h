0afa122303aa7504024670d6dd83e86bedd41dfc
Shane Smiskol <shane@smiskol.com>
2023-05-02 21:52:46 -0700

    misra pt 1

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -163,7 +163,7 @@ static int ford_rx_hook(CANPacket_t *to_push) {
     // Update vehicle speed
     if (addr == MSG_BrakeSysFeatures) {
       // Signal: Veh_V_ActlBrk
-      update_sample(&vehicle_speed, ((GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1)) * 0.01 / 3.6 * 100. + 0.5);
+      update_sample(&vehicle_speed, (((GET_BYTE(to_push, 0) << 8) | GET_BYTE(to_push, 1)) * 0.01 / 3.6 * 100.) + 0.5);
     }
 
     // Check vehicle speed against a second source
