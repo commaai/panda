32988398ba021645775a2a6c921c5f4354fc8305
Shane Smiskol <shane@smiskol.com>
2023-05-18 04:24:34 -0700

    whoops

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -284,7 +284,7 @@ static int ford_tx_hook(CANPacket_t *to_send) {
     violation |= longitudinal_gas_checks(gas, FORD_LONG_LIMITS);
 
     // Safety checks for stock AEB
-    violation |= cmbb_deny != 1U; // do not disable stock AEB
+    violation |= cmbb_deny != 0U; // do not disable stock AEB
     if (ford_stock_aeb) {
       violation |= accel != FORD_LONG_LIMITS.inactive_accel;
       violation |= gas != FORD_LONG_LIMITS.inactive_gas;
