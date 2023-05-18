2da8b80de7791001973c7a84654226bb5e921e25
Shane Smiskol <shane@smiskol.com>
2023-05-17 17:41:53 -0700

    Revert "delete useless setUpClass defs"
    This reverts commit f1c32f9c749ee85e84762e4e20a460798221b36f.

--- tests/safety/common.py
+++ tests/safety/common.py
@@ -53,14 +53,10 @@ def add_regen_tests(cls):
 
 
 class PandaSafetyTestBase(unittest.TestCase):
-  packer: CANPackerPanda
-  safety: libpanda_py.Panda
-
   @classmethod
   def setUpClass(cls):
     if cls.__name__ == "PandaSafetyTestBase":
-      # cls.safety = None
-      # cls.packer = None
+      cls.safety = None
       raise unittest.SkipTest
 
   def _reset_safety_hooks(self):
