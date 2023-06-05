e33178eae18d65612e3dba3ac7e3f2a5da18b88e
Justin Newberry <jnewberry0502@gmail.com>
2023-06-05 04:25:25 +0000

    revert panda stuff

--- board/safety/safety_subaru.h
+++ board/safety/safety_subaru.h
@@ -20,6 +20,15 @@ const SteeringLimits SUBARU_GEN2_STEERING_LIMITS = {
   .type = TorqueDriverLimited,
 };
 
+const LongitudinalLimits SUBARU_LONG_LIMITS = {
+  .min_gas = 0,
+  .max_gas = 3400,
+  .min_rpm = 0,
+  .max_rpm = 3200,
+  .max_brake = 400,
+};
+
+
 const CanMsg SUBARU_TX_MSGS[] = {
   {0x122, 0, 8},
   {0x221, 0, 8},
@@ -29,6 +38,19 @@ const CanMsg SUBARU_TX_MSGS[] = {
 };
 #define SUBARU_TX_MSGS_LEN (sizeof(SUBARU_TX_MSGS) / sizeof(SUBARU_TX_MSGS[0]))
 
+const CanMsg SUBARU_LONG_TX_MSGS[] = {
+  {0x122, 0, 8},
+  {0x220, 0, 8},
+  {0x221, 0, 8},
+  {0x222, 0, 8},
+  {0x321, 0, 8},
+  {0x322, 0, 8},
+  {0x323, 0, 8},
+  {0x13c, 2, 8},
+  {0x240, 2, 8}
+};
+#define SUBARU_LONG_TX_MSGS_LEN (sizeof(SUBARU_LONG_TX_MSGS) / sizeof(SUBARU_LONG_TX_MSGS[0]))
+
 const CanMsg SUBARU_GEN2_TX_MSGS[] = {
   {0x122, 0, 8},
   {0x221, 1, 8},
@@ -37,6 +59,24 @@ const CanMsg SUBARU_GEN2_TX_MSGS[] = {
   {0x323, 0, 8}
 };
 #define SUBARU_GEN2_TX_MSGS_LEN (sizeof(SUBARU_GEN2_TX_MSGS) / sizeof(SUBARU_GEN2_TX_MSGS[0]))
+    
+const CanMsg SUBARU_GEN2_LONG_TX_MSGS[] = {
+  {0x122, 0, 8},
+  {0x220, 1, 8},
+  {0x221, 1, 8},
+  {0x222, 1, 8},
+  {0x321, 0, 8},
+  {0x322, 0, 8},
+  {0x323, 0, 8},
+  {0x13c, 2, 8},
+  {0x240, 2, 8},
+  {0x787, 2, 8},
+
+  {0x121, 0, 8},
+  {0x22a, 0, 8},
+  {0x325, 0, 8}
+};
+#define SUBARU_GEN2_LONG_TX_MSGS_LEN (sizeof(SUBARU_GEN2_LONG_TX_MSGS) / sizeof(SUBARU_GEN2_LONG_TX_MSGS[0]))
 
 AddrCheckStruct subaru_addr_checks[] = {
   {.msg = {{ 0x40, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
@@ -58,10 +98,28 @@ AddrCheckStruct subaru_gen2_addr_checks[] = {
 #define SUBARU_GEN2_ADDR_CHECK_LEN (sizeof(subaru_gen2_addr_checks) / sizeof(subaru_gen2_addr_checks[0]))
 addr_checks subaru_gen2_rx_checks = {subaru_gen2_addr_checks, SUBARU_GEN2_ADDR_CHECK_LEN};
 
+AddrCheckStruct subaru_gen2_long_addr_checks[] = {
+  {.msg = {{ 0x40, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 10000U}, { 0 }, { 0 }}},
+  {.msg = {{0x119, 0, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{0x13a, 1, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{0x13c, 1, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 20000U}, { 0 }, { 0 }}},
+  {.msg = {{0x240, 1, 8, .check_checksum = true, .max_counter = 15U, .expected_timestep = 50000U}, { 0 }, { 0 }}},
+  {.msg = {{0x78f, 2, 8, .check_checksum = false, .max_counter = 0U, .expected_timestep = 50000U}, { 0 }, { 0 }}},
+};
+#define SUBARU_GEN2_LONG_ADDR_CHECK_LEN (sizeof(subaru_gen2_long_addr_checks) / sizeof(subaru_gen2_long_addr_checks[0]))
+addr_checks subaru_gen2_long_rx_checks = {subaru_gen2_long_addr_checks, SUBARU_GEN2_LONG_ADDR_CHECK_LEN};
+
 
 const uint16_t SUBARU_PARAM_GEN2 = 1;
-bool subaru_gen2 = false;
+const uint16_t SUBARU_PARAM_LONGITUDINAL = 2;
 
+bool subaru_gen2 = false;
+bool subaru_longitudinal = false;
+
+bool subaru_aeb = false;
+
+bool set_button_prev = false;
+bool resume_button_prev = false;
 
 static uint32_t subaru_get_checksum(CANPacket_t *to_push) {
   return (uint8_t)GET_BYTE(to_push, 0);
@@ -89,6 +147,7 @@ static int subaru_rx_hook(CANPacket_t *to_push) {
   if (valid) {
     const int bus = GET_BUS(to_push);
     const int alt_bus = subaru_gen2 ? 1 : 0;
+    const int alt_bus2 = subaru_gen2 ? 1 : 2;
 
     int addr = GET_ADDR(to_push);
     if ((addr == 0x119) && (bus == 0)) {
@@ -98,9 +157,15 @@ static int subaru_rx_hook(CANPacket_t *to_push) {
       update_sample(&torque_driver, torque_driver_new);
     }
 
+    // ES_LKAS_State LKAS_Alert: 5, 6
+    if ((addr == 0x322) && (bus == alt_bus2)) {
+      const int lkas_alert = ((GET_BYTE(to_push, 4) >> 3) & 5U);
+      subaru_aeb = ((lkas_alert == 5) || (lkas_alert == 6));
+    }
+
     // enter controls on rising edge of ACC, exit controls on ACC off
-    if ((addr == 0x240) && (bus == alt_bus)) {
-      bool cruise_engaged = GET_BIT(to_push, 41U) != 0U;
+    if ((addr == 0x241) && (bus == alt_bus)) {
+      bool cruise_engaged = GET_BIT(to_push, 55U) != 0U;
       pcm_cruise_check(cruise_engaged);
     }
 
@@ -110,7 +175,7 @@ static int subaru_rx_hook(CANPacket_t *to_push) {
     }
 
     if ((addr == 0x13c) && (bus == alt_bus)) {
-      brake_pressed = ((GET_BYTE(to_push, 7) >> 6) & 1U);
+      brake_pressed = GET_BIT(to_push, 62U) != 0U;
     }
 
     if ((addr == 0x40) && (bus == 0)) {
@@ -126,13 +191,25 @@ static int subaru_tx_hook(CANPacket_t *to_send) {
 
   int tx = 1;
   int addr = GET_ADDR(to_send);
+  bool violation = false;
 
   if (subaru_gen2) {
-    tx = msg_allowed(to_send, SUBARU_GEN2_TX_MSGS, SUBARU_GEN2_TX_MSGS_LEN);
-  } else {
-    tx = msg_allowed(to_send, SUBARU_TX_MSGS, SUBARU_TX_MSGS_LEN);
+    if (subaru_longitudinal) {
+      tx = msg_allowed(to_send, SUBARU_GEN2_LONG_TX_MSGS, SUBARU_GEN2_LONG_TX_MSGS_LEN);
+    }
+    else {
+      tx = msg_allowed(to_send, SUBARU_GEN2_TX_MSGS, SUBARU_GEN2_TX_MSGS_LEN);
+    }
   }
-
+  else {
+    if (subaru_longitudinal) {
+      tx = msg_allowed(to_send, SUBARU_LONG_TX_MSGS, SUBARU_LONG_TX_MSGS_LEN);
+    }
+    else {
+      tx = msg_allowed(to_send, SUBARU_TX_MSGS, SUBARU_TX_MSGS_LEN);
+    }
+  }
+ 
   // steer cmd checks
   if (addr == 0x122) {
     int desired_torque = ((GET_BYTES(to_send, 0, 4) >> 16) & 0x1FFFU);
@@ -142,7 +219,44 @@ static int subaru_tx_hook(CANPacket_t *to_send) {
     if (steer_torque_cmd_checks(desired_torque, -1, limits)) {
       tx = 0;
     }
+  }
 
+  if (subaru_longitudinal && controls_allowed) {
+    // check es_brake brake_pressure limits
+    if (addr == 0x220) {
+      int es_brake_pressure = ((GET_BYTES(to_send, 0, 4) >> 16) & 0xFFFFU);
+      violation |= !subaru_aeb && longitudinal_brake_checks(es_brake_pressure, SUBARU_LONG_LIMITS);
+    }
+
+    // check es_distance cruise_throttle limits
+    if ((addr == 0x221) && !gas_pressed) {
+      int cruise_throttle = ((GET_BYTES(to_send, 0, 4) >> 16) & 0xFFFU);
+      violation |= longitudinal_gas_checks(cruise_throttle, SUBARU_LONG_LIMITS);
+    }
+
+    // check es_status cruise_rpm limits
+    if ((addr == 0x222) && !gas_pressed) {
+      int cruise_rpm = ((GET_BYTES(to_send, 0, 4) >> 16) & 0xFFFU);
+      if (get_longitudinal_allowed()) {
+        violation |= max_limit_check(cruise_rpm, SUBARU_LONG_LIMITS.max_rpm, SUBARU_LONG_LIMITS.min_rpm);
+      } else {
+        violation |= cruise_rpm != 0;
+      }
+    }
+  }
+
+  // UDS safety checks
+  if (addr == 0x787){
+    int sid = GET_BYTES(to_send, 1, 1);
+
+    // Allow "tester present" and "read data by identifier"
+    bool allowed_sid = (sid == 0x3e) || (sid == 0x22);
+
+    violation |= (!allowed_sid);
+  }
+
+  if (violation) {
+    tx = 0;
   }
   return tx;
 }
@@ -151,17 +265,28 @@ static int subaru_fwd_hook(int bus_num, int addr) {
   int bus_fwd = -1;
 
   if (bus_num == 0) {
-    bus_fwd = 2;  // forward to camera
+    // Global Platform
+    // 0x13c is Brake_Status
+    // 0x240 is CruiseControl
+    bool block_msg = subaru_longitudinal && ((addr == 0x13c) || (addr == 0x240));
+    if (!block_msg) {
+      bus_fwd = 2;  // to the eyesight camera
+    }
   }
 
   if (bus_num == 2) {
-    // Global platform
-    // 0x122 ES_LKAS
-    // 0x321 ES_DashStatus
-    // 0x322 ES_LKAS_State
+    // Global Platform
+    // 0x122 is ES_LKAS
+    // 0x220 is ES_Brake
+    // 0x221 is ES_Distance
+    // 0x222 is ES_Status
+    // 0x321 is ES_DashStatus
+    // 0x322 is ES_LKAS_State
     // 0x323 INFOTAINMENT_STATUS
-    bool block_lkas = (addr == 0x122) || (addr == 0x321) || (addr == 0x322) || (addr == 0x323);
-    if (!block_lkas) {
+    bool block_common = (addr == 0x122) || (addr == 0x321) || (addr == 0x322) || (addr == 0x323);
+    bool block_long = (addr == 0x220) || (addr == 0x221) || (addr == 0x222);
+    bool block_msg = block_common || (subaru_longitudinal && block_long);
+    if (!block_msg) {
       bus_fwd = 0;  // Main CAN
     }
   }
@@ -172,8 +297,17 @@ static int subaru_fwd_hook(int bus_num, int addr) {
 static const addr_checks* subaru_init(uint16_t param) {
   subaru_gen2 = GET_FLAG(param, SUBARU_PARAM_GEN2);
 
+#ifdef ALLOW_DEBUG
+  subaru_longitudinal = GET_FLAG(param, SUBARU_PARAM_LONGITUDINAL);
+#endif
+
   if (subaru_gen2) {
-    subaru_rx_checks = (addr_checks){subaru_gen2_addr_checks, SUBARU_GEN2_ADDR_CHECK_LEN};
+    if(subaru_longitudinal){
+      subaru_rx_checks = (addr_checks){subaru_gen2_long_addr_checks, SUBARU_GEN2_LONG_ADDR_CHECK_LEN};
+    }
+    else{
+      subaru_rx_checks = (addr_checks){subaru_gen2_addr_checks, SUBARU_GEN2_ADDR_CHECK_LEN};
+    }
   } else {
     subaru_rx_checks = (addr_checks){subaru_addr_checks, SUBARU_ADDR_CHECK_LEN};
   }
@@ -188,5 +322,3 @@ const safety_hooks subaru_hooks = {
   .tx_lin = nooutput_tx_lin_hook,
   .fwd = subaru_fwd_hook,
 };
-
-
