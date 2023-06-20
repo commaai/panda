411815069b0880939877e0d5d220eed712b51fb3
Igor Biletksyy <bs@privacy.im>
2023-06-20 11:35:05 -0700

    fix typo

--- board/drivers/fdcan.h
+++ board/drivers/fdcan.h
@@ -64,7 +64,7 @@ void update_can_health_pkt(uint8_t can_number, bool error_irq) {
       can_health[can_number].total_tx_lost_cnt += 1U;
     }
     // Actually reset can core only on arbitration or data phase errors and when CEL couter reaches at least 100 errors
-    if (((CANx->IR & (FDCAN_IR_PED | FDCAN_IR_PEA)) != 0) && (((ecr_reg & FDCAN_ECR_CEL) >> FDCAN_ECR_CEL_Pos) == 100U)) {
+    if (((CANx->IR & (FDCAN_IR_PED | FDCAN_IR_PEA)) != 0) && (((ecr_reg & FDCAN_ECR_CEL) >> FDCAN_ECR_CEL_Pos) >= 100U)) {
       llcan_clear_send(CANx);
     }
     // Clear error interrupts
