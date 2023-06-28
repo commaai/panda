ef5b63be17d12795ea8bc8651c080c61b9988608
Igor Biletksyy <bs@privacy.im>
2023-06-28 13:33:49 -0700

    comment

--- board/drivers/fdcan.h
+++ board/drivers/fdcan.h
@@ -72,7 +72,7 @@ void update_can_health_pkt(uint8_t can_number, uint32_t ir_reg) {
     if ((ir_reg & (FDCAN_IR_RF0L)) != 0) {
       can_health[can_number].total_rx_lost_cnt += 1U;
     }
-    // Reset CAN core when TX error couter reaches at least 100 errors
+    // Reset CAN core when CEL couter reaches at least 100 errors
     if (((ir_reg & (FDCAN_IR_PED | FDCAN_IR_PEA)) != 0) && (((ecr_reg & FDCAN_ECR_CEL) >> FDCAN_ECR_CEL_Pos) >= 100U)) {
       can_health[can_number].can_core_reset_cnt += 1U;
       can_health[can_number].total_tx_lost_cnt += (FDCAN_TX_FIFO_EL_CNT - (FDCAN_TXFQS_TFFL & FDCAN_TXFQS_TFFL_Msk)); // TX FIFO msgs will be lost after reset
@@ -81,7 +81,6 @@ void update_can_health_pkt(uint8_t can_number, uint32_t ir_reg) {
     // Clear error interrupts
     CANx->IR |= (FDCAN_IR_PED | FDCAN_IR_PEA | FDCAN_IR_EP | FDCAN_IR_BO | FDCAN_IR_RF0L);
   }
-
   EXIT_CRITICAL();
 }
 
