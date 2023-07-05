b0f2cea48c3a96b7352d3f7e397012e37cf5ffb3
Aydan Ghazani <ghazani.aydan@gmail.com>
2023-07-05 19:19:19 +0000

    Add mypy github action

--- python/base.py
+++ python/base.py
@@ -49,7 +49,7 @@ class BaseSTBootloaderHandle(ABC):
     ...
 
   @abstractmethod
-  def program(self, address: int, dat: bytes) -> None:
+  def program(self, address: int, dat: bytes) -> any:
     ...
 
   @abstractmethod
