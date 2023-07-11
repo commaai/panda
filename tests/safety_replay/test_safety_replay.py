78f6d34cce637a9291fccc51a9c2522a6b693bb9
Justin Newberry <justin@comma.ai>
2023-07-11 09:39:47 -0700

    need to upload this route

--- tests/safety_replay/test_safety_replay.py
+++ tests/safety_replay/test_safety_replay.py
@@ -18,6 +18,8 @@ logs = [
   ReplayRoute("f89c604cf653e2bf|2018-09-29--13-46-50.bz2", Panda.SAFETY_GM),                  # GM.VOLT
   ReplayRoute("6fb4948a7ebe670e|2019-11-12--00-35-53.bz2", Panda.SAFETY_CHRYSLER),            # CHRYSLER.PACIFICA_2018_HYBRID
   ReplayRoute("0744286ead2fbb96|2023-05-01--16-27-01--35--rlog.bz2", Panda.SAFETY_SUBARU),    # SUBARU.IMPREZA
+  ReplayRoute("8de015561e1ea4a0|2023-07-02--20-14-07--13--rlog.bz2", Panda.SAFETY_SUBARU,     # SUBARU.CROSSTREK (openpilot long)
+              Panda.FLAG_SUBARU_LONG),    
   ReplayRoute("bad6ae3584ece5b5|2023-04-29--11-23-48--7--rlog.bz2", Panda.SAFETY_SUBARU,      # SUBARU.OUTBACK
               Panda.FLAG_SUBARU_GEN2, ALT_EXP.DISABLE_DISENGAGE_ON_GAS),
   ReplayRoute("76b83eb0245de90e|2020-03-05--19-16-05.bz2", Panda.SAFETY_VOLKSWAGEN_MQB),      # VOLKSWAGEN.GOLF (stock ACC)
@@ -28,7 +30,7 @@ logs = [
   ReplayRoute("fbbfa6af821552b9|2020-03-03--08-09-43.bz2", Panda.SAFETY_NISSAN),              # NISSAN.XTRAIL
   ReplayRoute("5b7c365c50084530_2020-04-15--16-13-24--3--rlog.bz2", Panda.SAFETY_HYUNDAI),    # HYUNDAI.SONATA
   ReplayRoute("610ebb9faaad6b43|2020-06-13--15-28-36.bz2", Panda.SAFETY_HYUNDAI_LEGACY),      # HYUNDAI.IONIQ_EV_LTD
-  ReplayRoute("5ab784f361e19b78_2020-06-08--16-30-41.bz2", Panda.SAFETY_SUBARU_PREGLOBAL),       # SUBARU.OUTBACK_PREGLOBAL
+  ReplayRoute("5ab784f361e19b78_2020-06-08--16-30-41.bz2", Panda.SAFETY_SUBARU_PREGLOBAL),    # SUBARU.OUTBACK_PREGLOBAL
   ReplayRoute("bb50caf5f0945ab1|2021-06-19--17-20-18.bz2", Panda.SAFETY_TESLA),               # TESLA.AP2_MODELS
   ReplayRoute("bd6a637565e91581_2021-10-29--22-18-31--1--rlog.bz2", Panda.SAFETY_MAZDA),      # MAZDA.CX9_2021
   ReplayRoute("1a5d045d2c531a6d_2022-06-07--22-03-00--1--rlog.bz2", Panda.SAFETY_HONDA_BOSCH, # HONDA.CIVIC_2022
