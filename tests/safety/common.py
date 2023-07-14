d05ec9c8f6f44b6be3aad631576f7d8e456394a7
Justin Newberry <justin@comma.ai>
2023-07-13 21:51:14 -0700

    Update common.py

--- tests/safety/common.py
+++ tests/safety/common.py
@@ -190,7 +190,7 @@ class TorqueSteeringSafetyTestBase(PandaSafetyTestBase, abc.ABC):
 
   @classmethod
   def setUpClass(cls):
-    super().setUpClass(cls)
+    super().setUpClass()
     if cls.__name__ == "TorqueSteeringSafetyTestBase":
       cls.safety = None
       raise unittest.SkipTest
@@ -336,7 +336,7 @@ class DriverTorqueSteeringSafetyTest(TorqueSteeringSafetyTestBase, abc.ABC):
 
   @classmethod
   def setUpClass(cls):
-    super().setUpClass(cls)
+    super().setUpClass()
     if cls.__name__ == "DriverTorqueSteeringSafetyTest":
       cls.safety = None
       raise unittest.SkipTest
