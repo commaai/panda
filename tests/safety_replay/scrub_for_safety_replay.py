1ca34c9c1df0ec9c15a9f428134570ff306bd40b
Justin Newberry <justin@comma.ai>
2023-07-14 14:38:53 -0700

    remove scrubber for now

--- tests/safety_replay/scrub_for_safety_replay.py
+++
@@ -1,5 +0,0 @@
-# Replace with input raw RLOG
-PATH_TO_RAW_RLOG = "/home/batman/Downloads/e886087f430e7fe7_2023-06-16--23-06-36--10--rlog.bz2"
-
-PATH_TO_RAW_RLOG = "/home/batman/Downloads/e886087f430e7fe7_2023-06-16--23-06-36--10--rlog.bz2"
-
