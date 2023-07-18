c3c81768af4b5a12c70237cc3c866cddd0aea8e9
Igor Biletksyy <bs@privacy.im>
2023-07-17 20:00:53 -0700

    is it test_reliability only?

--- tests/hitl/3_usb_to_can.py
+++ tests/hitl/3_usb_to_can.py
@@ -1,4 +1,4 @@
-import sys
+# import sys
 import time
 import pytest
 from flaky import flaky
@@ -30,43 +30,43 @@ def test_can_loopback(p):
     assert 0x1aa == sr[0][0] == lb[0][0]
     assert b"message" == sr[0][2] == lb[0][2]
 
-def test_reliability(p):
-  MSG_COUNT = 100
+# def test_reliability(p):
+#   MSG_COUNT = 100
 
-  p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
-  p.set_can_loopback(True)
-  p.set_can_speed_kbps(0, 1000)
+#   p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+#   p.set_can_loopback(True)
+#   p.set_can_speed_kbps(0, 1000)
 
-  # Check warm up idea
-  p.can_send(0x1aa, b"message", 0)
-  time.sleep(0.05)
-  r = p.can_recv()
+#   # Check warm up idea
+#   p.can_send(0x1aa, b"message", 0)
+#   time.sleep(0.05)
+#   r = p.can_recv()
 
-  addrs = list(range(100, 100 + MSG_COUNT))
-  ts = [(j, 0, b"\xaa" * 8, 0) for j in addrs]
+#   addrs = list(range(100, 100 + MSG_COUNT))
+#   ts = [(j, 0, b"\xaa" * 8, 0) for j in addrs]
 
-  for _ in range(100):
-    st = time.monotonic()
+#   for _ in range(100):
+#     st = time.monotonic()
 
-    p.can_send_many(ts)
+#     p.can_send_many(ts)
 
-    r = []
-    while len(r) < 200 and (time.monotonic() - st) < 0.5:
-      r.extend(p.can_recv())
+#     r = []
+#     while len(r) < 200 and (time.monotonic() - st) < 0.5:
+#       r.extend(p.can_recv())
 
-    sent_echo = [x for x in r if x[3] == 0x80]
-    loopback_resp = [x for x in r if x[3] == 0]
+#     sent_echo = [x for x in r if x[3] == 0x80]
+#     loopback_resp = [x for x in r if x[3] == 0]
 
-    assert sorted([x[0] for x in loopback_resp]) == addrs
-    assert sorted([x[0] for x in sent_echo]) == addrs
-    assert len(r) == 200
+#     assert sorted([x[0] for x in loopback_resp]) == addrs
+#     assert sorted([x[0] for x in sent_echo]) == addrs
+#     assert len(r) == 200
 
-    # take sub 20ms
-    et = (time.monotonic() - st) * 1000.0
-    assert et < 20
+#     # take sub 20ms
+#     et = (time.monotonic() - st) * 1000.0
+#     assert et < 20
 
-    sys.stdout.write("P")
-    sys.stdout.flush()
+#     sys.stdout.write("P")
+#     sys.stdout.flush()
 
 @flaky(max_runs=6, min_passes=1)
 def test_throughput(p):
