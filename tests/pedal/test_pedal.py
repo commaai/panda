977d702770ec8eb0701cd2c7e20a93804fe16bcd
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-08-04 10:40:53 -0700

    cleanup old pedal stuff (#1552)

--- tests/pedal/test_pedal.py
+++ tests/pedal/test_pedal.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 import time
-import subprocess
 import unittest
 
 from panda import Panda, PandaJungle, CanHandle, McuType, BASEDIR
@@ -44,7 +43,6 @@ class TestPedal(unittest.TestCase):
     return msgs
 
   def test_usb_fw(self):
-    subprocess.check_output(f"cd {BASEDIR} && PEDAL=1 PEDAL_USB=1 scons", shell=True)
     self._flash_over_can(PEDAL_BUS, f"{BASEDIR}/board/pedal/obj/pedal_usb.bin.signed")
     time.sleep(2)
     with Panda(PEDAL_SERIAL) as p:
@@ -52,7 +50,6 @@ class TestPedal(unittest.TestCase):
     self.assertTrue(self._listen_can_frames() > 40)
 
   def test_nonusb_fw(self):
-    subprocess.check_output(f"cd {BASEDIR} && PEDAL=1 scons", shell=True)
     self._flash_over_can(PEDAL_BUS, f"{BASEDIR}board/pedal/obj/pedal.bin.signed")
     time.sleep(2)
     self.assertTrue(self._listen_can_frames() > 40)
