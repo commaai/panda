3be52d88f7e494635f6a179807f10b15eaa0df84
Bruce Wayne <batman@comma.ai>
2023-08-06 20:12:01 -0700

    fail on first

--- tests/hitl/run_parallel_tests.sh
+++ tests/hitl/run_parallel_tests.sh
@@ -5,4 +5,4 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
 # n = number of pandas tested
-PARALLEL=1 pytest --durations=0 *.py -n 5 --dist loadgroup
+PARALLEL=1 pytest --durations=0 *.py -n 5 --dist loadgroup -x
--- tests/hitl/run_serial_tests.sh
+++ tests/hitl/run_serial_tests.sh
@@ -4,4 +4,4 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-NON_PARALLEL=1 pytest --durations=0 *.py
+NON_PARALLEL=1 pytest --durations=0 *.py -x
