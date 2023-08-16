f4e63e7da5e5f519c504eac3638be9b320769f47
Justin Newberry <justin@comma.ai>
2023-08-15 17:25:44 -0700

    Safety: Rename alt transmission rpm (#1583)
    * renamre transmision rpm
    
    * consistent whitespace
    
    * rename declarations too

--- board/safety_declarations.h
+++ board/safety_declarations.h
@@ -90,10 +90,10 @@ typedef struct {
   const int inactive_gas;
   const int max_brake;
 
-  // alternative cmd limits, ex: transmission rpm on subaru
-  const int max_alt;
-  const int min_alt;
-  const int inactive_alt;
+  // transmission rpm limits
+  const int max_transmission_rpm;
+  const int min_transmission_rpm;
+  const int inactive_transmission_rpm;
 
   // speed cmd limits
   const int inactive_speed;
@@ -168,7 +168,7 @@ bool steer_angle_cmd_checks(int desired_angle, bool steer_control_enabled, const
 bool longitudinal_accel_checks(int desired_accel, const LongitudinalLimits limits);
 bool longitudinal_speed_checks(int desired_speed, const LongitudinalLimits limits);
 bool longitudinal_gas_checks(int desired_gas, const LongitudinalLimits limits);
-bool longitudinal_alt_checks(int desired_alt, const LongitudinalLimits limits);
+bool longitudinal_transmission_rpm_checks(int desired_transmission_rpm, const LongitudinalLimits limits);
 bool longitudinal_brake_checks(int desired_brake, const LongitudinalLimits limits);
 bool longitudinal_interceptor_checks(CANPacket_t *to_send);
 void pcm_cruise_check(bool cruise_engaged);
