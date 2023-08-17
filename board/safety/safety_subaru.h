a675f793b346430abc643515b559d0543e662071
Justin Newberry <jnewberry0502@gmail.com>
2023-08-17 08:20:11 +0000

    better names

--- board/safety/safety_subaru.h
+++ board/safety/safety_subaru.h
@@ -41,8 +41,8 @@ const LongitudinalLimits SUBARU_LONG_LIMITS = {
 #define MSG_SUBARU_ES_UDS_Request        0x787
 
 #define MSG_SUBARU_ES_HighBeamAssist     0x121
-#define MSG_SUBARU_ES_Unknown_2          0x22a
-#define MSG_SUBARU_ES_Unknown_3          0x325
+#define MSG_SUBARU_ES_STATIC_1          0x22a
+#define MSG_SUBARU_ES_STATIC_2          0x325
 
 #define SUBARU_MAIN_BUS 0
 #define SUBARU_ALT_BUS  1
@@ -62,8 +62,8 @@ const LongitudinalLimits SUBARU_LONG_LIMITS = {
 #define SUBARU_ADDITIONAL_TX_MSGS(alt_bus)          \
   {MSG_SUBARU_ES_UDS_Request,    SUBARU_CAM_BUS,  8}, \
   {MSG_SUBARU_ES_HighBeamAssist, SUBARU_MAIN_BUS, 8}, \
-  {MSG_SUBARU_ES_Unknown_2,      SUBARU_MAIN_BUS, 8}, \
-  {MSG_SUBARU_ES_Unknown_3,      SUBARU_MAIN_BUS, 8}, \
+  {MSG_SUBARU_ES_STATIC_1,      SUBARU_MAIN_BUS, 8}, \
+  {MSG_SUBARU_ES_STATIC_2,      SUBARU_MAIN_BUS, 8}, \
 
 
 #define SUBARU_COMMON_ADDR_CHECKS(alt_bus)                                                                                                            \
