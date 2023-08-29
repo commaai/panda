60f062836758e47c35b2df7454bef9c309550197
Shane Smiskol <shane@smiskol.com>
2023-08-29 14:23:46 -0700

    start to cover all cases with lookup table

--- tests/python/test_uds.py
+++ tests/python/test_uds.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
+from typing import *
 from parameterized import parameterized_class
 import unittest
 import struct
 import threading
+from dataclasses import dataclass
 
+from panda.python import uds
 from panda.python.uds import SERVICE_TYPE, DATA_IDENTIFIER_TYPE, IsoTpMessage, UdsClient, get_rx_addr_for_tx_addr
 
-DEFAULT_VIN = '1H3110W0RLD5'
+DEFAULT_VIN_B = b'1H3110W0RLD5'
 
 
 class UdsServer(UdsClient):
@@ -25,12 +28,29 @@ class UdsServer(UdsClient):
     self.uds_thread.join()
 
   def _uds_response(self):
+    # {service_type: {subfunction: response (if True, positive, if bytes, return that)}}  # TODO: maybe just bytes
+    # TODO: might want to use a dataclass so we aren't using dynamically shaped dicts
+    # Dict[SERVICE_TYPE, Dict[bytes, bool | bytes]]
+    lookup = {
+      SERVICE_TYPE.TESTER_PRESENT: {
+        b'\x00': {  # sub function to
+          b'': b'',  # don't expect any extra data, don't respond with extra data
+        },
+      },
+      SERVICE_TYPE.READ_DATA_BY_IDENTIFIER: {
+        None: {  # no subfunction, only responds to data
+          b'\xF1\x00': b'CV1 MFC  AT USA LHD 1.00 1.05 99210-CV000 211027',
+          b'\xF1\x90': DEFAULT_VIN_B,
+        }
+      },
+    }
+
     # send request, wait for response
     max_len = 8 if self.sub_addr is None else 7
     isotp_msg = IsoTpMessage(self._server_can_client, timeout=0, debug=self.debug, max_len=max_len, single_frame_mode=True)
     isotp_msg.send(b"", setup_only=True)
     while not self.kill_event.is_set():
-      resp, _ = isotp_msg.recv(0)
+      resp, _ = isotp_msg.recv(0)  # resp is request from client
       print('UDS Server - got resp', resp)
 
       # message from client not fully built
@@ -40,23 +60,89 @@ class UdsServer(UdsClient):
       # got a message, now respond
       resp_sid = resp[0] if len(resp) > 0 else None
 
-      if resp_sid == SERVICE_TYPE.TESTER_PRESENT:
-        dat = bytes([SERVICE_TYPE.TESTER_PRESENT + 0x40, 0x0])
-        print('Car ECU - sending back:', dat)
-        isotp_msg.send(dat)
-      elif resp_sid == SERVICE_TYPE.READ_DATA_BY_IDENTIFIER:
-        print('Car ECU - READ DATA')
-        assert len(resp) > 1
-        resp_data = struct.unpack('!H', resp[1:])[0]
-        print(resp_data)
-        if resp_data == DATA_IDENTIFIER_TYPE.VIN:
-          dat = bytes([resp_sid + 0x40]) + resp[1:] + DEFAULT_VIN.encode('utf-8')
-          print('Car ECU - sending back:', dat)
-          isotp_msg.send(dat)
-        else:
-          dat = bytes([0x7F, resp_sid, *resp[1:]])
-          print('Car ECU - bad data id, sending back:', dat)
-          isotp_msg.send(dat)
+      if resp_sid in lookup:
+        # get subfunction from request if expected to have one
+        resp_sfn = bytes([resp[1]]) if len(resp) > 1 and None not in lookup[resp_sid] else None
+
+        # service_has_subfunction = None not in lookup[resp_sid]  # len(lookup[resp_sid]) == 1
+        # print(f'{service_has_subfunction=}')
+        if resp_sfn is None:
+          data = resp[(1 if resp_sfn is None else 2):]
+          print('data in service', data, lookup[resp_sid][None])
+          if data in lookup[resp_sid][None]:
+            print('here!')
+            send_dat = bytes([resp_sid + 0x40]) + resp[1:] + lookup[resp_sid][None][data]
+            isotp_msg.send(send_dat)
+          else:
+            # invalid data
+            dat = [0x7F]
+            if resp_sid is not None:
+              dat.append(resp_sid)
+            dat.append(0x31)  # request out of range
+            dat = bytes(dat)
+            print('Car ECU - bad request, sending back:', dat)
+            isotp_msg.send(dat)
+
+        else:  # has subfunction
+          # resp_sfn = resp[1] if len(resp) > 1 else None
+          print('resp_sfn', resp_sfn)
+          if resp_sfn not in lookup[resp_sid]:
+            # return invalid subfunction
+            dat = [0x7F]
+            if resp_sid is not None:  # TODO: should be always true
+              dat.append(resp_sid)
+            dat.append(0x7e)
+            dat = bytes(dat)
+            print('Car ECU - bad subfunction, sending back:', dat.hex())
+            isotp_msg.send(dat)
+
+          else:
+            # valid subfunction, now check data
+            data = resp[(1 if resp_sfn is None else 2):]
+            if data in lookup[resp_sid][resp_sfn]:
+              # valid data
+              send_dat = [resp_sid + 0x40, int.from_bytes(resp_sfn)]
+              if len(lookup[resp_sid][resp_sfn][data]):
+                send_dat.append(lookup[resp_sid][resp_sfn][data])  # add data if exists
+              print('send_dat', send_dat, resp_sid)
+              send_dat = bytes(send_dat)
+              isotp_msg.send(send_dat)
+
+            # # send_dat = [resp_sid + 0x40, resp_sfn]
+            # # if len(lookup[resp_sid][resp_sfn]):
+            # #   send_dat.append(int.from_bytes(lookup[resp_sid][bytes([resp_sfn])]))  # add data if exists
+            # print('send_dat', send_dat)
+            # send_dat = bytes(send_dat)
+            # print('sending', send_dat)
+            # isotp_msg.send(send_dat)
+            # # something
+            else:
+              # invalid data
+              dat = [0x7F]
+              if resp_sid is not None:
+                dat.append(resp_sid)
+              dat = bytes(dat)
+              print('Car ECU - bad request, sending back:', dat)
+              isotp_msg.send(dat)
+        # print(resp_sid, resp.hex(), 'in lookup')
+
+      # if resp_sid == SERVICE_TYPE.TESTER_PRESENT:
+      #   dat = bytes([SERVICE_TYPE.TESTER_PRESENT + 0x40, 0x0])
+      #   print('Car ECU - sending back:', dat)
+      #   isotp_msg.send(dat)
+      # elif resp_sid == SERVICE_TYPE.READ_DATA_BY_IDENTIFIER:
+      #   print('Car ECU - READ DATA')
+      #   assert len(resp) > 1
+      #   resp_data = struct.unpack('!H', resp[1:])[0]
+      #   print(resp_data)
+      #   if resp_data == DATA_IDENTIFIER_TYPE.VIN:
+      #     dat = bytes([resp_sid + 0x40]) + resp[1:] + DEFAULT_VIN.encode('utf-8')
+      #     print('Car ECU - sending back:', dat)
+      #     isotp_msg.send(dat)
+      #   else:
+      #     dat = bytes([0x7F, resp_sid, *resp[1:]])
+      #     print('Car ECU - bad data id, sending back:', dat)
+      #     isotp_msg.send(dat)
 
       else:
         dat = [0x7F]
@@ -81,13 +167,13 @@ class MockCanBuffer:
       return self.rx_msg
 
 
-@parameterized_class([
-  {"tx_addr": 0x750, "sub_addr": None},
-  {"tx_addr": 0x750, "sub_addr": 0xf},
-])
+# @parameterized_class([
+#   {"tx_addr": 0x750, "sub_addr": None},
+#   {"tx_addr": 0x750, "sub_addr": 0xf},
+# ])
 class TestUds(unittest.TestCase):
-  tx_addr: int
-  sub_addr: int
+  tx_addr: int = 0x750
+  sub_addr: int = None
 
   def setUp(self):
     self.rx_addr = get_rx_addr_for_tx_addr(self.tx_addr)
@@ -98,21 +184,38 @@ class TestUds(unittest.TestCase):
   def tearDown(self):
     self.uds_server.stop()
 
-  def test_tester_present(self):
-    """
-    Tests IsoTpMessage and CanClient both sending and responding to a
-    tester present request with and without sub-addresses
-    """
+  # def test_tester_present_v2(self):
+  #   ret = self.uds_server._uds_request(SERVICE_TYPE.TESTER_PRESENT, 0)
+  #   print('ret', ret)
 
-    self.uds_server.tester_present()
+  # def test_tester_present(self):
+  #   """
+  #   Tests IsoTpMessage and CanClient both sending and responding to a
+  #   tester present request with and without sub-addresses
+  #   """
+  #
+  #   self.uds_server.tester_present()
+  #
+  # def test_unsupported_service_type(self):
+  #   with self.assertRaises(uds.NegativeResponseError):
+  #     self.uds_server._uds_request(100)
+  #
+  #   with self.assertRaises(uds.NegativeResponseError):
+  #     self.uds_server._uds_request(SERVICE_TYPE.ACCESS_TIMING_PARAMETER)
 
-  def test_vin_query(self):
+  def test_read_by_identifier(self):
     """
     Tests all four ISO-TP frame types in both directions (sending as openpilot and the car ECU)
     """
 
     response = self.uds_server.read_data_by_identifier(DATA_IDENTIFIER_TYPE.VIN)
-    self.assertEqual(response.decode('utf-8'), DEFAULT_VIN)
+    self.assertEqual(response, DEFAULT_VIN_B)
+
+    response = self.uds_server._uds_request(SERVICE_TYPE.READ_DATA_BY_IDENTIFIER, None, b'\xf1\x00')
+    self.assertEqual(response, b'\xf1\x00CV1 MFC  AT USA LHD 1.00 1.05 99210-CV000 211027')
+
+    with self.assertRaises(uds.NegativeResponseError):
+      self.uds_server._uds_request(SERVICE_TYPE.READ_DATA_BY_IDENTIFIER, None, b'\xf1\x01')
 
 
 if __name__ == '__main__':
