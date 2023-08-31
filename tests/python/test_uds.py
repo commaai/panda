b933e8f54091ab2cf7fc57aa3c83e817ba99836b
Shane Smiskol <shane@smiskol.com>
2023-08-31 00:59:44 -0700

    clean up mock can buffer

--- tests/python/test_uds.py
+++ tests/python/test_uds.py
@@ -135,43 +135,24 @@ class UdsServer(UdsClient):
 class MockCanBuffer:
   def __init__(self):
     self.lock = threading.Lock()
-    self.rx_msg = []
-    self.rx_msgs = []
     # two buffers prevents server from reading its own messages and vice versa
     self.rx_msgs_server = []  # TODO: name these better
     self.rx_msgs_client = []
 
   def can_send(self, addr, dat, bus, server=False, timeout=0):
-    print('adding to can_send')
     with self.lock:
-      if server:  # from server, for client
-        print('added to can_send server', (addr, 0, dat, bus))
-        self.rx_msgs_server.append([(addr, 0, dat, bus)])
-      else:  # vice versa
-        print('added to can_send client', (addr, 0, dat, bus))
-        self.rx_msgs_client.append([(addr, 0, dat, bus)])
-
-      # print('added to can_send', (addr, 0, dat, bus))
-      # self.rx_msg = [(addr, 0, dat, bus)]
-      # self.rx_msgs.append([(addr, 0, dat, bus)])
+      print(f'added to can_send {server=}', (addr, 0, dat, bus))
+      buf = self.rx_msgs_server if server else self.rx_msgs_client
+      buf.append([(addr, 0, dat, bus)])
 
   def can_recv(self, server=False):
-    print(self.rx_msgs_client, self.rx_msgs_server, server)
     with self.lock:
-      # return self.rx_msg
-      if not server:  # for client
-        if len(self.rx_msgs_server):
-          print('returned server', self.rx_msgs_server[0])
-          return self.rx_msgs_server.pop(0)
-        else:
-          return []
+      buf = self.rx_msgs_client if server else self.rx_msgs_server
+      if len(buf):
+        print(f'returned {server=}', buf[0])
+        return buf.pop(0)
       else:
-        print('must be server')
-        if len(self.rx_msgs_client):
-          print('returned server', self.rx_msgs_client[0])
-          return self.rx_msgs_client.pop(0)
-        else:
-          return []
+        return []
 
 
 # @parameterized_class([
@@ -187,7 +168,6 @@ class TestUds(unittest.TestCase):
       None: {  # no subfunction
         b'\xF1\x00': b'\xF1\x00CV1 MFC  AT USA LHD 1.00 1.05 99210-CV000 211027',
         b'\xF1\x90': b'\xF1\x901H3110W0RLD5',
-        b'\xF1\x02': b'1234567hjifd AS Y&*(Y&F YS&Yf sukdh fkJS YHFush jksd f89123456',
       }
     }
   }
@@ -198,9 +178,6 @@ class TestUds(unittest.TestCase):
     self.uds_server = UdsServer(can_buf, get_rx_addr_for_tx_addr(self.tx_addr), self.tx_addr, sub_addr=self.sub_addr)
     self.uds_server.set_services(STANDARD_UDS_SERVER_SERVICES | self.TEST_UDS_SERVER_SERVICES)
     self.uds_server.start()
-
-    # isotp_msg_client = IsoTpMessage(can_client, timeout=0.1, debug=self.debug, max_len=max_len, single_frame_mode=self.single_frame_mode)
-    # isotp_msg_server = IsoTpMessage(can_client, timeout=0.1, debug=self.debug, max_len=max_len, single_frame_mode=self.single_frame_mode)
 
   def tearDown(self):
     self.uds_server.stop()
@@ -277,22 +254,18 @@ class TestUds(unittest.TestCase):
     Tests all four ISO-TP frame types in both directions (sending as openpilot and the car ECU)
     """
 
-    response = self.uds_server._uds_request(SERVICE_TYPE.READ_DATA_BY_IDENTIFIER, data=b'\xf1\x02')
-    print('response', response)
-    self.assertEqual(response, b'1234567hjifd AS Y&*(Y&F YS&Yf sukdh fkJS YHFush jksd f89123456')
+    response = self.uds_server.read_data_by_identifier(DATA_IDENTIFIER_TYPE.VIN)
+    self.assertEqual(response, b'1H3110W0RLD5')
 
-    # response = self.uds_server.read_data_by_identifier(DATA_IDENTIFIER_TYPE.VIN)
-    # self.assertEqual(response, b'1H3110W0RLD5')
+    # test non-standard id
+    response = self.uds_server._uds_request(SERVICE_TYPE.READ_DATA_BY_IDENTIFIER, data=b'\xf1\x00')
+    self.assertEqual(response, b'\xf1\x00CV1 MFC  AT USA LHD 1.00 1.05 99210-CV000 211027')
 
-    # # test non-standard id
-    # response = self.uds_server._uds_request(SERVICE_TYPE.READ_DATA_BY_IDENTIFIER, data=b'\xf1\x00')
-    # self.assertEqual(response, b'\xf1\x00CV1 MFC  AT USA LHD 1.00 1.05 99210-CV000 211027')
-    #
-    # with self.assertRaises(uds.NegativeResponseError):
-    #   self.uds_server._uds_request(SERVICE_TYPE.READ_DATA_BY_IDENTIFIER, 0, data=b'\xf1\x00')
-    #
-    # with self.assertRaises(uds.NegativeResponseError):
-    #   self.uds_server._uds_request(SERVICE_TYPE.READ_DATA_BY_IDENTIFIER, data=b'\xf1\x01')
+    with self.assertRaises(uds.NegativeResponseError):
+      self.uds_server._uds_request(SERVICE_TYPE.READ_DATA_BY_IDENTIFIER, 0, data=b'\xf1\x00')
+
+    with self.assertRaises(uds.NegativeResponseError):
+      self.uds_server._uds_request(SERVICE_TYPE.READ_DATA_BY_IDENTIFIER, data=b'\xf1\x01')
 
 
 if __name__ == '__main__':
