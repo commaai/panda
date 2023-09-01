656bc28b615956df752b33ed4d95989d8d2a3e0f
Jason Wen <haibin.wen3@gmail.com>
2023-08-31 23:31:33 -0400

    HKG CAN-FD: Move CRC lookup function to common Hyundai safety

--- board/safety/safety_hyundai_canfd.h
+++ board/safety/safety_hyundai_canfd.h
@@ -117,9 +117,6 @@ AddrCheckStruct hyundai_canfd_ice_addr_checks[] = {
 addr_checks hyundai_canfd_rx_checks = {hyundai_canfd_addr_checks, HYUNDAI_CANFD_ADDR_CHECK_LEN};
 
 
-uint16_t hyundai_canfd_crc_lut[256];
-
-
 const int HYUNDAI_PARAM_CANFD_HDA2 = 16;
 const int HYUNDAI_PARAM_CANFD_ALT_BUTTONS = 32;
 const int HYUNDAI_PARAM_CANFD_HDA2_ALT_STEERING = 128;
@@ -151,27 +148,7 @@ static uint32_t hyundai_canfd_compute_checksum(CANPacket_t *to_push) {
   int len = GET_LEN(to_push);
   uint32_t address = GET_ADDR(to_push);
 
-  uint16_t crc = 0;
-
-  for (int i = 2; i < len; i++) {
-    crc = (crc << 8U) ^ hyundai_canfd_crc_lut[(crc >> 8U) ^ GET_BYTE(to_push, i)];
-  }
-
-  // Add address to crc
-  crc = (crc << 8U) ^ hyundai_canfd_crc_lut[(crc >> 8U) ^ ((address >> 0U) & 0xFFU)];
-  crc = (crc << 8U) ^ hyundai_canfd_crc_lut[(crc >> 8U) ^ ((address >> 8U) & 0xFFU)];
-
-  if (len == 8) {
-    crc ^= 0x5f29U;
-  } else if (len == 16) {
-    crc ^= 0x041dU;
-  } else if (len == 24) {
-    crc ^= 0x819dU;
-  } else if (len == 32) {
-    crc ^= 0x9f5bU;
-  } else {
-
-  }
+  uint16_t crc = hyundai_common_canfd_compute_checksum(to_push, len, address);
 
   return crc;
 }
