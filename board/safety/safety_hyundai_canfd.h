19ae6ddf20b667756543d6d367eaf56ff7f19f39
Shane Smiskol <shane@smiskol.com>
2023-09-07 01:38:08 -0700

    revert

--- board/safety/safety_hyundai_canfd.h
+++ board/safety/safety_hyundai_canfd.h
@@ -237,7 +237,7 @@ static int hyundai_canfd_rx_hook(CANPacket_t *to_push) {
 
   if (valid && (bus == scc_bus)) {
     // cruise state
-    if ((addr == 0x1a0)) {
+    if ((addr == 0x1a0) && !hyundai_longitudinal) {
       // 1=enabled, 2=driver override
       int cruise_status = ((GET_BYTE(to_push, 8) >> 4) & 0x7U);
       bool cruise_engaged = (cruise_status == 1) || (cruise_status == 2);
