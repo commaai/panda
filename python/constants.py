bdecf826050d06eda40dde88064070ebdaee3a1e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-09-14 12:58:56 -0700

    F2: fix flash sectors (#1663)

--- python/constants.py
+++ python/constants.py
@@ -10,9 +10,9 @@ USBPACKET_MAX_SIZE = 0x40
 class McuConfig(NamedTuple):
   mcu: str
   mcu_idcode: int
+  sector_sizes: List[int]
   uid_address: int
   block_size: int
-  sector_sizes: List[int]
   serial_number_address: int
   app_address: int
   app_fn: str
@@ -26,23 +26,22 @@ class McuConfig(NamedTuple):
 Fx = (
   0x1FFF7A10,
   0x800,
-  [0x4000 for _ in range(4)] + [0x10000] + [0x20000 for _ in range(11)],
   0x1FFF79C0,
   0x8004000,
   "panda.bin.signed",
   0x8000000,
   "bootstub.panda.bin",
 )
-F2Config = McuConfig("STM32F2", 0x411, *Fx)
-F4Config = McuConfig("STM32F4", 0x463, *Fx)
+F2Config = McuConfig("STM32F2", 0x411, [0x4000 for _ in range(4)] + [0x10000] + [0x20000 for _ in range(7)], *Fx)
+F4Config = McuConfig("STM32F4", 0x463, [0x4000 for _ in range(4)] + [0x10000] + [0x20000 for _ in range(11)], *Fx)
 
 H7Config = McuConfig(
   "STM32H7",
   0x483,
+  [0x20000 for _ in range(7)],
   0x1FF1E800,
   0x400,
   # there is an 8th sector, but we use that for the provisioning chunk, so don't program over that!
-  [0x20000 for _ in range(7)],
   0x080FFFC0,
   0x8020000,
   "panda_h7.bin.signed",
