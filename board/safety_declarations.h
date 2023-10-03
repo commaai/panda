482e42a3b500d571b521238b58361cf3f177bead
Shane Smiskol <shane@smiskol.com>
2023-10-02 19:00:13 -0700

    here too

--- board/safety_declarations.h
+++ board/safety_declarations.h
@@ -196,7 +196,6 @@ bool controls_allowed = false;
 bool relay_malfunction = false;
 bool gas_interceptor_detected = false;
 int gas_interceptor_prev = 0;
-bool gas_pressed = false;
 bool gas_pressed_prev = false;
 bool brake_pressed = false;
 bool brake_pressed_prev = false;
