42c0c27799f2fbb3b4e9d0bd028345c2fb270d6f
Shane Smiskol <shane@smiskol.com>
2023-10-03 20:55:59 -0700

    don't change lat

--- board/safety.h
+++ board/safety.h
@@ -86,11 +86,6 @@ bool get_longitudinal_allowed(void) {
   return controls_allowed && !gas_pressed_prev;
 }
 
-bool get_lateral_allowed(void) {
-  // No steering allowed while pre-enabling at a standstill with brake
-  return controls_allowed && !brake_pressed_prev;
-}
-
 bool get_gas_allowed(void) {
   // No +acceleration/gas command while pre-enabled at a stop with brake
   return get_longitudinal_allowed() && !brake_pressed_prev;
@@ -552,7 +547,7 @@ bool steer_torque_cmd_checks(int desired_torque, int steer_req, const SteeringLi
   bool violation = false;
   uint32_t ts = microsecond_timer_get();
 
-  if (get_lateral_allowed()) {
+  if (controls_allowed) {
     // *** global torque limit check ***
     violation |= max_limit_check(desired_torque, limits.max_steer, -limits.max_steer);
 
@@ -579,7 +574,7 @@ bool steer_torque_cmd_checks(int desired_torque, int steer_req, const SteeringLi
   }
 
   // no torque if controls is not allowed
-  if (!get_lateral_allowed() && (desired_torque != 0)) {
+  if (!controls_allowed && (desired_torque != 0)) {
     violation = true;
   }
 
@@ -621,7 +616,7 @@ bool steer_torque_cmd_checks(int desired_torque, int steer_req, const SteeringLi
   }
 
   // reset to 0 if either controls is not allowed or there's a violation
-  if (violation || !get_lateral_allowed()) {
+  if (violation || !controls_allowed) {
     valid_steer_req_count = 0;
     invalid_steer_req_count = 0;
     desired_torque_last = 0;
@@ -637,7 +632,7 @@ bool steer_torque_cmd_checks(int desired_torque, int steer_req, const SteeringLi
 bool steer_angle_cmd_checks(int desired_angle, bool steer_control_enabled, const SteeringLimits limits) {
   bool violation = false;
 
-  if (get_lateral_allowed() && steer_control_enabled) {
+  if (controls_allowed && steer_control_enabled) {
     // convert floating point angle rate limits to integers in the scale of the desired angle on CAN,
     // add 1 to not false trigger the violation. also fudge the speed by 1 m/s so rate limits are
     // always slightly above openpilot's in case we read an updated speed in between angle commands
@@ -680,7 +675,7 @@ bool steer_angle_cmd_checks(int desired_angle, bool steer_control_enabled, const
   }
 
   // No angle control allowed when controls are not allowed
-  violation |= !get_lateral_allowed() && steer_control_enabled;
+  violation |= !controls_allowed && steer_control_enabled;
 
   return violation;
 }
--- board/safety_declarations.h
+++ board/safety_declarations.h
@@ -148,7 +148,6 @@ bool driver_limit_check(int val, int val_last, struct sample_t *val_driver,
   const int MAX, const int MAX_RATE_UP, const int MAX_RATE_DOWN,
   const int MAX_ALLOWANCE, const int DRIVER_FACTOR);
 bool get_longitudinal_allowed(void);
-bool get_lateral_allowed(void);
 bool get_gas_allowed(void);
 bool rt_rate_limit_check(int val, int val_last, const int MAX_RT_DELTA);
 float interpolate(struct lookup_t xy, float x);
