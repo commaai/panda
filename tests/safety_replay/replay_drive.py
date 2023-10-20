229107f120fb56612419e4194da5bc070228da07
Shane Smiskol <shane@smiskol.com>
2023-10-19 19:57:10 -0700

    replay drive: fix space formatting

--- tests/safety_replay/replay_drive.py
+++ tests/safety_replay/replay_drive.py
@@ -35,7 +35,7 @@ def replay_drive(lr, safety_mode, param, alternative_experience, segment=False):
       safety_tick_rx_invalid |= not safety.addr_checks_valid() or safety_tick_rx_invalid
 
     if msg.which() == 'sendcan':
-     for canmsg in msg.sendcan:
+      for canmsg in msg.sendcan:
         to_send = package_can_msg(canmsg)
         sent = safety.safety_tx_hook(to_send)
         if not sent:
