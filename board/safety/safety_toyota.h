06f61a9713a8a3457d9c01389aa8c10ed98f030e
Shane Smiskol <shane@smiskol.com>
2023-10-19 23:29:55 -0700

    measure vehicle speed

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -150,10 +150,19 @@ static int toyota_rx_hook(CANPacket_t *to_push) {
       }
     }
 
+    // sample speed
     if (addr == 0xaa) {
-      // check that all wheel speeds are at zero value with offset
-      bool standstill = (GET_BYTES(to_push, 0, 4) == 0x6F1A6F1AU) && (GET_BYTES(to_push, 4, 4) == 0x6F1A6F1AU);
-      vehicle_moving = !standstill;
+      int speed = 0;
+      // sum 4 wheel speeds. conversion: raw * 0.01 - 67.67
+      for (uint8_t i = 0U; i < 8U; i += 2U) {
+        int wheel_speed = (GET_BYTE(to_push, i) << 8U) + GET_BYTE(to_push, (i + 1U));
+        speed += wheel_speed - 0x1a6f;
+      }
+      // check that all wheel speeds are at zero value
+      vehicle_moving = speed != 0;
+
+      speed = ROUND(speed / 4.0 * 0.01 / 3.6 * VEHICLE_SPEED_FACTOR);
+      update_sample(&vehicle_speed, speed);
     }
 
     // most cars have brake_pressed on 0x226, corolla and rav4 on 0x224
