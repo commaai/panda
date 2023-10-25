40485608af4d80c400a43d370d75a5875cbe0ce5
Shane Smiskol <shane@smiskol.com>
2023-10-24 17:50:55 -0700

    both do same thing

--- tests/test_coverage.sh
+++ tests/test_coverage.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 set -e
 
-rm -f libpanda/*.gcda
-rm -f libpanda/*.gcov
-rm -f *.gcov
+#rm -f libpanda/*.gcda
+#rm -f libpanda/*.gcov
+#rm -f *.gcov
 rm -rf libpanda/coverage-out
-scons -j8 -D --safety-coverage
+rm -f libpanda/coverage.info
+#scons -j8 -D --safety-coverage
+#
+## run safety tests to generate coverage data
+#./safety/test.sh
+#
+#GCOV_OUTPUT=$(gcov -n libpanda/panda.c)
+#INCOMPLETE_COVERAGE=$(echo "$GCOV_OUTPUT" | paste -s -d' \n' | grep "File.*safety/safety_.*.h" | grep -v "100.00%")
+#if [ ! -z "$INCOMPLETE_COVERAGE" ]; then
+#  echo "Some files have less than 100% coverage:"
+#  echo "$INCOMPLETE_COVERAGE"
+#  exit 1
+#else
+#  echo "All checked files have 100% coverage!"
+#fi
+#exit
 
-# run safety tests to generate coverage data
-./safety/test.sh
+#geninfo libpanda/ -o libpanda/coverage.info
+lcov -c -d libpanda/ -o libpanda/coverage.info
 
-GCOV_OUTPUT=$(gcov -n libpanda/panda.c)
-INCOMPLETE_COVERAGE=$(echo "$GCOV_OUTPUT" | paste -s -d' \n' | grep "File.*safety/safety_.*.h" | grep -v "100.00%")
-if [ ! -z "$INCOMPLETE_COVERAGE" ]; then
-  echo "Some files have less than 100% coverage:"
-  echo "$INCOMPLETE_COVERAGE"
-  exit 1
-else
-  echo "All checked files have 100% coverage!"
-fi
-exit
-
-lcov --capture --directory libpanda/ --output-file libpanda/coverage.info
-genhtml libpanda/coverage.info --output-directory libpanda/coverage-out
+genhtml libpanda/coverage.info -o libpanda/coverage-out
