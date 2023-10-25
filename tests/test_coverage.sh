ce66f544b55aa4cda5822918ce01978c5e42d6d3
Shane Smiskol <shane@smiskol.com>
2023-10-24 17:36:53 -0700

    check coverage

--- tests/test_coverage.sh
+++ tests/test_coverage.sh
@@ -7,9 +7,19 @@ rm -f *.gcov
 rm -rf libpanda/coverage-out
 scons -j8 -D --safety-coverage
 
-
+# run safety tests to generate coverage data
 ./safety/test.sh
 
-gcov libpanda/panda.c
+GCOV_OUTPUT=$(gcov -n libpanda/panda.c)
+INCOMPLETE_COVERAGE=$(echo "$GCOV_OUTPUT" | paste -s -d' \n' | grep "File.*safety/safety_.*.h" | grep -v "100.00%")
+if [ ! -z "$INCOMPLETE_COVERAGE" ]; then
+  echo "Some files have less than 100% coverage:"
+  echo "$INCOMPLETE_COVERAGE"
+  exit 1
+else
+  echo "All checked files have 100% coverage!"
+fi
+exit
+
 lcov --capture --directory libpanda/ --output-file libpanda/coverage.info
 genhtml libpanda/coverage.info --output-directory libpanda/coverage-out
