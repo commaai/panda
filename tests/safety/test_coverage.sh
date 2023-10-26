891b28fe725d87f147dc70f7ae2cfc4c3c3181c1
Shane Smiskol <shane@smiskol.com>
2023-10-25 21:39:19 -0700

    move

--- tests/safety/test_coverage.sh
+++ tests/safety/test_coverage.sh
@@ -2,21 +2,21 @@
 set -e
 
 # reset coverage data and generate gcc note file
-rm -f libpanda/*.gcda
-rm -rf libpanda/coverage-out
-rm -f libpanda/coverage.info
+rm -f ../libpanda/*.gcda
+rm -rf coverage-out
+rm -f coverage.info
 scons -j$(nproc) -D --safety-coverage
 
 # run safety tests to generate coverage data
-./safety/test.sh
+./test.sh
 
 # generate and open report
-#geninfo libpanda/ -o libpanda/coverage.info
-#genhtml libpanda/coverage.info -o libpanda/coverage-out
-#browse libpanda/coverage-out/index.html
+geninfo ../libpanda/ -o coverage.info
+genhtml coverage.info -o coverage-out
+browse coverage-out/index.html
 
 # test coverage
-GCOV_OUTPUT=$(gcov -n libpanda/panda.c)
+GCOV_OUTPUT=$(gcov -n ../libpanda/panda.c)
 INCOMPLETE_COVERAGE=$(echo "$GCOV_OUTPUT" | paste -s -d' \n' | grep "File.*safety/safety_.*.h" | grep -v "100.00%")
 if [ ! -z "$INCOMPLETE_COVERAGE" ]; then
   echo "Some files have less than 100% coverage:"
