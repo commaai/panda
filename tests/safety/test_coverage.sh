ddb1666ab3d3f607dabc4e82f3f6f8b3eb070997
Shane Smiskol <shane@smiskol.com>
2023-10-30 13:53:40 -0700

    make report optional

--- tests/safety/test_coverage.sh
+++ tests/safety/test_coverage.sh
@@ -2,15 +2,17 @@
 set -e
 
 # reset coverage data and generate gcc note file
-scons -j$(nproc) -D --safety-coverage
+scons -j$(nproc) -D --coverage
 
 # run safety tests to generate coverage data
 ./test.sh
 
 # generate and open report
-geninfo ../libpanda/ -o coverage.info
-genhtml coverage.info -o coverage-out
-browse coverage-out/index.html
+if [ "$1" == "--report" ]; then
+  geninfo ../libpanda/ -o coverage.info
+  genhtml coverage.info -o coverage-out
+  browse coverage-out/index.html
+fi
 
 # test coverage
 GCOV_OUTPUT=$(gcov -n ../libpanda/panda.c)
