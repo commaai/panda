28d6482aad60d2c70941dac6736609a8c9a01d5b
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2023-11-02 16:36:25 +0100

    Update safety.h add new alternative exp to generic rx check

--- board/safety.h
+++ board/safety.h
@@ -259,7 +259,7 @@ void generic_rx_checks(bool stock_ecu_detected) {
   gas_pressed_prev = gas_pressed;
 
   // exit controls on rising edge of brake press
-  if (brake_pressed && (!brake_pressed_prev || vehicle_moving)) {
+  if (brake_pressed && (!brake_pressed_prev || vehicle_moving) && (alternative_experience & ALT_EXP_ALLOW_LATERAL_ON_BRAKE)) {
     controls_allowed = false;
   }
   brake_pressed_prev = brake_pressed;
