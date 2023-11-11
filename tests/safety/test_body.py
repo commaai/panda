333d92e66c5cfe112627761bd723bdc68cac9031
Shane Smiskol <shane@smiskol.com>
2023-11-10 18:06:12 -0800

    clean up

--- tests/safety/test_body.py
+++ tests/safety/test_body.py
@@ -26,6 +26,10 @@ class TestBody(common.PandaSafetyTest):
     values = {"TORQUE_L": torque_l, "TORQUE_R": torque_r}
     return self.packer.make_can_msg_panda("TORQUE_CMD", 0, values)
 
+  def _knee_torque_cmd_msg(self, torque_l, torque_r):
+    values = {"TORQUE_L": torque_l, "TORQUE_R": torque_r}
+    return self.packer.make_can_msg_panda("KNEE_TORQUE_CMD", 0, values)
+
   def _max_motor_rpm_cmd_msg(self, max_rpm_l, max_rpm_r):
     values = {"MAX_RPM_L": max_rpm_l, "MAX_RPM_R": max_rpm_r}
     return self.packer.make_can_msg_panda("MAX_MOTOR_RPM_CMD", 0, values)
@@ -43,25 +47,23 @@ class TestBody(common.PandaSafetyTest):
     self.assertTrue(self.safety.get_vehicle_moving())  # always moving
     self.assertTrue(self.safety.get_controls_allowed())
 
-  # def test_tx_hook(self):
-  #   self.assertFalse(self._tx(self._torque_cmd_msg(0, 0)))
-  #   self.safety.set_controls_allowed(True)
-  #   self.assertTrue(self._tx(self._torque_cmd_msg(0, 0)))
-  #
-  #
-  # def test_can_flasher(self):
-  #   # CAN flasher always allowed
-  #   self.safety.set_controls_allowed(False)
-  #   self.assertTrue(self._tx(common.make_msg(0, 0x1, 8)))
-  #
-  #   # 0xdeadfaceU enters CAN flashing mode for base & knee
-  #   for addr in (0x250, 0x350):
-  #     self.assertTrue(self._tx(common.make_msg(0, addr, dat=b'\xce\xfa\xad\xde\x1e\x0b\xb0\x0a')))
-  #     self.assertFalse(self._tx(common.make_msg(0, addr, dat=b'\xce\xfa\xad\xde\x1e\x0b\xb0')))  # not correct data/len
-  #     self.assertFalse(self._tx(common.make_msg(0, addr + 1, dat=b'\xce\xfa\xad\xde\x1e\x0b\xb0\x0a')))  # wrong address
+  def test_tx_hook(self):
+    self.assertFalse(self._tx(self._torque_cmd_msg(0, 0)))
+    self.assertFalse(self._tx(self._knee_torque_cmd_msg(0, 0)))
+    self.safety.set_controls_allowed(True)
+    self.assertTrue(self._tx(self._torque_cmd_msg(0, 0)))
+    self.assertTrue(self._tx(self._knee_torque_cmd_msg(0, 0)))
 
+  def test_can_flasher(self):
+    # CAN flasher always allowed
+    self.safety.set_controls_allowed(False)
+    self.assertTrue(self._tx(common.make_msg(0, 0x1, 8)))
 
-# TODO: add knee tests
+    # 0xdeadfaceU enters CAN flashing mode for base & knee
+    for addr in (0x250, 0x350):
+      self.assertTrue(self._tx(common.make_msg(0, addr, dat=b'\xce\xfa\xad\xde\x1e\x0b\xb0\x0a')))
+      self.assertFalse(self._tx(common.make_msg(0, addr, dat=b'\xce\xfa\xad\xde\x1e\x0b\xb0')))  # not correct data/len
+      self.assertFalse(self._tx(common.make_msg(0, addr + 1, dat=b'\xce\xfa\xad\xde\x1e\x0b\xb0\x0a')))  # wrong address
 
 
 if __name__ == "__main__":
