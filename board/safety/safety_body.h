642c367220e7125660dd68f9ff21faff5697d02f
Shane Smiskol <shane@smiskol.com>
2023-11-10 18:10:54 -0800

    Update board/safety/safety_body.h

--- board/safety/safety_body.h
+++ board/safety/safety_body.h
@@ -38,8 +38,7 @@ static int body_tx_hook(CANPacket_t *to_send) {
 
   // Allow going into CAN flashing mode for base & knee even if controls are not allowed
   bool flash_msg = ((addr == 0x250) || (addr == 0x350)) && (len == 8);
-  if (!controls_allowed && (GET_BYTES(to_send, 0, 4) == 0xdeadfaceU) && (GET_BYTES(to_send, 4, 4) == 0x0ab00b1eU) &&
-      flash_msg) {
+  if (!controls_allowed && (GET_BYTES(to_send, 0, 4) == 0xdeadfaceU) && (GET_BYTES(to_send, 4, 4) == 0x0ab00b1eU) && flash_msg) {
     tx = 1;
   }
 
