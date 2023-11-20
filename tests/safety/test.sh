ff694fdf9a3c88d5e950d6b26a8bb570f0e595a7
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-11-20 00:12:20 -0800

    revert that

--- tests/safety/test.sh
+++ tests/safety/test.sh
@@ -4,5 +4,5 @@ set -e
 HW_TYPES=( 6 9 )
 for hw_type in "${HW_TYPES[@]}"; do
   echo "Testing HW_TYPE: $hw_type"
-  HW_TYPE=$hw_type python -m unittest discover . -f
+  HW_TYPE=$hw_type python -m unittest discover .
 done
