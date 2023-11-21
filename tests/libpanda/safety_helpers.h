e442fb911a7c3cc5ad5d06b80a63366ee30970cf
Shane Smiskol <shane@smiskol.com>
2023-11-21 15:33:54 -0800

    set_enable_gas_interceptor not needed

--- tests/libpanda/safety_helpers.h
+++ tests/libpanda/safety_helpers.h
@@ -35,10 +35,6 @@ void set_gas_interceptor_detected(bool c){
   gas_interceptor_detected = c;
 }
 
-void set_enable_gas_interceptor(bool c){
-  enable_gas_interceptor = c;
-}
-
 bool get_controls_allowed(void){
   return controls_allowed;
 }
