6e15c9d70045356e947fba72facba4495050e089
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-12-04 20:42:42 -0800

    fix path

--- tests/safety/test.sh
+++ tests/safety/test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-HW_TYPES=( 6 9 )
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+cd $DIR
 
+HW_TYPES=( 6 9 )
 for hw_type in "${HW_TYPES[@]}"; do
   echo "Testing HW_TYPE: $hw_type"
   HW_TYPE=$hw_type pytest -n auto --dist loadfile test_*.py -k 'not Base'
