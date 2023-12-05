87760be111fa68bedee8a6af4dfebafc2509e7f5
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-12-04 20:09:38 -0800

    run safety tests in parallel

--- tests/safety/test.sh
+++ tests/safety/test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-HW_TYPES=( 6 9 )
+HW_TYPES=( 6 )
+
 for hw_type in "${HW_TYPES[@]}"; do
   echo "Testing HW_TYPE: $hw_type"
-  HW_TYPE=$hw_type python -m unittest discover .
+  #HW_TYPE=$hw_type python -m unittest discover .
+  HW_TYPE=$hw_type pytest -n auto --dist loadfile .
 done
