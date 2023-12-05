485fbe9be73a91672de2ef6530353ba2522bed99
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-12-04 21:11:31 -0800

    try this

--- tests/safety/mutation.sh
+++ tests/safety/mutation.sh
@@ -2,5 +2,9 @@
 set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+cd $DIR
 
-mutmut run --paths-to-mutate $DIR/
+rm test_*.py
+git checkout test_nissan.py
+rm -f .mutmut-cache
+mutmut run --runner="./test_nissan.py" --rerun-all --disable-mutation-types=string --paths-to-mutate $DIR --tests-dir $DIR/
