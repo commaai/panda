0d7872470c2248df168198644769d2e7adf7e29b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-12-04 20:27:35 -0800

    fix

--- tests/safety/test.sh
+++ tests/safety/test.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-HW_TYPES=( 6 )
+HW_TYPES=( 6 9 )
 
 for hw_type in "${HW_TYPES[@]}"; do
   echo "Testing HW_TYPE: $hw_type"
-  #HW_TYPE=$hw_type python -m unittest discover .
-  HW_TYPE=$hw_type pytest -n auto --dist loadfile .
+  HW_TYPE=$hw_type pytest -n auto --dist loadfile test_*.py -k 'not Base'
 done
