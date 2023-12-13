5f127e8c73a839fc341d253c7ce3967ff93e3a83
Shane Smiskol <shane@smiskol.com>
2023-12-12 22:28:46 -0800

    type hint

--- tests/safety/common.py
+++ tests/safety/common.py
@@ -125,21 +125,23 @@ class GasInterceptorSafetyTest(PandaSafetyTestBase):
   cnt_gas_cmd = 0
   cnt_user_gas = 0
 
+  packer: CANPackerPanda
+
   @classmethod
   def setUpClass(cls):
     if cls.__name__ == "GasInterceptorSafetyTest":
       cls.safety = None
       raise unittest.SkipTest
 
-  def _interceptor_gas_cmd(self, gas):
-    values = {"COUNTER_PEDAL": self.__class__.cnt_gas_cmd & 0xF}
+  def _interceptor_gas_cmd(self, gas: int):
+    values: dict[str, float | int] = {"COUNTER_PEDAL": self.__class__.cnt_gas_cmd & 0xF}
     if gas > 0:
       values["GAS_COMMAND"] = gas * 255.
       values["GAS_COMMAND2"] = gas * 255.
     self.__class__.cnt_gas_cmd += 1
     return self.packer.make_can_msg_panda("GAS_COMMAND", 0, values)
 
-  def _interceptor_user_gas(self, gas):
+  def _interceptor_user_gas(self, gas: int):
     values = {"INTERCEPTOR_GAS": gas, "INTERCEPTOR_GAS2": gas,
               "COUNTER_PEDAL": self.__class__.cnt_user_gas}
     self.__class__.cnt_user_gas += 1
@@ -182,6 +184,7 @@ class GasInterceptorSafetyTest(PandaSafetyTestBase):
 
   def test_gas_interceptor_safety_check(self):
     for gas in np.arange(0, 4000, 100):
+      gas = int(gas)
       for controls_allowed in [True, False]:
         self.safety.set_controls_allowed(controls_allowed)
         if controls_allowed:
