097adc9905bac66e0394018196cfc7e82183ac88
Shane Smiskol <shane@smiskol.com>
2023-12-19 02:58:15 -0800

    now don't need this

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -345,7 +345,7 @@ static bool toyota_tx_hook(CANPacket_t *to_send) {
   if (addr == 0x750) {
     // this address is sub-addressed. only allow tester present to radar (0xF)
     bool invalid_uds_msg = (GET_BYTES(to_send, 0, 4) != 0x003E020FU) || (GET_BYTES(to_send, 4, 4) != 0x0U);
-    if (invalid_uds_msg || toyota_stock_longitudinal) {
+    if (invalid_uds_msg) {
       tx = 0;
     }
   }
