7810206c284d02b9adc0e0d7b33f06e764ac81ca
bongbui321 <hoang.bui.vh@gmail.com>
2024-01-14 00:11:53 -0500

    10 misra rules

--- tests/misra/test_mutation.py
+++ tests/misra/test_mutation.py
@@ -12,13 +12,32 @@ ROOT = os.path.join(HERE, "../../")
 # - at least one violation in each safety/safety*.h file
 # - come up with a pattern for each rule (cppcheck tests probably have good ones?)
 mutations = [
-  (None, None, False),
   # F4 only
   ("board/stm32fx/llbxcan.h", "s/1U/1/g", True),
   # H7 only
   ("board/stm32h7/llfdcan.h", "s/return ret;/if (true) { return ret; } else { return false; }/g", True),
   # general safety
   ("board/safety/safety_toyota.h", "s/is_lkas_msg =.*;/is_lkas_msg = addr == 1 || addr == 2;/g", True),
+  # misra-c2012-12.1
+  ("board/safety/safety_chrysler.h", "s/(chrysler_platform == CHRYSLER_PACIFICA)/chrysler_platform == CHRYSLER_PACIFICA/g", True),
+  # misra-c2012-13.3
+  ("board/safety/safety_defaults.h", "s/bus_fwd = 2;/int temp = 0;temp = bus_fwd++ + 2;bus_fwd = temp;/g", True),
+  # misra-c2012-13.4
+  ("board/safety/safety_defaults.h", "s/bus_fwd = 2;/int x; int y; bus_fwd = (x=2) && (y=2);/g", True),
+  # misra-c2012-13.5
+  ("board/safety/safety_defaults.h", "s/bus_fwd = 2;/int temp = 0; if (true && temp++) { bus_fwd = 2; }/g", True),
+  # misra-c2012-13.6
+  ("board/safety/safety_defaults.h", "s/bus_fwd = 2;/int temp = 0; if (sizeof(temp++)) { bus_fwd = 2; }/g", True),
+  # misra-c2012-14.1
+  ("board/safety/safety_elm327.h", "$ a for (float j = 0; j < (float)1; j++) {continue)",True),
+  # misra-c2012-14.4
+  ("board/safety/safety_elm327.h", "$ a int len = 10; if (len - 8) {;}", True),
+  # misra-c2012-16.4
+  ( "board/safety/safety_elm327.h", r"$ a void test(int temp) {switch (temp) { case 1: ; }}\n", True),
+  # misra-c2012-20.4
+  ( "board/safety/safety_elm327.h", r"$ a #define auto 1\n", True),
+  # misra-c2012-20.5
+  ( "board/safety/safety_elm327.h", r"$ a #define TEST 1\n#undef TEST\n", True),
 ]
 
 @pytest.mark.parametrize("fn, patch, should_fail", mutations)
@@ -38,4 +57,4 @@ def test_misra_mutation(fn, patch, should_fail):
     assert failed == should_fail
 
 if __name__ == "__main__":
-  pytest.main([__file__, "-n 4"])
+  pytest.main([__file__, "-n 8"])
