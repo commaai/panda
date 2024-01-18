4ee5d17390f9fdfd3cd9c817a14e000b26ca331e
bongbui321 <hoang.bui.vh@gmail.com>
2024-01-17 22:04:25 -0500

    test time

--- tests/misra/test_mutation.py
+++ tests/misra/test_mutation.py
@@ -13,14 +13,12 @@ ROOT = os.path.join(HERE, "../../")
 # - at least one violation in each safety/safety*.h file
 # - come up with a pattern for each rule (cppcheck tests probably have good ones?)
 mutations = [
-   # F4 only
+   # misra-c2012-10.4
   ("board/main.c", "1i int test(int tmp, float tmp2) { return tmp - tmp2; }", True),
-  # H7 only
-  ("board/main.c", "1i bool test(bool state){ if (state) { return true; } else {return false; } }", True),
-  # general safety
-  ("board/safety/safety_toyota.h", "s/is_lkas_msg =.*;/is_lkas_msg = addr == 1 || addr == 2;/g", True),
+  # misra-c2012-15.5
+  ("board/main.c", "$ a bool test(bool state){ if (state) { return true; } else { return false; } }", True),
   # misra-c2012-12.1
-  ("board/main.c", "1i bool test(int tmp) { return tmp == 8 ? true : false; }", True),
+  ("board/main.c", "1i int test(int tmp) { return tmp == 8 ? 1 : 2; }", True),
   # misra-c2012-13.3
   ("board/main.c", "1i void test(int tmp) { int tmp2 = tmp++ + 2; if (tmp2) {;}}", True),
   # misra-c2012-13.4
@@ -34,11 +32,13 @@ mutations = [
   # misra-c2012-14.4
   ("board/main.c", "1i void test(int len) { if (len - 8) {;} }", True),
   # misra-c2012-16.4
-  ( "board/main.c", r"1i void test(int temp) {switch (temp) { case 1: ; }}\n", True),
+  ("board/main.c", r"1i void test(int temp) {switch (temp) { case 1: ; }}\n", True),
+  # misra-c2012-17.8
+  ("board/main.c", "1i void test(int cnt) { for (cnt=0;;cnt++) {;} }", True),
   # misra-c2012-20.4
-  ( "board/main.c", r"1i #define auto 1\n", True),
+  ("board/main.c", r"1i #define auto 1\n", True),
   # misra-c2012-20.5
-  ( "board/main.c", r"1i #define TEST 1\n#undef TEST\n", True),
+  ("board/main.c", r"1i #define TEST 1\n#undef TEST\n", True),
   (None, None, False),
 ]
 
