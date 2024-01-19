d11b5e36f3ef3ad0b2861a6daf3215607e0d3fcd
bongbui321 <hoang.bui.vh@gmail.com>
2024-01-18 20:19:34 -0500

    cleanup

--- tests/misra/test_mutation.py
+++ tests/misra/test_mutation.py
@@ -5,7 +5,6 @@ import shutil
 import subprocess
 import tempfile
 import hashlib
-from typing import List
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 ROOT = os.path.join(HERE, "../../")
@@ -13,45 +12,43 @@ ROOT = os.path.join(HERE, "../../")
 # TODO: test more cases
 # - at least one violation in each safety/safety*.h file
 # - come up with a pattern for each rule (cppcheck tests probably have good ones?)
-mutations: List[List] = [
+mutations = [
   # default
-  [None, None, False, None],
+  (None, None, False),
   # misra-c2012-10.4
-  ["board/main.c", "1i int test(int tmp, float tmp2) { return tmp - tmp2; }", True],
+  ("board/main.c", "1i int test(int tmp, float tmp2) { return tmp - tmp2; }", True),
   # misra-c2012-15.5
-  ["board/main.c", "1i bool test(bool state){ if (state) { return true; } else { return false; } }", True],
+  ("board/main.c", "1i bool test(bool state){ if (state) { return true; } else { return false; } }", True),
   # misra-c2012-12.1
-  ["board/main.c", "1i int test(int tmp) { return tmp == 8 ? 1 : 2; }", True],
+  ("board/main.c", "1i int test(int tmp) { return tmp == 8 ? 1 : 2; }", True),
   # misra-c2012-13.3
-  ["board/main.c", "1i void test(int tmp) { int tmp2 = tmp++ + 2; if (tmp2) {;}}", True],
+  ("board/main.c", "1i void test(int tmp) { int tmp2 = tmp++ + 2; if (tmp2) {;}}", True),
   # misra-c2012-13.4
-  ["board/main.c", "1i int test(int x, int y) { return (x=2) && (y=2); }", True],
+  ("board/main.c", "1i int test(int x, int y) { return (x=2) && (y=2); }", True),
   # misra-c2012-13.5
-  ["board/main.c", "1i void test(int tmp) { if (true && tmp++) {;} }", True],
+  ("board/main.c", "1i void test(int tmp) { if (true && tmp++) {;} }", True),
   # misra-c2012-13.6
-  ["board/main.c", "1i void test(int tmp) { if (sizeof(tmp++)) {;} }", True],
+  ("board/main.c", "1i void test(int tmp) { if (sizeof(tmp++)) {;} }", True),
   # misra-c2012-14.1
-  ["board/main.c", "1i void test(float len) { for (float j = 0; j < len; j++) {;} }",True],
+  ("board/main.c", "1i void test(float len) { for (float j = 0; j < len; j++) {;} }",True),
   # misra-c2012-14.4
-  ["board/main.c", "1i void test(int len) { if (len - 8) {;} }", True],
+  ("board/main.c", "1i void test(int len) { if (len - 8) {;} }", True),
   # misra-c2012-16.4
-  ["board/main.c", r"1i void test(int temp) {switch (temp) { case 1: ; }}\n", True],
+  ("board/main.c", r"1i void test(int temp) {switch (temp) { case 1: ; }}\n", True),
   # misra-c2012-17.8
-  ["board/main.c", "1i void test(int cnt) { for (cnt=0;;cnt++) {;} }", True],
+  ("board/main.c", "1i void test(int cnt) { for (cnt=0;;cnt++) {;} }", True),
   # misra-c2012-20.4
-  ["board/main.c", r"1i #define auto 1\n", True],
+  ("board/main.c", r"1i #define auto 1\n", True),
   # misra-c2012-20.5
-  ["board/main.c", r"1i #define TEST 1\n#undef TEST\n", True],
+  ("board/main.c", r"1i #define TEST 1\n#undef TEST\n", True),
 ]
 
-del_header = "/[#include, #ifdef, #else]/d"
-for m in mutations[1:]:
-  m.append(del_header)
-
-@pytest.mark.parametrize("fn, patch, should_fail, del_header", mutations)
-def test_misra_mutation(fn, patch, should_fail, del_header):
+@pytest.mark.parametrize("fn, patch, should_fail", mutations)
+def test_misra_mutation(fn, patch, should_fail):
   key = hashlib.md5((str(fn) + str(patch)).encode()).hexdigest()
   tmp = os.path.join(tempfile.gettempdir(), key)
+
+  del_header = "/[#include, #ifdef, #else]/d" if fn is not None else None
 
   if os.path.exists(tmp):
     shutil.rmtree(tmp)
