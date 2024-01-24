d395034b518a776843763b40f84d3989927e863d
Comma Device <device@comma.ai>
2024-01-24 19:41:46 +0000

    CI: unbrick bootokick test panda from bad FW

--- tests/reflash_internal_panda.py
+++ tests/reflash_internal_panda.py
@@ -8,32 +8,25 @@ class GPIO:
   HUB_RST_N = 30
 
 
-def gpio_init(pin, output):
-  with open(f"/sys/class/gpio/gpio{pin}/direction", 'wb') as f:
-    f.write(b"out" if output else b"in")
-
 def gpio_set(pin, high):
+  with open(f"/sys/class/gpio/gpio{pin}/direction", 'wb') as f:
+    f.write(b"out")
   with open(f"/sys/class/gpio/gpio{pin}/value", 'wb') as f:
     f.write(b"1" if high else b"0")
 
 
-if __name__ == "__main__":
-  for pin in (GPIO.STM_RST_N, GPIO.STM_BOOT0, GPIO.HUB_RST_N):
-    gpio_init(pin, True)
-
-  # reset USB hub
-  gpio_set(GPIO.HUB_RST_N, 0)
-  time.sleep(0.5)
-  gpio_set(GPIO.HUB_RST_N, 1)
-
-  # flash bootstub
-  print("resetting into DFU")
-  gpio_set(GPIO.STM_RST_N, 1)
-  gpio_set(GPIO.STM_BOOT0, 1)
-  time.sleep(1)
-  gpio_set(GPIO.STM_RST_N, 0)
-  gpio_set(GPIO.STM_BOOT0, 0)
-  time.sleep(1)
+def reflash_bootstub():
+  # we have to try a few times since there's a race condition
+  # between SPI and USB in the ST bootloader
+  for tryy in range(5):
+    print("resetting into DFU")
+    gpio_set(GPIO.STM_RST_N, 1)
+    gpio_set(GPIO.STM_BOOT0, 1)
+    time.sleep(0.1)
+    gpio_set(GPIO.STM_RST_N, 0)
+    gpio_set(GPIO.STM_BOOT0, 0)
+    if Panda.wait_for_dfu(None, timeout=5):
+      break
 
   print("flashing bootstub")
   PandaDFU(None).recover()
@@ -41,9 +34,18 @@ if __name__ == "__main__":
   gpio_set(GPIO.STM_RST_N, 1)
   time.sleep(0.5)
   gpio_set(GPIO.STM_RST_N, 0)
-  time.sleep(1)
 
-  print("flashing app")
-  p = Panda()
-  assert p.bootstub
-  p.flash()
+
+if __name__ == "__main__":
+  # reset USB hub for dos
+  gpio_set(GPIO.HUB_RST_N, 0)
+  time.sleep(0.5)
+  gpio_set(GPIO.HUB_RST_N, 1)
+
+  reflash_bootstub()
+  #time.sleep(1)
+
+  #print("flashing app")
+  #p = Panda()
+  #assert p.bootstub
+  #p.flash()
--- tests/som/on-device.py
+++ tests/som/on-device.py
@@ -3,10 +3,11 @@ import os
 import time
 
 from panda import Panda
-
+from panda.tests.reflash_internal_panda import reflash_bootstub
 
 if __name__ == "__main__":
   flag_set = False
+  no_panda_count = 0
   while True:
     try:
       with Panda(disable_checks=False) as p:
@@ -19,6 +20,11 @@ if __name__ == "__main__":
         dt = p.get_datetime()
         if dt.year == 2040 and dt.month == 8:
           os.system("sudo poweroff")
+        no_panda_count = 0
     except Exception as e:
       print(str(e))
+      no_panda_count += 1
+      if no_panda_count >= 5:
+        reflash_bootstub()
+        no_panda_count = 0
     time.sleep(0.5)
