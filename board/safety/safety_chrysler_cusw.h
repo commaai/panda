b7d38d1b0083373f6ac141fe096c997a0006b05c
Jason Young <jyoung8607@gmail.com>
2024-02-18 12:33:28 -0500

    fix ACC_1 message size

--- board/safety/safety_chrysler_cusw.h
+++ board/safety/safety_chrysler_cusw.h
@@ -43,7 +43,7 @@ RxCheck chrysler_cusw_rx_checks[] = {
   {.msg = {{CHRYSLER_CUSW_ADDRS.BRAKE_2, 0, 8, .check_checksum = true, .max_counter = 15U, .frequency = 50U}, { 0 }, { 0 }}},
   {.msg = {{CHRYSLER_CUSW_ADDRS.EPS_STATUS, 0, 8, .check_checksum = true, .max_counter = 0U, .frequency = 100U}, { 0 }, { 0 }}},
   {.msg = {{CHRYSLER_CUSW_ADDRS.ACCEL_GAS, 0, 5, .check_checksum = true, .max_counter = 15U, .frequency = 50U}, { 0 }, { 0 }}},
-  {.msg = {{CHRYSLER_CUSW_ADDRS.ACC_1, 0, 8, .check_checksum = false, .max_counter = 0U, .frequency = 17U}, { 0 }, { 0 }}},
+  {.msg = {{CHRYSLER_CUSW_ADDRS.ACC_1, 0, 7, .check_checksum = false, .max_counter = 0U, .frequency = 16U}, { 0 }, { 0 }}},
 };
 
 const ChryslerCuswAddrs *chrysler_cusw_addrs = &CHRYSLER_CUSW_ADDRS;
