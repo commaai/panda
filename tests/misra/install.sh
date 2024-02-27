b51ee7421346bdca35d1415b081c366c54b84ec7
Aryan <aryangiroud@gmail.com>
2024-02-27 20:56:46 +0530

    update install.sh

--- tests/misra/install.sh
+++ tests/misra/install.sh
@@ -13,8 +13,7 @@ cd $CPPCHECK_DIR
 VERS="2.13.0"
 git fetch origin $VERS
 git checkout $VERS
-git cherry-pick -n f6b538e855f0bacea33c4074664628024ef39dc6
-git cherry-pick -n b11b42087ff29569bc3740f5aa07eb6616ea4f63
+git cherry-pick -n f6b538e855f0bacea33c4074664628024ef39dc6 b11b42087ff29569bc3740f5aa07eb6616ea4f63
 
 #make clean
 make MATCHCOMPILTER=yes CXXFLAGS="-O2" -j8
