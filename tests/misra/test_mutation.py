895a7001c9d21ac7c4ace65debe70dfaee017443
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-03-11 16:18:56 -0700

    Rename stm32fx/ -> stm32f4/ (#1898)
    * Rename stm32fx/ -> stm32f4/
    
    * jungle too

--- tests/misra/test_mutation.py
+++ tests/misra/test_mutation.py
@@ -14,21 +14,21 @@ IGNORED_PATHS = (
   'board/obj',
   'board/jungle',
   'board/stm32h7/inc',
-  'board/stm32fx/inc',
+  'board/stm32f4/inc',
   'board/fake_stm.h',
 
   # bootstub only files
   'board/flasher.h',
   'board/bootstub.c',
   'board/bootstub_declarations.h',
-  'board/stm32fx/llflash.h'
+  'board/stm32f4/llflash.h'
 )
 
 mutations = [
   # default
   (None, None, False),
   # F4 only
-  ("board/stm32fx/llbxcan.h", "s/1U/1/g", True),
+  ("board/stm32f4/llbxcan.h", "s/1U/1/g", True),
   # H7 only
   ("board/stm32h7/llfdcan.h", "s/return ret;/if (true) { return ret; } else { return false; }/g", True),
   # general safety
@@ -60,7 +60,7 @@ patterns = [
 
 all_files = glob.glob('board/**', root_dir=ROOT, recursive=True)
 files = [f for f in all_files if f.endswith(('.c', '.h')) and not f.startswith(IGNORED_PATHS)]
-assert len(files) > 70, all(d in files for d in ('board/main.c', 'board/stm32fx/llbxcan.h', 'board/stm32h7/llfdcan.h', 'board/safety/safety_toyota.h'))
+assert len(files) > 70, all(d in files for d in ('board/main.c', 'board/stm32f4/llbxcan.h', 'board/stm32h7/llfdcan.h', 'board/safety/safety_toyota.h'))
 
 for p in patterns:
   mutations.append((random.choice(files), p, True))
