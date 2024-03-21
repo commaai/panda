f656b9de28eef34021c1bfdce073f7e9d6490556
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-03-21 15:47:13 -0700

    usb serial

--- tests/hitl/8_misc.py
+++ tests/hitl/8_misc.py
@@ -1,5 +1,6 @@
 from panda import Panda
 
-def test_startup_time(p):
+def test_boot_time(p):
+  # boot time should be instant
   p.reset(reconnect=False)
-  assert Panda.wait_for_panda(p.serial, timeout=0.3)
+  assert Panda.wait_for_panda(p.get_usb_serial(), timeout=0.3)
