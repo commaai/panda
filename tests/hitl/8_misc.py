09a72059b829ae4646102c050043b8537823a6a6
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-03-21 16:14:58 -0700

    just test for now

--- tests/hitl/8_misc.py
+++ tests/hitl/8_misc.py
@@ -2,5 +2,7 @@ from panda import Panda
 
 def test_boot_time(p):
   # boot time should be instant
+  st = time.monotonic()
   p.reset(reconnect=False)
-  assert Panda.wait_for_panda(p.get_usb_serial(), timeout=0.3)
+  assert Panda.wait_for_panda(p.get_usb_serial(), timeout=3.0)
+  assert time.monotonic() - st < 3.0
