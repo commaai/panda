256463552e84b6808ff53382f586ce3f25d2029f
Shane Smiskol <shane@smiskol.com>
2024-06-03 23:10:43 -0700

    clear stuck CAN messages before switching multiplexing

--- board/main.c
+++ board/main.c
@@ -97,6 +97,8 @@ void set_safety_mode(uint16_t mode, uint16_t param) {
       heartbeat_counter = 0U;
       heartbeat_lost = false;
       if (current_board->has_obd) {
+        // Clear any pending messages in the can core (from sending while comma power is unplugged)
+        llcan_clear_send(CANIF_FROM_CAN_NUM(1));
         if (param == 0U) {
           current_board->set_can_mode(CAN_MODE_OBD_CAN2);
         } else {
