5bb460a02dffe7a95c7cee5444c0060693b96669
dzid26 <dzidmail@gmail.com>
2024-06-04 11:43:40 +0100

    Fix cppcheck checklist diff

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -79,7 +79,7 @@ printf "\n${GREEN}Success!${NC} took $SECONDS seconds\n"
 
 # ensure list of checkers is up to date
 cd $DIR
-if [ -z "$SKIP_TABLES_DIFF" ] && ! git diff --quiet coverage_table; then
+if [ -z "$SKIP_TABLES_DIFF" ] && ! git diff --quiet $CHECKLIST; then
   echo -e "\n${YELLOW}WARNING: Cppcheck checkers.txt report has changed. Review and commit...${NC}"
   exit 3
 fi
