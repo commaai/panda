caeb0d388e87373a01aa6b13654cab2ef47e7fc7
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-06-19 20:34:26 +0200

    Update can_common.h it works, try bit 17 for vw ignition

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -224,7 +224,7 @@ void ignition_can_hook(CANPacket_t *to_push) {
 
     // Volkswagen MEB exception
     if ((addr == 0x3C0) && (len == 4)) {
-      ignition_can = GET_BIT(to_push, 16) || GET_BIT(to_push, 17) || GET_BIT(to_push, 18);
+      ignition_can = GET_BIT(to_push, 17);
       ignition_can_cnt = 0U;
     }
   }
