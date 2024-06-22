f91e206307ac7efb9014675ca49eaebb450b1eee
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-06-22 21:01:20 +0200

    Update safety_volkswagen_meb.h correct tx msgs

--- board/safety/safety_volkswagen_meb.h
+++ board/safety/safety_volkswagen_meb.h
@@ -31,9 +31,9 @@ const LongitudinalLimits VOLKSWAGEN_MEB_LONG_LIMITS = {
 #define MSG_LDW_02         0x397   // TX by OP, Lane line recognition and text alerts
 
 // Transmit of GRA_ACC_01 is allowed on bus 0 and 2 to keep compatibility with gateway and camera integration
-const CanMsg VOLKSWAGEN_MEB_STOCK_TX_MSGS[] = {{MSG_HCA_01, 0, 8}, {MSG_SPEED_01, 0, 8}, {MSG_GRA_ACC_01, 0, 8},
+const CanMsg VOLKSWAGEN_MEB_STOCK_TX_MSGS[] = {{MSG_LANE_ASSIST_01 0, 8}, {MSG_GRA_ACC_01, 0, 8},
                                                {MSG_GRA_ACC_01, 2, 8}, {MSG_LDW_02, 0, 8}, {MSG_LH_EPS_03, 2, 8}};
-const CanMsg VOLKSWAGEN_MEB_LONG_TX_MSGS[] = {{MSG_HCA_01, 0, 8}, {MSG_SPEED_01, 0, 8}, {MSG_LDW_02, 0, 8}, {MSG_LH_EPS_03, 2, 8}};
+const CanMsg VOLKSWAGEN_MEB_LONG_TX_MSGS[] = {, {MSG_LANE_ASSIST_01, 0, 8}, {MSG_LDW_02, 0, 8}, {MSG_LH_EPS_03, 2, 8}};
 
 RxCheck volkswagen_meb_rx_checks[] = {
   {.msg = {{MSG_LH_EPS_03, 0, 8, .check_checksum = true, .max_counter = 15U, .frequency = 100U}, { 0 }, { 0 }}},
