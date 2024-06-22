6d265b9f7f56867e1a16473e04800c1f5c1ef27d
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-06-22 13:09:40 +0200

    Update safety_volkswagen_meb.h add forwarding logic back in

--- board/safety/safety_volkswagen_meb.h
+++ board/safety/safety_volkswagen_meb.h
@@ -184,9 +184,25 @@ static bool volkswagen_meb_tx_hook(const CANPacket_t *to_send) {
 
 static int volkswagen_meb_fwd_hook(int bus_num, int addr) {
   int bus_fwd = -1;
-  UNUSED(bus_num);
-  UNUSED(addr);
-  UNUSED(bus_fwd);
+
+  switch (bus_num) {
+    case 0:
+      bus_fwd = 2;
+      break;
+    case 2:
+      if ((addr == MSG_SPEED_01) || (addr == MSG_LDW_02)) {
+        // openpilot takes over LKAS steering control and related HUD messages from the camera
+        bus_fwd = -1;
+      } else {
+        // Forward all remaining traffic from Extended CAN devices to J533 gateway
+        bus_fwd = 0;
+      }
+      break;
+    default:
+      // No other buses should be in use; fallback to do-not-forward
+      bus_fwd = -1;
+      break;
+  }
 
   return bus_fwd;
 }
