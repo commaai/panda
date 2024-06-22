71836cfda04d48152374e91ecbaeb9513b4a4a31
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-06-22 17:09:40 +0200

    Update safety.h

--- board/safety.h
+++ board/safety.h
@@ -52,7 +52,7 @@
 #define SAFETY_FAW 26U
 #define SAFETY_BODY 27U
 #define SAFETY_HYUNDAI_CANFD 28U
-#define SAFETY_VOLKSWAGEN_MEB 29U
+#define SAFETY_VOLKSWAGEN_MEB 32U
 
 uint16_t current_safety_mode = SAFETY_SILENT;
 uint16_t current_safety_param = 0;
