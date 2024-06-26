c324365835980d2de7255744e4c7fdb66e3e0398
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-06-26 16:39:48 -0700

    fix

--- board/main.c
+++ board/main.c
@@ -69,7 +69,6 @@ void set_safety_mode(uint16_t mode, uint16_t param) {
     err = set_safety_hooks(mode_copy, 0U);
     // TERMINAL ERROR: we can't continue if SILENT safety mode isn't succesfully set
     assert(err == 0, "Error: Failed setting SILENT mode. Hanging\n");
-    }
   }
   safety_tx_blocked = 0;
   safety_rx_invalid = 0;
@@ -311,7 +310,7 @@ int main(void) {
   print("\n\n\n************************ MAIN START ************************\n");
 
   // check for non-supported board types
-  assert(hw_type == HW_TYPE_UNKNOWN, "Unsupported board type");
+  assert(hw_type != HW_TYPE_UNKNOWN, "Unsupported board type");
 
   print("Config:\n");
   print("  Board type: 0x"); puth(hw_type); print("\n");
