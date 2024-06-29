053716a7d760ab70cc18525491e4b4103e3c120c
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-06-29 19:05:33 +0200

    Update safety_volkswagen_meb.h fix lane assist byte to int operation

--- board/safety/safety_volkswagen_meb.h
+++ board/safety/safety_volkswagen_meb.h
@@ -202,7 +202,7 @@ static bool volkswagen_meb_tx_hook(const CANPacket_t *to_send) {
   // Signal: HCA_01.HCA_01_LM_Offset (absolute torque)
   // Signal: HCA_01.HCA_01_LM_OffSign (direction)
   if (addr == MSG_MEB_LANE_ASSIST_01) {
-    int desired_torque = GET_BYTE(to_send, 3) | ((GET_BYTE(to_send, 4) & 0x7U) << 8);
+    int desired_torque = GET_BYTE(to_send, 3) | ((GET_BYTE(to_send, 4) & 0x07U) << 8);
     bool sign = GET_BIT(to_send, 39U);
     if (sign) {
       desired_torque *= -1;
