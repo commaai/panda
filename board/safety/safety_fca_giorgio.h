4481fc1eea77981b9122822bf0e603491ab8a5b3
Jason Young <jyoung8607@gmail.com>
2024-07-02 22:16:39 -0400

    bump limits for testing

--- board/safety/safety_fca_giorgio.h
+++ board/safety/safety_fca_giorgio.h
@@ -1,7 +1,7 @@
 // lateral limits
 const SteeringLimits FCA_GIORGIO_STEERING_LIMITS = {
-  .max_steer = 200,
-  .max_rt_delta = 150,
+  .max_steer = 500,
+  .max_rt_delta = 500,
   .max_rt_interval = 250000,
   .max_rate_up = 4,
   .max_rate_down = 4,
