9de9a51f559fb7bd6a24b6bc1f22370724c21464
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-19 12:24:59 +0200

    Update safety_volkswagen_common.h

--- board/safety/safety_volkswagen_common.h
+++ board/safety/safety_volkswagen_common.h
@@ -7,5 +7,6 @@ bool volkswagen_longitudinal = false;
 bool volkswagen_set_button_prev = false;
 bool volkswagen_resume_button_prev = false;
 int volkswagen_change_torque_prev = 0;
+int volkswagen_steer_frame_cnt = 0;
 
 #endif
