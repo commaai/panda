57d34a26594bcd23ac1904161c13de6416932e4f
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-19 14:58:48 +0200

    Update safety_volkswagen_common.h

--- board/safety/safety_volkswagen_common.h
+++ board/safety/safety_volkswagen_common.h
@@ -7,6 +7,7 @@ bool volkswagen_longitudinal = false;
 bool volkswagen_set_button_prev = false;
 bool volkswagen_resume_button_prev = false;
 int volkswagen_change_torque_prev = 0;
-int volkswagen_steer_frame_cnt = 0;
+int volkswagen_steer_frame_cnt = 0; // allow lag for steering safety by counter
+int volkswagen_accel_prev = 0; // allow lag for accel safety by 1 panda frame
 
 #endif
