fb8e86346c5496e4228c8b8e309f75f57140bf42
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-19 12:19:13 +0200

    Update safety_volkswagen_common.h

--- board/safety/safety_volkswagen_common.h
+++ board/safety/safety_volkswagen_common.h
@@ -6,5 +6,6 @@ const uint16_t FLAG_VOLKSWAGEN_LONG_CONTROL = 1;
 bool volkswagen_longitudinal = false;
 bool volkswagen_set_button_prev = false;
 bool volkswagen_resume_button_prev = false;
+int volkswagen_change_torque_prev = 0;
 
 #endif
