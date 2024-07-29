7dacc9cfbf4a078b30a0c6d4a35ba1c280e33dfb
infiniteCable <75014343+infiniteCable@users.noreply.github.com>
2024-07-29 21:26:23 +0200

    Update safety_volkswagen_meb.h fix

--- board/safety/safety_volkswagen_meb.h
+++ board/safety/safety_volkswagen_meb.h
@@ -20,7 +20,7 @@
 // lateral limits for angle
 const SteeringLimits VOLKSWAGEN_MEB_STEERING_LIMITS = {
   // SG_ Steering_Angle : 24|15@1+ (0.0174,0) [0|360] "Unit_DegreOfArc" XXX
-  .max_steer = 2068966 // 360 deg
+  .max_steer = 2068966, // 360 deg
   .angle_deg_to_can = 5747, // 1 / 0.0174 deg to can
   .angle_rate_up_lookup = {
     {0., 5., 15.},
@@ -28,7 +28,7 @@ const SteeringLimits VOLKSWAGEN_MEB_STEERING_LIMITS = {
   },
   .angle_rate_down_lookup = {
     {0., 5., 15.},
-    {1000., 700., 80}.
+    {1000., 700., 80}
   },
   .inactive_angle_is_zero = true,
 };
