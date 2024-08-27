2b94e4fef710a22313b99d8c5b723d8db5e9c8bd
dzid26 <dzidmail@gmail.com>
2024-08-27 00:15:29 +0000

    Enable CAN3 printer (#1991)
    * panda has 3 can peripherals
    
    * enable printing CAN3

--- python/__init__.py
+++ python/__init__.py
@@ -22,7 +22,7 @@ __version__ = '0.0.10'
 CANPACKET_HEAD_SIZE = 0x6
 DLC_TO_LEN = [0, 1, 2, 3, 4, 5, 6, 7, 8, 12, 16, 20, 24, 32, 48, 64]
 LEN_TO_DLC = {length: dlc for (dlc, length) in enumerate(DLC_TO_LEN)}
-PANDA_BUS_CNT = 4
+PANDA_BUS_CNT = 3
 
 
 def calculate_checksum(data):
