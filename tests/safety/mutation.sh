d08a712b3880744cebe15bd6184f22072a931e28
Maxime Desroches <desroches.maxime@gmail.com>
2024-09-24 16:56:38 -0700

    work on push

--- tests/safety/mutation.sh
+++ tests/safety/mutation.sh
@@ -8,13 +8,15 @@ cd ../../
 scons --mutation -j$(nproc)
 cd $DIR
 
+GIT_REF="${GIT_REF:-origin/master}"
+
 SAFETY_MODELS=( body chrysler defaults gm honda hyundai_canfd hyundai mazda nissan volkswagen_pq )
 for safety_model in "${SAFETY_MODELS[@]}"; do
   echo "  "
   echo "  "
   echo -e "Testing mutation on : safety_$safety_model"
   echo -e "" > mull.yml
-  echo -e "gitDiffRef: origin/master\n" >> mull.yml
+  echo -e "gitDiffRef: $GIT_REF\n" >> mull.yml
   echo -e "gitProjectRoot: ../../" >> mull.yml
 
   #PYTHONPATH=/home/batman/:/home/batman/panda/opendbc/:$PYTHONPATH mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./test_$safety_model.py || true
