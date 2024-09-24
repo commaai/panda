5d3d7dae7c4f0a429f99a6d5bce27270a1ccd2e7
Maxime Desroches <desroches.maxime@gmail.com>
2024-09-24 16:02:19 -0700

    get diff

--- tests/safety/mutation.sh
+++ tests/safety/mutation.sh
@@ -8,7 +8,7 @@ cd ../../
 scons --mutation -j$(nproc)
 cd $DIR
 
-SAFETY_MODELS=( body chrysler defaults gm honda hyundai_canfd hyundai mazda nissan )
+SAFETY_MODELS=( body chrysler defaults gm honda hyundai_canfd hyundai mazda nissan volkswagen_pq )
 #SAFETY_MODELS=( volkswagen_pq )
 for safety_model in "${SAFETY_MODELS[@]}"; do
   echo "  "
@@ -19,6 +19,6 @@ for safety_model in "${SAFETY_MODELS[@]}"; do
   echo -e "gitDiffRef: origin/master\n" >> mull.yml
   echo -e "gitProjectRoot: ../../" >> mull.yml
 
-  #PYTHONPATH=/home/batman/:/home/batman/panda/opendbc/:$PYTHONPATH mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./test_mazda.py
+  #PYTHONPATH=/home/batman/:/home/batman/panda/opendbc/:$PYTHONPATH mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./test_$safety_model.py || true
   mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./test_$safety_model.py || true
 done
