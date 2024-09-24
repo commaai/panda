d148da507275c1ff74f51d92ac54bedc762ba0bf
Maxime Desroches <desroches.maxime@gmail.com>
2024-09-24 15:58:48 -0700

    origin

--- tests/safety/mutation.sh
+++ tests/safety/mutation.sh
@@ -11,10 +11,12 @@ cd $DIR
 SAFETY_MODELS=( body chrysler defaults gm honda hyundai_canfd hyundai mazda nissan )
 #SAFETY_MODELS=( volkswagen_pq )
 for safety_model in "${SAFETY_MODELS[@]}"; do
-  echo -e "\n\nTesting mutation on : safety_$safety_model"
+  echo "  "
+  echo "  "
+  echo -e "Testing mutation on : safety_$safety_model"
   echo -e "" > mull.yml
   #echo -e "includePaths:\n - \".*safety_$safety_model.h\"" > mull.yml
-  echo -e "gitDiffRef: master\n" >> mull.yml
+  echo -e "gitDiffRef: origin/master\n" >> mull.yml
   echo -e "gitProjectRoot: ../../" >> mull.yml
 
   #PYTHONPATH=/home/batman/:/home/batman/panda/opendbc/:$PYTHONPATH mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./test_mazda.py
