140e498928f25292f3b27e3fa3e34a35d3c0cd95
Maxime Desroches <desroches.maxime@gmail.com>
2024-09-25 15:27:14 -0700

    report

--- tests/safety/test.sh
+++ tests/safety/test.sh
@@ -11,19 +11,20 @@ scons -j$(nproc) -D
 HW_TYPES=( 6 9 )
 for hw_type in "${HW_TYPES[@]}"; do
   echo "Testing HW_TYPE: $hw_type"
-  #HW_TYPE=$hw_type pytest test_*.py
-  PYTHONPATH=/home/batman/:/home/batman/panda/opendbc/:$PYTHONPATH LLVM_PROFILE_FILE="safety_%m.profraw" HW_TYPE=$hw_type pytest test_*.py
+  LLVM_PROFILE_FILE="safety_%m.profraw" HW_TYPE=$hw_type pytest test_*.py
+  #PYTHONPATH=/home/batman/:/home/batman/panda/opendbc/:$PYTHONPATH LLVM_PROFILE_FILE="safety_%m.profraw" HW_TYPE=$hw_type pytest test_*.py
 done
 
-# generate and open report
+# generate coverage report
+llvm-profdata-17 merge -sparse safety_*.profraw -o safety.profdata
+
+# open html report
 if [ "$1" == "--report" ]; then
-  geninfo ../libpanda/ -o coverage.info
-  genhtml coverage.info -o coverage-out
-  sensible-browser coverage-out/index.html
+  llvm-cov-17 show -format=html -show-branches=count -instr-profile=safety.profdata ../libpanda/libpanda.so -sources ../../board/safety/safety_*.h ../../board/safety.h -o coverage_report
+  sensible-browser coverage_report/index.html
 fi
 
 # test line coverage
-llvm-profdata-17 merge -sparse safety_*.profraw -o safety.profdata
 INCOMPLETE_COVERAGE=$(llvm-cov-17 report -show-region-summary=false -show-branch-summary=false -instr-profile=safety.profdata ../libpanda/libpanda.so -sources ../../board/safety/safety_*.h | awk '$7 != "100.00%"' | head -n -1)
 if [ ! $(echo "$INCOMPLETE_COVERAGE" | wc -l) -eq 2 ]; then
   echo "FAILED: Some files have less than 100% line coverage:"
