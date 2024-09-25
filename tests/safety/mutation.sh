23cf0d35f25f5b2439cf9c777d2575563ebfe13d
Maxime Desroches <desroches.maxime@gmail.com>
2024-09-24 17:20:21 -0700

    test all modes

--- tests/safety/mutation.sh
+++ tests/safety/mutation.sh
@@ -10,15 +10,13 @@ cd $DIR
 
 GIT_REF="${GIT_REF:-origin/master}"
 
-SAFETY_MODELS=( body chrysler defaults gm honda hyundai_canfd hyundai mazda nissan volkswagen_pq )
-for safety_model in "${SAFETY_MODELS[@]}"; do
-  echo "  "
-  echo "  "
-  echo -e "Testing mutation on : safety_$safety_model"
-  echo -e "" > mull.yml
-  echo -e "gitDiffRef: $GIT_REF\n" >> mull.yml
-  echo -e "gitProjectRoot: ../../" >> mull.yml
+SAFETY_MODELS=$(find * | grep "^test_.*\.py")
+for safety_model in ${SAFETY_MODELS[@]}; do
+  echo ""
+  echo ""
+  echo -e "Testing mutations on : $safety_model"
+  echo -e "gitDiffRef: $GIT_REF\ngitProjectRoot: ../../" > mull.yml
 
-  #PYTHONPATH=/home/batman/:/home/batman/panda/opendbc/:$PYTHONPATH mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./test_$safety_model.py || true
-  mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./test_$safety_model.py
+  #PYTHONPATH=/home/batman/:/home/batman/panda/opendbc/:$PYTHONPATH mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./$safety_model
+  mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./$safety_model
 done
