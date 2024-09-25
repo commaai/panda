72081083addd947f68200b2be97e3621e9948e5a
Maxime Desroches <desroches.maxime@gmail.com>
2024-09-25 13:02:30 -0700

    better

--- Dockerfile
+++ Dockerfile
@@ -14,15 +14,12 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     python3 \
     python3-dev \
     python3-pip \
+    clang-17 \
  && rm -rf /var/lib/apt/lists/* && \
     apt clean && \
     cd /usr/lib/gcc/arm-none-eabi/* && \
     rm -rf arm/ && \
     rm -rf thumb/nofp thumb/v6* thumb/v8* thumb/v7+fp thumb/v7-r+fp.sp
-
-RUN apt-get update && apt-get install -y curl clang-17 && \
-    curl -1sLf 'https://dl.cloudsmith.io/public/mull-project/mull-stable/setup.deb.sh' | bash && \
-    apt-get update && apt-get install -y mull-17
 
 ENV CPPCHECK_DIR=/tmp/cppcheck
 COPY tests/misra/install.sh /tmp/
--- tests/safety/mutation.sh
+++ tests/safety/mutation.sh
@@ -4,18 +4,18 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-cd ../../
-scons --mutation -j$(nproc)
-cd $DIR
+$DIR/install_mull.sh
+
+scons --mutation -j$(nproc) -D
 
 GIT_REF="${GIT_REF:-origin/master}"
+echo -e "gitDiffRef: $GIT_REF\ngitProjectRoot: ../../" > mull.yml
 
 SAFETY_MODELS=$(find * | grep "^test_.*\.py")
 for safety_model in ${SAFETY_MODELS[@]}; do
   echo ""
   echo ""
   echo -e "Testing mutations on : $safety_model"
-  echo -e "gitDiffRef: $GIT_REF\ngitProjectRoot: ../../" > mull.yml
 
   #PYTHONPATH=/home/batman/:/home/batman/panda/opendbc/:$PYTHONPATH mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./$safety_model
   mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./$safety_model
