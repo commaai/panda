147b359928d2d4af546decf62f7408a49f35fcd4
Maxime Desroches <desroches.maxime@gmail.com>
2024-10-01 15:39:21 -0700

    save

--- tests/safety/mutation.sh
+++ tests/safety/mutation.sh
@@ -4,18 +4,32 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
-$DIR/install_mull.sh
+MULL_OPS="mutators: [cxx_increment, cxx_decrement, cxx_comparison, cxx_bitwise_assignment, cxx_bitwise, cxx_arithmetic_assignment, cxx_arithmetic]"
+MULL_MODE="${MULL_MODE:-SAFETY_ONLY}"
 
 GIT_REF="${GIT_REF:-origin/master}"
 GIT_ROOT=$(git rev-parse --show-toplevel)
-echo -e "mutators:\n  - cxx_all" > $GIT_ROOT/mull.yml
-scons --mutation -j$(nproc) -D
-echo -e "timeout: 10000\ngitDiffRef: $GIT_REF\ngitProjectRoot: $GIT_ROOT" >> $GIT_ROOT/mull.yml
+MULL_GIT="gitDiffRef: $GIT_REF\ngitProjectRoot: $GIT_ROOT"
 
-SAFETY_MODELS=$(find * | grep "^test_.*\.py")
-for safety_model in ${SAFETY_MODELS[@]}; do
+function write_conf() {
+  echo -e "timeout: 10000\n$MULL_OPS\n$1" > $GIT_ROOT/mull.yml
+}
+
+$DIR/install_mull.sh
+
+write_conf
+scons --mutation -j$(nproc) -D
+
+SAFETY_TESTS=$(find * | grep "^test_.*\.py")
+for SAFETY_TEST in ${SAFETY_TESTS[@]}; do
   echo ""
   echo ""
-  echo -e "Testing mutations on : $safety_model"
-  mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./$safety_model
+  echo -e "Testing mutations on : $SAFETY_TEST"
+  #if [[ $MULL_MODE == "SAFETY_ONLY" ]]; then
+    #SAFETY_MODE=$(echo $SAFETY_TEST | sed -e 's/test_/safety_/g' | sed -e 's/\.py/\.h/g')
+    #write_conf "includePaths:\n - $SAFETY_MODE"
+  #else
+    #write_conf "$MULL_GIT"
+  #fi
+  mull-runner-17 --ld-search-path /lib/x86_64-linux-gnu/ ../libpanda/libpanda.so -test-program=./$SAFETY_TEST > "FAILURES_$SAFETY_TEST" || true
 done
