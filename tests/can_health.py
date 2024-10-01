2b5db7aa1dd1d09af38deaf188f373889f128e8e
dzid26 <radek@comma.ai>
2024-10-01 12:59:55 -0700

    can_health printer

--- tests/can_health.py
+++ tests/can_health.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-
 import time
-from panda import PandaJungle
+from panda import Panda
 
 if __name__ == "__main__":
-  jungle = PandaJungle()
 
+  panda = Panda()
   while True:
     print(chr(27) + "[2J") # clear screen
     for bus in range(3):
-      print(f"\nBus {bus}:\n{jungle.can_health(bus)}")
+      print(f"\nBus {bus}:\n{panda.can_health(bus)}")
     print()
     time.sleep(1)
