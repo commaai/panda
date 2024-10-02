27e7d96ba49c93b3242302a039296506651ba51f
dzid26 <radek@comma.ai>
2024-10-01 19:12:25 -0700

    fix indent

--- tests/can_health.py
+++ tests/can_health.py
@@ -7,12 +7,12 @@ RED = '\033[91m'
 GREEN = '\033[92m'
 
 def colorize_errors(value):
-    if isinstance(value, str):
-        if re.search(r'(?i)No error', value):
-            return f'{GREEN}{value}\033[0m'
-        elif re.search(r'(?i)(?<!No error\s)(err|error)', value):
-            return f'{RED}{value}\033[0m'
-    return str(value)
+  if isinstance(value, str):
+    if re.search(r'(?i)No error', value):
+      return f'{GREEN}{value}\033[0m'
+    elif re.search(r'(?i)(?<!No error\s)(err|error)', value):
+      return f'{RED}{value}\033[0m'
+  return str(value)
 
 if __name__ == "__main__":
 
