f01d273d2758cfaffb7bdd9ef7aa83a67fcb1b96
Jason Young <jyoung8607@gmail.com>
2024-10-01 20:04:26 -0400

    gate SecOC variant on ALLOW_DEBUG

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -335,7 +335,9 @@ static safety_config toyota_init(uint16_t param) {
   const uint32_t TOYOTA_PARAM_LTA = 4UL << TOYOTA_PARAM_OFFSET;
   const uint32_t TOYOTA_PARAM_SECOC_CAR = 8UL << TOYOTA_PARAM_OFFSET;
 
+#ifdef ALLOW_DEBUG
   toyota_secoc_car = GET_FLAG(param, TOYOTA_PARAM_SECOC_CAR);
+#endif
   toyota_alt_brake = GET_FLAG(param, TOYOTA_PARAM_ALT_BRAKE);
   toyota_stock_longitudinal = GET_FLAG(param, TOYOTA_PARAM_STOCK_LONGITUDINAL);
   toyota_lta = GET_FLAG(param, TOYOTA_PARAM_LTA);
