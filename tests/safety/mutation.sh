47f84169425c181e9034800d8acb1c12e591f2e4
Maxime Desroches <desroches.maxime@gmail.com>
2024-10-02 21:19:17 -0700

    mutation test: add more ops (#2059)
    * log
    
    * more

--- tests/safety/mutation.sh
+++ tests/safety/mutation.sh
@@ -8,7 +8,7 @@ $DIR/install_mull.sh
 
 GIT_REF="${GIT_REF:-origin/master}"
 GIT_ROOT=$(git rev-parse --show-toplevel)
-MULL_OPS="mutators: [cxx_increment, cxx_decrement, cxx_comparison, cxx_boundary, cxx_bitwise_assignment, cxx_bitwise, cxx_arithmetic_assignment, cxx_arithmetic]"
+MULL_OPS="mutators: [cxx_increment, cxx_decrement, cxx_comparison, cxx_boundary, cxx_bitwise_assignment, cxx_bitwise, cxx_arithmetic_assignment, cxx_arithmetic, cxx_remove_negation, cxx_replace_scalar_call]"
 echo -e "$MULL_OPS" > $GIT_ROOT/mull.yml
 scons --mutation -j$(nproc) -D
 echo -e "timeout: 10000\ngitDiffRef: $GIT_REF\ngitProjectRoot: $GIT_ROOT" >> $GIT_ROOT/mull.yml
