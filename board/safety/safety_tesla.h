adecbac7857eb37cad1d69bea8cf7b827c0696fd
lukasloetkolben <61192133+lukasloetkolben@users.noreply.github.com>
2024-10-14 18:53:26 -0700

    add generic_rx_check for eacMonitor

--- board/safety/safety_tesla.h
+++ board/safety/safety_tesla.h
@@ -57,6 +57,7 @@ static void tesla_rx_hook(const CANPacket_t *to_push) {
   }
 
   generic_rx_checks((addr == 0x488) && (bus == 0));
+  generic_rx_checks((addr == 0x27d) && (bus == 0));
 
   if (tesla_longitudinal) {
       generic_rx_checks((addr == 0x2b9) && (bus == 0));
