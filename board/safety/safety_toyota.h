1418744bca675d87bb5e265cf2b3f79cf802adc6
Jason Young <jyoung8607@gmail.com>
2024-11-01 16:55:42 -0400

    cleaner, hopefully satisfies mutation test

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -223,8 +223,7 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
       int desired_accel = (GET_BYTE(to_send, 0) << 8) | GET_BYTE(to_send, 1);
       desired_accel = to_signed(desired_accel, 16);
 
-      bool violation = false;
-      violation |= !toyota_secoc;  // Only SecOC cars may transmit this message
+      bool violation = !toyota_secoc;  // Only SecOC cars may transmit this message
       violation |= longitudinal_accel_checks(desired_accel, TOYOTA_LONG_LIMITS);
 
       if (violation) {
