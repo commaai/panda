4ca9905011ba875efa4278b2dcb507a759b0e13e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-12-22 09:56:44 -0800

    cuatro: enable SOM reset

--- board/boards/cuatro.h
+++ board/boards/cuatro.h
@@ -67,8 +67,8 @@ static void cuatro_set_fan_enabled(bool enabled) {
 
 static void cuatro_set_bootkick(BootState state) {
   set_gpio_output(GPIOA, 0, state != BOOT_BOOTKICK);
-  // only use if we have to
-  //set_gpio_output(GPIOC, 12, state != BOOT_RESET);
+  // TODO: confirm we need this
+  set_gpio_output(GPIOC, 12, state != BOOT_RESET);
 }
 
 static void cuatro_set_amp_enabled(bool enabled){
