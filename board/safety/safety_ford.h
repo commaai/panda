11e9606f6fdc9397722a509b3bf56ace04dfa889
Shane Smiskol <shane@smiskol.com>
2025-01-22 19:57:45 -0800

    nice and clean!

--- board/safety/safety_ford.h
+++ board/safety/safety_ford.h
@@ -363,42 +363,33 @@ static safety_config ford_init(uint16_t param) {
     {.msg = {{FORD_DesiredTorqBrk, 0, 8, .frequency = 50U}, { 0 }, { 0 }}},
   };
 
+  #define FORD_COMMON_TX_MSGS          \
+    {FORD_Steering_Data_FD1, 0, 8},    \
+    {FORD_Steering_Data_FD1, 2, 8},    \
+    {FORD_ACCDATA_3, 0, 8},            \
+    {FORD_Lane_Assist_Data1, 0, 8},    \
+    {FORD_IPMA_Data, 0, 8},            \
+
   static const CanMsg FORD_CANFD_LONG_TX_MSGS[] = {
-    {FORD_Steering_Data_FD1, 0, 8},
-    {FORD_Steering_Data_FD1, 2, 8},
+    FORD_COMMON_TX_MSGS
     {FORD_ACCDATA, 0, 8},
-    {FORD_ACCDATA_3, 0, 8},
-    {FORD_Lane_Assist_Data1, 0, 8},
     {FORD_LateralMotionControl2, 0, 8},
-    {FORD_IPMA_Data, 0, 8},
   };
 
   static const CanMsg FORD_CANFD_STOCK_TX_MSGS[] = {
-    {FORD_Steering_Data_FD1, 0, 8},
-    {FORD_Steering_Data_FD1, 2, 8},
-    {FORD_ACCDATA_3, 0, 8},
-    {FORD_Lane_Assist_Data1, 0, 8},
+    FORD_COMMON_TX_MSGS
     {FORD_LateralMotionControl2, 0, 8},
-    {FORD_IPMA_Data, 0, 8},
   };
 
   static const CanMsg FORD_STOCK_TX_MSGS[] = {
-    {FORD_Steering_Data_FD1, 0, 8},
-    {FORD_Steering_Data_FD1, 2, 8},
-    {FORD_ACCDATA_3, 0, 8},
-    {FORD_Lane_Assist_Data1, 0, 8},
+    FORD_COMMON_TX_MSGS
     {FORD_LateralMotionControl, 0, 8},
-    {FORD_IPMA_Data, 0, 8},
   };
 
   static const CanMsg FORD_LONG_TX_MSGS[] = {
-    {FORD_Steering_Data_FD1, 0, 8},
-    {FORD_Steering_Data_FD1, 2, 8},
+    FORD_COMMON_TX_MSGS
     {FORD_ACCDATA, 0, 8},
-    {FORD_ACCDATA_3, 0, 8},
-    {FORD_Lane_Assist_Data1, 0, 8},
     {FORD_LateralMotionControl, 0, 8},
-    {FORD_IPMA_Data, 0, 8},
   };
 
   UNUSED(param);
