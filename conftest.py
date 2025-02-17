92fef79de28d1a684e641d3b0cdcb1020ec3abb0
Shane Smiskol <shane@smiskol.com>
2025-02-17 00:39:11 -0800

    rm conftest

--- conftest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-def pytest_pycollect_makeitem(collector, name, obj):
-  if isinstance(obj, type) and issubclass(obj, unittest.TestCase):
-    abstract_methods = getattr(obj, '__abstractmethods__', None)
-    if abstract_methods:
-      raise TypeError(f"Test class {name} is abstract and missing implementations for: {abstract_methods}")
