3199175c3118ce3cbdff620b98530f3068637465
savojovic <jovo.savic00@gmail.com>
2025-03-15 11:08:07 +0100

    Refactor headers in panda/board/jungle/boards/.*h

---
+++ board/jungle/boards/src/board.c
@@ -0,0 +1,15 @@
+#include "board.h"
+
+void unused_set_individual_ignition(uint8_t bitmask) {
+  UNUSED(bitmask);
+}
+
+void unused_board_enable_header_pin(uint8_t pin_num, bool enabled) {
+  UNUSED(pin_num);
+  UNUSED(enabled);
+}
+
+void unused_set_panda_individual_power(uint8_t port_num, bool enabled) {
+  UNUSED(port_num);
+  UNUSED(enabled);
+}
