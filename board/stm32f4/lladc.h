77969616a41c39c0acf0b860e05a40813cd31d24
savojovic <jovo.savic00@gmail.com>
2025-03-15 13:16:34 +0100

    Refactor headers in panda/board/stm32f4/*.h

--- board/stm32f4/lladc.h
+++ board/stm32f4/lladc.h
@@ -1,24 +1,9 @@
+#pragma once
 
-void register_set(volatile uint32_t *addr, uint32_t val, uint32_t mask);
+void register_set(volatile uint32_t *addr, uint32_t val, uint32_t mask); // TODO: Check if this should be removed
 
-void adc_init(void) {
-  register_set(&(ADC->CCR), ADC_CCR_TSVREFE | ADC_CCR_VBATE, 0xC30000U);
-  register_set(&(ADC1->CR2), ADC_CR2_ADON, 0xFF7F0F03U);
-  register_set(&(ADC1->SMPR1), ADC_SMPR1_SMP12 | ADC_SMPR1_SMP13, 0x7FFFFFFU);
-}
+void adc_init(void);
 
-uint16_t adc_get_raw(uint8_t channel) {
-  // Select channel
-  register_set(&(ADC1->JSQR), ((uint32_t) channel << 15U), 0x3FFFFFU);
+uint16_t adc_get_raw(uint8_t channel);
 
-  // Start conversion
-  ADC1->SR &= ~(ADC_SR_JEOC);
-  ADC1->CR2 |= ADC_CR2_JSWSTART;
-  while (!(ADC1->SR & ADC_SR_JEOC));
-
-  return ADC1->JDR1;
-}
-
-uint16_t adc_get_mV(uint8_t channel) {
-  return (adc_get_raw(channel) * current_board->avdd_mV) / 4095U;
-}
+uint16_t adc_get_mV(uint8_t channel);
--- board/stm32f4/llflash.h
+++ board/stm32f4/llflash.h
@@ -1,28 +1,11 @@
-bool flash_is_locked(void) {
-  return (FLASH->CR & FLASH_CR_LOCK);
-}
+#pragma once
 
-void flash_unlock(void) {
-  FLASH->KEYR = 0x45670123;
-  FLASH->KEYR = 0xCDEF89AB;
-}
+bool flash_is_locked(void);
 
-bool flash_erase_sector(uint8_t sector, bool unlocked) {
-  // don't erase the bootloader(sector 0)
-  if (sector != 0 && sector < 12 && unlocked) {
-    FLASH->CR = (sector << 3) | FLASH_CR_SER;
-    FLASH->CR |= FLASH_CR_STRT;
-    while (FLASH->SR & FLASH_SR_BSY);
-    return true;
-  }
-  return false;
-}
+void flash_unlock(void);
 
-void flash_write_word(void *prog_ptr, uint32_t data) {
-  uint32_t *pp = prog_ptr;
-  FLASH->CR = FLASH_CR_PSIZE_1 | FLASH_CR_PG;
-  *pp = data;
-  while (FLASH->SR & FLASH_SR_BSY);
-}
+bool flash_erase_sector(uint8_t sector, bool unlocked);
 
-void flush_write_buffer(void) { }
+void flash_write_word(void *prog_ptr, uint32_t data);
+
+void flush_write_buffer(void);
