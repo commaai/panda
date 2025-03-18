624948889db0cfafee7baf588edf600823fe1156
Comma Device <device@comma.ai>
2025-03-18 12:37:29 +0000

    2 seconds?

--- tests/hitl/2_health.py
+++ tests/hitl/2_health.py
@@ -9,7 +9,7 @@ from panda import Panda
 @pytest.mark.skip_panda_types((Panda.HW_TYPE_DOS, ))
 def test_voltage(p):
   # LPF needs to stabilize first
-  while p.health()['uptime'] < 1:
+  while p.health()['uptime'] < 2:
     time.sleep(0.1)
 
   for _ in range(10):
