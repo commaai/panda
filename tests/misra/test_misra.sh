f2508ad313f0565efb41e57a0e46d6aa21e2f714
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-04-03 13:29:03 -0400

    fix misra pt1

--- tests/misra/test_misra.sh
+++ tests/misra/test_misra.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PANDA_DIR=$(realpath $DIR/../../)
@@ -44,12 +44,19 @@ cppcheck() {
   echo -e "\n\n\n\n\nTEST variant options:" >> $CHECKLIST
   echo -e ""${@//$PANDA_DIR/}"\n\n" >> $CHECKLIST # (absolute path removed)
 
-  $CPPCHECK_DIR/cppcheck --inline-suppr -I $PANDA_DIR/board/ \
+  $CPPCHECK_DIR/cppcheck --inline-suppr \
+          -I $PANDA_DIR/ \
+          -I $PANDA_DIR/board/ \
+          -I $PANDA_DIR/board/stm32f4/ \
+          -I $PANDA_DIR/board/stm32h7/ \
+          -I $PANDA_DIR/include/board/ \
+          -I $PANDA_DIR/include/board/stm32f4/ \
+          -I $PANDA_DIR/include/board/stm32h7/ \
           -I "$(arm-none-eabi-gcc -print-file-name=include)" \
           -I $PANDA_DIR/board/stm32f4/inc/ -I $PANDA_DIR/board/stm32h7/inc/ \
           -I $PANDA_DIR/../opendbc/safety/ \
           --suppressions-list=$DIR/suppressions.txt --suppress=*:*inc/* \
-          --suppress=*:*include/* --error-exitcode=2 --check-level=exhaustive --safety \
+          --error-exitcode=2 --check-level=exhaustive --safety \
           --platform=arm32-wchar_t4 $COMMON_DEFINES --checkers-report=$CHECKLIST.tmp \
           --std=c11 "$@" 2>&1 | tee $OUTPUT
 
