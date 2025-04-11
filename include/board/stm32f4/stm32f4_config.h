2b499e0e0325552e2bd36cbbaff023eacc09d83c
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-04-11 11:36:55 -0400

    more cleanup

--- include/board/stm32f4/stm32f4_config.h
+++ include/board/stm32f4/stm32f4_config.h
@@ -40,7 +40,7 @@
 
 #include "stm32f4/llusb.h"
 
-static void early_gpio_float(void) {
+void early_gpio_float(void) {
   RCC->AHB1ENR = RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN | RCC_AHB1ENR_GPIOCEN;
 
   GPIOB->MODER = 0; GPIOC->MODER = 0;
