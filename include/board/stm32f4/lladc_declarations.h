165b71de9763557f2f29f944bdc7a2b5a551f399
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-04-11 11:30:34 -0400

    forward declare stm32f4

---
+++ include/board/stm32f4/lladc_declarations.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdint.h>
+
+void register_set(volatile uint32_t *addr, uint32_t val, uint32_t mask);
+void adc_init(void);
+uint16_t adc_get_raw(uint8_t channel);
+uint16_t adc_get_mV(uint8_t channel);
