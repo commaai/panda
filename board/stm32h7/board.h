c48b3e743fc79f56081bfb3bd87ab524dfa9084e
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-04-14 12:19:19 -0400

    delete unused code!

--- board/stm32h7/board.h
+++ board/stm32h7/board.h
@@ -9,7 +9,6 @@
 #include "drivers/harness.h"
 #include "drivers/fan.h"
 #include "stm32h7/llfan.h"
-#include "stm32h7/lldac.h"
 #include "drivers/fake_siren.h"
 #include "stm32h7/sound.h"
 #include "drivers/clock_source.h"
