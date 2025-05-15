ca8911d877858de5864b16494cbd0a5232c10459
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-05-15 18:10:37 -0400

    cleanup

--- board/can.h
+++ board/can.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define PANDA_CAN_CNT 3U
-#define PANDA_BUS_CNT 3U
-
 // bump this when changing the CAN packet
 #define CAN_PACKET_VERSION 4
+
+static const uint8_t PANDA_CAN_CNT = 3U;
+static const uint8_t PANDA_BUS_CNT = 3U;
