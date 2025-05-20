c25971cc79525f695cc650d027ce7a7a0cc106cc
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-05-20 10:13:19 -0700

    install first

--- tests/misra/test_mutation.py
+++ tests/misra/test_mutation.py
@@ -65,7 +65,7 @@ for p in patterns:
   mutations.append((random.choice(files), p, True))
 
 # TODO: remove sampling once test_misra.sh is faster
-mutations = random.sample(mutations, 4)
+mutations = random.sample(mutations, 2)
 
 @pytest.mark.parametrize("fn, patch, should_fail", mutations)
 def test_misra_mutation(fn, patch, should_fail):
