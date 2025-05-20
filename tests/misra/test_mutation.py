4fc243b998e3f7e32d47b21edb1a89526b4b484c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-05-20 10:18:47 -0700

    CI: speed up MISRA mutation job (#2213)
    * CI: speed up MISRA mutation job
    
    * install first

--- tests/misra/test_mutation.py
+++ tests/misra/test_mutation.py
@@ -64,6 +64,9 @@ assert len(files) > 70, all(d in files for d in ('board/main.c', 'board/stm32f4/
 for p in patterns:
   mutations.append((random.choice(files), p, True))
 
+# TODO: remove sampling once test_misra.sh is faster
+mutations = random.sample(mutations, 2)
+
 @pytest.mark.parametrize("fn, patch, should_fail", mutations)
 def test_misra_mutation(fn, patch, should_fail):
   with tempfile.TemporaryDirectory() as tmp:
