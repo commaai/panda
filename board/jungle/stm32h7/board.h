18189d208a5b7f55fa3ffd446add1dfda8eab2ed
Comma Device <device@comma.ai>
2025-07-30 14:04:53 +0000

    use common ADC driver in jungle

--- board/jungle/stm32h7/board.h
+++ board/jungle/stm32h7/board.h
@@ -1,6 +1,6 @@
 #include "board/jungle/boards/board_declarations.h"
 
-#include "board/jungle/stm32h7/lladc.h"
+#include "board/stm32h7/lladc.h"
 #include "board/jungle/boards/board_v2.h"
 
 void detect_board_type(void) {
