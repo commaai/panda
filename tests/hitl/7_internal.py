c9ff9b07886d36d99a56a04ff9aa54dea55a7131
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-09-13 11:07:02 -0700

    not relevant anymore

--- tests/hitl/7_internal.py
+++ tests/hitl/7_internal.py
@@ -38,21 +38,3 @@ def test_fan_cooldown(p):
   for _ in range(5):
     assert p.get_fan_rpm() <= Panda.MAX_FAN_RPMs[bytes(p.get_type())]
     time.sleep(0.5)
-
-def test_fan_overshoot(p):
-
-  # make sure it's stopped completely
-  p.set_fan_power(0)
-  while p.get_fan_rpm() > 0:
-    time.sleep(0.1)
-
-  # set it to 30% power to mimic going onroad
-  p.set_fan_power(30)
-  max_rpm = 0
-  for _ in range(50):
-    max_rpm = max(max_rpm, p.get_fan_rpm())
-    time.sleep(0.1)
-
-  # tolerate 10% overshoot
-  expected_rpm = Panda.MAX_FAN_RPMs[bytes(p.get_type())] * 30 / 100
-  assert max_rpm <= 1.1 * expected_rpm, f"Fan overshoot: {(max_rpm / expected_rpm * 100) - 100:.1f}%"
