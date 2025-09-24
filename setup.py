c93a96abe3d0e9db0b7a580b11e4c53e4bb0cfea
prayansh_chhablani <prayanshchhablani@gmail.com>
2025-09-25 00:41:27 +0530

    address comments

--- setup.py
+++ setup.py
@@ -3,22 +3,19 @@ import subprocess
 from setuptools import setup, find_packages
 from setuptools.command.build import build as _build
 
+
 class build(_build):
     def run(self):
         env = os.environ.copy()
-        env['FINAL_PROVISIONING'] = '1'
 
         project_root = os.path.abspath(os.path.dirname(__file__))
 
         print("Building firmware with scons...")
 
-        subprocess.check_call(
-            ["scons", "-j4"],
-            cwd=project_root,
-            env=env
-        )
+        subprocess.check_call(["scons", "-j4"], cwd=project_root, env=env)
 
         super().run()
+
 
 setup(
     name="pandacan",
