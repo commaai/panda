11fe40da73d9711d0f137bef48548605bb13034a
Jakub Berkop <jakub.berkop@gmail.com>
2025-12-05 23:35:49 +0100

    Misra fixups

--- board/stm32h7/llflash.c
+++ board/stm32h7/llflash.c
@@ -3,17 +3,20 @@
 #include "stm32h7_config.h"
 
 // cppcheck-suppress misra-c2012-8.7
+// cppcheck-suppress unusedFunction; Used in board/flasher.c, which is excluded from cppcheck
 bool flash_is_locked(void) {
   return (FLASH->CR1 & FLASH_CR_LOCK);
 }
 
 // cppcheck-suppress misra-c2012-8.7
+// cppcheck-suppress unusedFunction; Used in board/flasher.c, which is excluded from cppcheck
 void flash_unlock(void) {
   FLASH->KEYR1 = 0x45670123u;
   FLASH->KEYR1 = 0xCDEF89ABu;
 }
 
 // cppcheck-suppress misra-c2012-8.7
+// cppcheck-suppress unusedFunction; Used in board/flasher.c, which is excluded from cppcheck
 bool flash_erase_sector(uint8_t sector, bool is_unlocked) {
   // don't erase the bootloader(sector 0)
   bool ret = false;
@@ -27,6 +30,7 @@ bool flash_erase_sector(uint8_t sector, bool is_unlocked) {
 }
 
 // cppcheck-suppress misra-c2012-8.7
+// cppcheck-suppress unusedFunction; Used in board/flasher.c, which is excluded from cppcheck
 void flash_write_word(void *prog_ptr, uint32_t data) {
   uint32_t *pp = prog_ptr;
   FLASH->CR1 |= FLASH_CR_PG;
@@ -35,6 +39,7 @@ void flash_write_word(void *prog_ptr, uint32_t data) {
 }
 
 // cppcheck-suppress misra-c2012-8.7
+// cppcheck-suppress unusedFunction; Used in board/flasher.c, which is excluded from cppcheck
 void flush_write_buffer(void) {
   if ((FLASH->SR1 & FLASH_SR_WBNE) != 0u) {
     FLASH->CR1 |= FLASH_CR_FW;
