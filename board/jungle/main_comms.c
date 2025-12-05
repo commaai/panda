b008e0ea7ccf0d79228c8c5c6d4224230ecabc44
Jakub Berkop <jakub.berkop@gmail.com>
2025-12-05 14:54:49 +0100

    Cleanup

--- board/jungle/main_comms.c
+++ board/jungle/main_comms.c
@@ -12,8 +12,6 @@
 #include "board/libc.h"
 #include "board/drivers/uart.h"
 
-extern int _app_start[0xc000]; // Only first 3 sectors of size 0x4000 are used
-
 bool generated_can_traffic = false;
 
 static int get_jungle_health_pkt(void *dat) {
