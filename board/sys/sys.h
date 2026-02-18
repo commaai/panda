e9da947622243b176668b0561b0ffee452c0b76e
elkoled <elkoled@gmail.com>
2026-02-17 18:54:10 -0800

    rm

--- board/sys/sys.h
+++ board/sys/sys.h
@@ -58,9 +58,6 @@ void fault_recovered(uint32_t fault);
 
 // ******************** power_saving ********************
 
-// WARNING: To stay in compliance with the SIL2 rules laid out in STM UM2331, we should never use any of the available hardware low power modes during safety function execution.
-// See rule: CoU_3
-
 extern bool power_save_enabled;
 
 void set_power_save_state(bool enable);
