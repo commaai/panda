a602521b9660f63719877df06a2b48bfab30d2ab
xunwen-art <xunwen.art@gmail.com>
2026-03-09 02:46:39 +0800

    fix: remove implementations from interrupts.h - keep only declarations
    interrupts.h should only have declarations, not implementations.
    Implementations are in interrupts.c.

--- board/drivers/interrupts.h
+++ board/drivers/interrupts.h
@@ -3,79 +3,14 @@
 
 #include "board/drivers/drivers.h"
 
-// interrupt struct and related macros are now defined in drivers.h
-// This file only contains the implementation
+// interrupt struct and related macros are defined in drivers.h
+// This file only contains declarations
 
-void unused_interrupt_handler(void) {
-  // Something is wrong if this handler is called!
-  print("Unused interrupt handler called!\n");
-  fault_occurred(FAULT_UNUSED_INTERRUPT_HANDLED);
-}
-
-// Global interrupt array - defined here, declared in drivers.h
-interrupt interrupts[NUM_INTERRUPTS];
-
-static bool check_interrupt_rate = false;
-
-static uint32_t idle_time = 0U;
-static uint32_t busy_time = 0U;
-// interrupt_load is declared in drivers.h
-
-void handle_interrupt(IRQn_Type irq_type){
-  static uint8_t interrupt_depth = 0U;
-  static uint32_t last_time = 0U;
-  uint32_t time;
-  
-  ENTER_CRITICAL();
-  if (interrupt_depth == 0U) {
-    time = microsecond_timer_get();
-    idle_time += get_ts_elapsed(time, last_time);
-    last_time = time;
-  }
-  interrupt_depth += 1U;
-  EXIT_CRITICAL();
-
-  interrupts[irq_type].call_counter++;
-  interrupts[irq_type].handler();
-
-  EXIT_CRITICAL();
-  interrupt_depth -= 1U;
-  ENTER_CRITICAL();
-
-  if (interrupt_depth == 0U) {
-    time = microsecond_timer_get();
-    busy_time += get_ts_elapsed(time, last_time);
-    last_time = time;
-  }
-}
-
-void interrupt_timer_handler(void) {
-  static uint32_t call_rate_counters[NUM_INTERRUPTS] = {0};
-  uint32_t i;
-
-  for (i = 0U; i < NUM_INTERRUPTS; i++) {
-    if (interrupts[i].handler != unused_interrupt_handler) {
-      call_rate_counters[i] += interrupts[i].call_counter;
-      if (check_interrupt_rate && (call_rate_counters[i] > interrupts[i].max_call_rate)) {
-        interrupts[i].call_rate_fault += 1U;
-      }
-      interrupts[i].call_counter = 0U;
-    }
-  }
-  interrupt_load = ((float)busy_time) / ((float)(busy_time + idle_time));
-  busy_time = 0U;
-  idle_time = 0U;
-}
-
-void init_interrupts(bool check_rate_limit) {
-  uint32_t i;
-  check_interrupt_rate = check_rate_limit;
-  for (i = 0U; i < NUM_INTERRUPTS; i++) {
-    interrupts[i].handler = unused_interrupt_handler;
-    interrupts[i].call_counter = 0U;
-    interrupts[i].call_rate = 0U;
-    interrupts[i].call_rate_fault = 0U;
-  }
-}
+void unused_interrupt_handler(void);
+void handle_interrupt(IRQn_Type irq_type);
+void interrupt_timer_handler(void);
+void init_interrupts(bool check_rate_limit);
+void interrupt_timer_init(void);
+uint32_t microsecond_timer_get(void);
 
 #endif
