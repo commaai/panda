8ad955b9516a6a35571e7a4800dc75cdb6347136
elkoled <elkoled@gmail.com>
2026-05-01 12:51:47 -0700

    move can ignition to opendbc

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -1,4 +1,5 @@
 #include "board/drivers/drivers.h"
+#include "opendbc/safety/ignition.h"
 
 uint32_t safety_tx_blocked = 0;
 uint32_t safety_rx_invalid = 0;
@@ -7,7 +8,7 @@ uint32_t rx_buffer_overflow = 0;
 
 can_health_t can_health[PANDA_CAN_CNT] = {{0}, {0}, {0}};
 
-// Ignition detected from CAN meessages
+// Ignition detected from CAN messages
 bool ignition_can = false;
 uint32_t ignition_can_cnt = 0U;
 
@@ -154,67 +155,6 @@ void can_set_forwarding(uint8_t from, uint8_t to) {
   bus_config[from].forwarding_bus = to;
 }
 #endif
-
-void ignition_can_hook(CANPacket_t *msg) {
-  if (msg->bus == 0U) {
-    int len = GET_LEN(msg);
-
-    // GM exception
-    if ((msg->addr == 0x1F1U) && (len == 8)) {
-      // SystemPowerMode (2=Run, 3=Crank Request)
-      ignition_can = (msg->data[0] & 0x2U) != 0U;
-      ignition_can_cnt = 0U;
-    }
-
-    // Rivian R1S/T GEN1 exception
-    if ((msg->addr == 0x152U) && (len == 8)) {
-      // 0x152 overlaps with Subaru pre-global which has this bit as the high beam
-      int counter = msg->data[1] & 0xFU;  // max is only 14
-
-      static int prev_counter_rivian = -1;
-      if ((counter == ((prev_counter_rivian + 1) % 15)) && (prev_counter_rivian != -1)) {
-        // VDM_OutputSignals->VDM_EpasPowerMode
-        ignition_can = ((msg->data[7] >> 4U) & 0x3U) == 1U;  // VDM_EpasPowerMode_Drive_On=1
-        ignition_can_cnt = 0U;
-      }
-      prev_counter_rivian = counter;
-    }
-
-    // Tesla Model 3/Y exception
-    if ((msg->addr == 0x221U) && (len == 8)) {
-      // 0x221 overlaps with Rivian which has random data on byte 0
-      int counter = msg->data[6] >> 4;
-
-      static int prev_counter_tesla = -1;
-      if ((counter == ((prev_counter_tesla + 1) % 16)) && (prev_counter_tesla != -1)) {
-        // VCFRONT_LVPowerState->VCFRONT_vehiclePowerState
-        int power_state = (msg->data[0] >> 5U) & 0x3U;
-        ignition_can = power_state == 0x3;  // VEHICLE_POWER_STATE_DRIVE=3
-        ignition_can_cnt = 0U;
-      }
-      prev_counter_tesla = counter;
-    }
-
-    // Mazda exception
-    if ((msg->addr == 0x9EU) && (len == 8)) {
-      ignition_can = (msg->data[0] >> 5) == 0x6U;
-      ignition_can_cnt = 0U;
-    }
-
-    // Volkswagen MEB exception
-    if ((msg->addr == 0x3C0U) && (len == 4)) {
-      ignition_can = (msg->data[2] >> 1) & 1U;
-      ignition_can_cnt = 0U;
-    }
-  }
-
-  // TODO: this is too loose, Teslas have 0x222
-  // body v2 exception
-  // if (((msg->bus == 0U) || (msg->bus == 2U)) && (msg->addr == 0x222U)) {
-  //   ignition_can = true;
-  //   ignition_can_cnt = 0U;
-  // }
-}
 
 bool can_tx_check_min_slots_free(uint32_t min) {
   return
