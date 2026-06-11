1a98b6dd4771b3e5c710391aa4c2ddd79c7f569d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-06-11 13:50:35 -0700

    setup.sh: remove apt installs

--- setup.sh
+++ setup.sh
@@ -7,21 +7,6 @@ cd $DIR
 PLATFORM=$(uname -s)
 
 echo "installing dependencies"
-if [[ $PLATFORM == "Darwin" ]]; then
-  # pass
-  :
-elif [[ $PLATFORM == "Linux" ]]; then
-  # for AGNOS since we clear the apt lists
-  if [[ ! -d /"var/lib/apt/" ]]; then
-    sudo apt update
-  fi
-
-  sudo apt-get install -y --no-install-recommends \
-    curl ca-certificates gcc git \
-    python3-dev
-else
-  echo "WARNING: unsupported platform. skipping apt/brew install."
-fi
 
 if ! command -v uv &>/dev/null; then
   echo "'uv' is not installed. Installing 'uv'..."
