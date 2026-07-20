d79fb069e886052f9ff93edda0b6c8dfd5b82bd6
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-07-20 15:29:09 -0700

    expose lateral controls state

--- python/__init__.py
+++ python/__init__.py
@@ -543,6 +543,7 @@ class Panda:
       "sbu2_voltage_mV": a[23],
       "som_reset_triggered": a[24],
       "sound_output_level": a[25],
+      "controls_allowed_lateral": a[26],
     }
 
   @ensure_health_packet_version
