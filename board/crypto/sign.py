3c370f4610bd838927a4513c155b3b9b21b7322b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-07-23 09:26:21 -0700

    rm pycryptodome

--- board/crypto/sign.py
+++ board/crypto/sign.py
@@ -3,15 +3,31 @@ import os
 import sys
 import struct
 import hashlib
-from Crypto.PublicKey import RSA
 import binascii
+import base64
+
+
+def read_der(dat):
+  length = dat[1]
+  offset = 2
+  if length & 0x80:
+    length_bytes = length & 0x7f
+    length = int.from_bytes(dat[offset:offset + length_bytes], "big")
+    offset += length_bytes
+  return dat[offset:offset + length], dat[offset + length:]
 
 # increment this to make new hardware not run old versions
 VERSION = 2
 
 if __name__ == "__main__":
-  with open(sys.argv[3]) as k:
-    rsa = RSA.importKey(k.read())
+  with open(sys.argv[3], "rb") as k:
+    pem = b"".join(k.read().splitlines()[1:-1])
+  der, _ = read_der(base64.b64decode(pem))
+  values = []
+  while der:
+    value, der = read_der(der)
+    values.append(int.from_bytes(value, "big"))
+  _, rsa_n, _, rsa_d = values[:4]
 
   with open(sys.argv[1], "rb") as f:
     dat = f.read()
@@ -32,7 +48,7 @@ if __name__ == "__main__":
 
     print("hash:", str(binascii.hexlify(dd), "utf-8"))
     dd = b"\x00\x01" + b"\xff" * 0x69 + b"\x00" + dd
-    rsa_out = pow(int.from_bytes(dd, byteorder='big', signed=False), rsa.d, rsa.n)
+    rsa_out = pow(int.from_bytes(dd, byteorder='big', signed=False), rsa_d, rsa_n)
     sig = (hex(rsa_out)[2:].rjust(0x100, '0'))
     x += binascii.unhexlify(sig)
     f.write(x)
