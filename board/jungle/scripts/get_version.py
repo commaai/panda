aa00a4981ef083689a2b27b1ec5773b8aa1c7974
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-09-11 15:05:25 -0700

    rm jungle/scripts/

--- board/jungle/scripts/get_version.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from panda import PandaJungle
-
-if __name__ == "__main__":
-  for p in PandaJungle.list():
-    pp = PandaJungle(p)
-    print(f"{pp.get_serial()[0]}: {pp.get_version()}")
-
-
